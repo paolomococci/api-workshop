7aa2e230ca5d0610895f958c94ebd2aa5e6d7361
paolomococci <paolomococci@gmail.com>
2021-11-19 16:26:32 +0100

    I added the project sample-java17 developed thanks to Spring Boot

---
+++ Spring-Boot/sample-java17/src/test/java/local/mocaccino/sample/SampleApplicationTests.java
@@ -0,0 +1,12 @@
+package local.mocaccino.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
