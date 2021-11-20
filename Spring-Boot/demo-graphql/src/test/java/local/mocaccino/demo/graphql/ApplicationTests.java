51891c46f02a9fad0d897c857d96b2f1ac5f48e3
paolomococci <paolomococci@gmail.com>
2021-11-20 06:41:40 +0100

    I added the project demo-graphql developed thanks to Spring Boot

---
+++ Spring-Boot/demo-graphql/src/test/java/local/mocaccino/demo/graphql/ApplicationTests.java
@@ -0,0 +1,12 @@
+package local.mocaccino.demo.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void sampleTest() {
+	}
+}
