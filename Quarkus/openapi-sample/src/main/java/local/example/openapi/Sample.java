e254dced1018308100f9776aef33f585746bc29e
paolomococci <paolomococci@gmail.com>
2021-12-28 16:30:31 +0100

    edited class Sample belonging to project openapi-sample

--- Quarkus/openapi-sample/src/main/java/local/example/openapi/Sample.java
+++ Quarkus/openapi-sample/src/main/java/local/example/openapi/Sample.java
@@ -4,4 +4,25 @@ public class Sample {
 
     private String name;
     private String definition;
+
+    public Sample(String name, String definition) {
+        this.name = name;
+        this.definition = definition;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
 }
