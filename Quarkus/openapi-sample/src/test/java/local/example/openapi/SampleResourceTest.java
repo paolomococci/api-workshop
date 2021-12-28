31f117c0dde633d0422c4a51dd173a3b1b864655
paolomococci <paolomococci@gmail.com>
2021-12-28 17:18:56 +0100

    edited class SampleResourceTest belonging to project openapi-sample

--- Quarkus/openapi-sample/src/test/java/local/example/openapi/SampleResourceTest.java
+++ Quarkus/openapi-sample/src/test/java/local/example/openapi/SampleResourceTest.java
@@ -12,7 +12,7 @@ import static org.hamcrest.CoreMatchers.is;
 public class SampleResourceTest {
 
     @Test
-    public void sampleEndpointTest() {
+    public void readEndpointTest() {
 
         given()
           .when().get("/sample")
