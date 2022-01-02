057787010e3ce7e658aa5581986e2112a8ebf750
paolomococci <paolomococci@gmail.com>
2022-01-02 17:32:11 +0100

    edited class ApplicationTests belonging to project cart

--- Spring-Boot/cart/src/test/java/local/example/demo/ApplicationTests.java
+++ Spring-Boot/cart/src/test/java/local/example/demo/ApplicationTests.java
@@ -1,6 +1,7 @@
-package local.example.demo.api;
+package local.example.demo;
 
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
