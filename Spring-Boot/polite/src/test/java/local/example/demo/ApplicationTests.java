e06bb8baef45c20bfd9a44370c0af329b5de8e6a
paolomococci <paolomococci@gmail.com>
2022-01-02 10:31:10 +0100

    added class ApplicationTests belonging to project polite

---
+++ Spring-Boot/polite/src/test/java/local/example/demo/ApplicationTests.java
@@ -0,0 +1,36 @@
+package local.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ApplicationTests {
+
+	private static final String PATIENTS_PATH = "/patients/";
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	void readTest()
+			throws Exception {
+		this.mockMvc
+				.perform(get(PATIENTS_PATH+1))
+				.andExpect(status().isNotImplemented());
+	}
+
+	@Test
+	void readAllTest()
+			throws Exception {
+		this.mockMvc
+				.perform(get(PATIENTS_PATH))
+				.andExpect(status().isNotImplemented());
+	}
+}
