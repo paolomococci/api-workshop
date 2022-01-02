db4c63a43ec386e0381408e9dcfce1fe91ada910
paolomococci <paolomococci@gmail.com>
2022-01-02 12:05:54 +0100

    edited class ApplicationTests belonging to project growing

---
+++ Spring-Boot/growing/src/test/java/local/example/demo/ApplicationTests.java
@@ -0,0 +1,37 @@
+package local.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ApplicationTests {
+
+	private static final String GROWING_PATH = "/growing/";
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	void readTest()
+			throws Exception {
+		this.mockMvc
+				.perform(get(GROWING_PATH+1))
+				.andExpect(status().isNotImplemented());
+	}
+
+	@Test
+	void readAllTest()
+			throws Exception {
+		this.mockMvc
+				.perform(get(GROWING_PATH))
+				.andExpect(status().isNotImplemented());
+	}
+}
