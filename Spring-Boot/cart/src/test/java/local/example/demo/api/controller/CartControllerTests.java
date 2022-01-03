d9e39b18e00941f4ba04e91bd6fe77925cb0888d
paolomococci <paolomococci@gmail.com>
2022-01-03 08:45:54 +0100

    edited class CartControllerTests belonging to project cart

--- Spring-Boot/cart/src/test/java/local/example/demo/api/controller/CartControllerTests.java
+++ Spring-Boot/cart/src/test/java/local/example/demo/api/controller/CartControllerTests.java
@@ -1,4 +1,14 @@
 package local.example.demo.api.controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class CartControllerTests {
+
+    @Test
+    void someTest()
+            throws Exception {
+        // TODO
+    }
 }
