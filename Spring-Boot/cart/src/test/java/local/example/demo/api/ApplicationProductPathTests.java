0d27e215310f8a4759754b215aed5b67fe368e1c
paolomococci <paolomococci@gmail.com>
2022-01-03 08:04:22 +0100

    edited class ApplicationProductPathTests belonging to project cart

--- Spring-Boot/cart/src/test/java/local/example/demo/api/ApplicationProductPathTests.java
+++ Spring-Boot/cart/src/test/java/local/example/demo/api/ApplicationProductPathTests.java
@@ -1,7 +1,14 @@
 package local.example.demo.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ApplicationProductPathTests {
+
+    @Autowired
+    private MockMvc mockMvc;
 }
