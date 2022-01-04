650faf22fd5554eede1b543a1083ec213b3fde4f
paolomococci <paolomococci@gmail.com>
2022-01-04 06:24:44 +0100

    edited class CartController belonging to project cart

--- Spring-Boot/cart/src/main/java/local/example/demo/controller/CartController.java
+++ Spring-Boot/cart/src/main/java/local/example/demo/controller/CartController.java
@@ -1,7 +1,10 @@
 package local.example.demo.controller;
 
+import local.example.demo.api.ApplicationApi;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class CartController {
+public class CartController
+        implements ApplicationApi {
 }
