b188e4b0ad10f6410b0fef8d8674c6da6df95bde
paolomococci <paolomococci@gmail.com>
2022-01-05 08:22:51 +0100

    edited class CartController belonging to project cart

--- Spring-Boot/cart/src/main/java/local/example/demo/controller/CartController.java
+++ Spring-Boot/cart/src/main/java/local/example/demo/controller/CartController.java
@@ -4,7 +4,12 @@ import local.example.demo.api.ApplicationApi;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 public class CartController
         implements ApplicationApi {
+
+    private static final Logger loggerFactory = LoggerFactory.getLogger(CartController.class);
 }
