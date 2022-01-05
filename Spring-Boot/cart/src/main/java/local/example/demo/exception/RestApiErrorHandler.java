ed8cff5b6149fa5ba5b4c5adb88506c3b6f70a91
paolomococci <paolomococci@gmail.com>
2022-01-05 19:59:35 +0100

    edited class RestApiErrorHandler belonging to project cart

--- Spring-Boot/cart/src/main/java/local/example/demo/exception/RestApiErrorHandler.java
+++ Spring-Boot/cart/src/main/java/local/example/demo/exception/RestApiErrorHandler.java
@@ -3,8 +3,15 @@ package local.example.demo.exception;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+import java.util.Locale;
 
 @ControllerAdvice
 public class RestApiErrorHandler {
@@ -15,5 +22,24 @@ public class RestApiErrorHandler {
 
     public RestApiErrorHandler(MessageSource messageSource) {
         this.messageSource = messageSource;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Error> handleException(
+            HttpServletRequest httpServletRequest,
+            Exception exception,
+            Locale locale
+    ) {
+        exception.printStackTrace();
+
+        Error error = ErrorUtil.createError(
+                ErrorCode.GENERIC_ERROR.getErrorMessageKey(),
+                ErrorCode.GENERIC_ERROR.getErrorCode(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value()
+        ).setUrl(
+                httpServletRequest.getRequestURL().toString()
+        ).setRequestMethod(httpServletRequest.getMethod());
+
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
