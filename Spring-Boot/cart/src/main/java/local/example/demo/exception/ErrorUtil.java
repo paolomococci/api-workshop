08733204bc832ffa069df4c72fb58f5142afa34f
paolomococci <paolomococci@gmail.com>
2022-01-05 10:54:29 +0100

    edited class ErrorUtil belonging to project cart

--- Spring-Boot/cart/src/main/java/local/example/demo/exception/ErrorUtil.java
+++ Spring-Boot/cart/src/main/java/local/example/demo/exception/ErrorUtil.java
@@ -4,4 +4,17 @@ public class ErrorUtil {
 
     public ErrorUtil() {
     }
+
+    public static Error createError(
+            final String errorMessageKey,
+            final String errorCode,
+            final Integer httpStatusCode
+    ) {
+        Error error = new Error();
+        error.setMessage(errorMessageKey);
+        error.setErrorCode(errorCode);
+        error.setStatus(httpStatusCode);
+
+        return error;
+    }
 }
