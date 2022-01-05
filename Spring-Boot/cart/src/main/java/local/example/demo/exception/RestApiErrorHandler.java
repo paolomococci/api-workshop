d328b06d442c1145ce96f3a341173d3fc2ca41b2
paolomococci <paolomococci@gmail.com>
2022-01-05 11:03:54 +0100

    edited class RestApiErrorHandler belonging to project cart

--- Spring-Boot/cart/src/main/java/local/example/demo/exception/RestApiErrorHandler.java
+++ Spring-Boot/cart/src/main/java/local/example/demo/exception/RestApiErrorHandler.java
@@ -1,4 +1,18 @@
 package local.example.demo.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.context.MessageSource;
+
+@ControllerAdvice
 public class RestApiErrorHandler {
+
+    private static final Logger loggerFactory = LoggerFactory.getLogger(RestApiErrorHandler.class);
+    private final MessageSource messageSource;
+
+    public RestApiErrorHandler(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 }
