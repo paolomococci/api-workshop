b67b63ed0b4e77ea02a4e6fd7ff22a24df9c2882
paolomococci <paolomococci@gmail.com>
2022-01-05 10:00:30 +0100

    added class Error belonging to project cart

--- Spring-Boot/cart/src/main/java/local/example/demo/exception/Error.java
+++ Spring-Boot/cart/src/main/java/local/example/demo/exception/Error.java
@@ -1,4 +1,50 @@
 package local.example.demo.exception;
 
 public class Error {
+
+    private String errorCode;
+    private String message;
+    private Integer status;
+    private String url = "Unavailable";
+    private String reqMethod = "Unavailable";
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getReqMethod() {
+        return reqMethod;
+    }
+
+    public void setReqMethod(String reqMethod) {
+        this.reqMethod = reqMethod;
+    }
 }
