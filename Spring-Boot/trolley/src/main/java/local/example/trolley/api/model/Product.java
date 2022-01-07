e9bacc12f37d933e5298df2fba97841948810b99
paolomococci <paolomococci@gmail.com>
2022-01-07 19:42:53 +0100

    edited class Product belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/model/Product.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/model/Product.java
@@ -1,4 +1,17 @@
 package local.example.trolley.api.model;
 
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    private long id;
 }
