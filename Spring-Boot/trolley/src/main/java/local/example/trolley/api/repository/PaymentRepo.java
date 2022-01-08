2bd3410e3fc9bf78a38a4f58f815c44ad4e47c36
paolomococci <paolomococci@gmail.com>
2022-01-08 17:24:16 +0100

    edited interface PaymentRepo belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/PaymentRepo.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/PaymentRepo.java
@@ -5,7 +5,9 @@ import local.example.trolley.api.model.Payment;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.UUID;
+
 @RepositoryRestResource
 public interface PaymentRepo
-        extends CrudRepository<Payment, Long> {
+        extends CrudRepository<Payment, UUID> {
 }
