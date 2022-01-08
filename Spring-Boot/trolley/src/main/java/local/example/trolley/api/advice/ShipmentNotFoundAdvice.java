1c0683b4c660b2e0e06662c54ac067ab54fbd3c8
paolomococci <paolomococci@gmail.com>
2022-01-08 05:09:15 +0100

    edited class ShipmentNotFoundAdvice belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/advice/ShipmentNotFoundAdvice.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/advice/ShipmentNotFoundAdvice.java
@@ -1,4 +1,22 @@
 package local.example.trolley.api.advice;
 
+import local.example.trolley.api.exception.ShipmentNotFoundException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
 public class ShipmentNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String shipmentNotFoundHandler(
+            ShipmentNotFoundException shipmentNotFoundException
+    ) {
+        return shipmentNotFoundException.getMessage();
+    }
 }
