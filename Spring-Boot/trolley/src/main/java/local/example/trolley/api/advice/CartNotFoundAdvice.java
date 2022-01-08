05b35a620dd71dd1b1a43b1ccc0eae18dab91e51
paolomococci <paolomococci@gmail.com>
2022-01-08 05:07:26 +0100

    edited class CartNotFoundAdvice belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/advice/CartNotFoundAdvice.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/advice/CartNotFoundAdvice.java
@@ -1,4 +1,21 @@
 package local.example.trolley.api.advice;
 
+import local.example.trolley.api.exception.CartNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
 public class CartNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String cartNotFoundHandler(
+            CartNotFoundException cartNotFoundException
+    ) {
+        return cartNotFoundException.getMessage();
+    }
 }
