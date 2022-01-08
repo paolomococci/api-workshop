36b2c4f14888ceea36410641477f3af30fab4382
paolomococci <paolomococci@gmail.com>
2022-01-08 05:08:17 +0100

    edited class UserNotFoundAdvice belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/advice/UserNotFoundAdvice.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/advice/UserNotFoundAdvice.java
@@ -1,4 +1,22 @@
 package local.example.trolley.api.advice;
 
+import local.example.trolley.api.exception.UserNotFoundException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
 public class UserNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String userNotFoundHandler(
+            UserNotFoundException userNotFoundException
+    ) {
+        return userNotFoundException.getMessage();
+    }
 }
