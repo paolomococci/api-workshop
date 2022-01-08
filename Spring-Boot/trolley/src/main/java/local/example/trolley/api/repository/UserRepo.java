97f0f666051a75bac8153de42794beef3a295544
paolomococci <paolomococci@gmail.com>
2022-01-08 10:06:22 +0100

    edited interface UserRepo belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/UserRepo.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/UserRepo.java
@@ -1,4 +1,11 @@
 package local.example.trolley.api.repository;
 
-public interface UserRepo {
+import local.example.trolley.api.model.User;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface UserRepo
+        extends CrudRepository<User, Long> {
 }
