1798eeda96d30b5fb00ca9d116bb63abbc9bee82
paolomococci <paolomococci@gmail.com>
2022-01-08 06:14:47 +0100

    edited class AddressRepresentationModelAssembler belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/assembler/AddressRepresentationModelAssembler.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/assembler/AddressRepresentationModelAssembler.java
@@ -1,4 +1,25 @@
 package local.example.trolley.api.assembler;
 
-public class AddressRepresentationModelAssembler {
+import local.example.trolley.api.model.Address;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AddressRepresentationModelAssembler
+        implements RepresentationModelAssembler<Address, EntityModel<Address>> {
+
+    @Override
+    public EntityModel<Address> toModel(Address address) {
+        return null;
+    }
+
+    @Override
+    public CollectionModel<EntityModel<Address>> toCollectionModel(
+            Iterable<? extends Address> addresses
+    ) {
+        return RepresentationModelAssembler.super.toCollectionModel(addresses);
+    }
 }
