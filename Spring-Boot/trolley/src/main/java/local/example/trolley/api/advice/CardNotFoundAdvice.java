ac6fa1cfc2539045de636ea7da21f478aba87416
paolomococci <paolomococci@gmail.com>
2022-01-08 05:07:14 +0100

    edited class CardNotFoundAdvice belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/advice/CardNotFoundAdvice.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/advice/CardNotFoundAdvice.java
@@ -1,4 +1,22 @@
 package local.example.trolley.api.advice;
 
+import local.example.trolley.api.exception.CardNotFoundException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
 public class CardNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String cardNotFoundHandler(
+            CardNotFoundException cardNotFoundException
+    ) {
+        return cardNotFoundException.getMessage();
+    }
 }
