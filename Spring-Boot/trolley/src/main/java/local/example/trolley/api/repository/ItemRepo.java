1e1e8a85b0a4cc0031661f32fa12bb04e98ee835
paolomococci <paolomococci@gmail.com>
2022-01-08 17:22:57 +0100

    edited interface ItemRepo belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/ItemRepo.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/ItemRepo.java
@@ -5,7 +5,9 @@ import local.example.trolley.api.model.Item;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.UUID;
+
 @RepositoryRestResource
 public interface ItemRepo
-        extends CrudRepository<Item, Long> {
+        extends CrudRepository<Item, UUID> {
 }
