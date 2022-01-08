ed9c27b857100464b3df8b04a35455da15482d1f
paolomococci <paolomococci@gmail.com>
2022-01-08 14:53:26 +0100

    edited interface OrderItemRepo belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/OrderItemRepo.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/OrderItemRepo.java
@@ -5,7 +5,7 @@ import local.example.trolley.api.model.OrderItem;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource(path = "correlations")
 public interface OrderItemRepo
         extends CrudRepository<OrderItem, Long> {
 }
