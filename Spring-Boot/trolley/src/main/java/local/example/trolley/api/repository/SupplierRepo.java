7334a68828cb03eb13afc89a85f06a052893f78d
paolomococci <paolomococci@gmail.com>
2022-01-08 10:14:32 +0100

    edited interface SupplierRepo belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/SupplierRepo.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/SupplierRepo.java
@@ -1,4 +1,11 @@
 package local.example.trolley.api.repository;
 
-public interface SupplierRepo {
+import local.example.trolley.api.model.Supplier;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface SupplierRepo
+        extends CrudRepository<Supplier, Long> {
 }
