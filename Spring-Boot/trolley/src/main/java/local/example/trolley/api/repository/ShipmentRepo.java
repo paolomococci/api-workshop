742ae684d96e890517dfa5b0ceb9a2330b4fe810
paolomococci <paolomococci@gmail.com>
2022-01-08 17:24:40 +0100

    edited interface ShipmentRepo belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/ShipmentRepo.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/ShipmentRepo.java
@@ -5,7 +5,9 @@ import local.example.trolley.api.model.Shipment;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.UUID;
+
 @RepositoryRestResource
 public interface ShipmentRepo
-        extends CrudRepository<Shipment, Long> {
+        extends CrudRepository<Shipment, UUID> {
 }
