8dff53fe50ea0c8ca0bab7c94a671060a1bffc70
paolomococci <paolomococci@gmail.com>
2022-01-08 17:22:24 +0100

    edited interface AuthorizationRepo belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/AuthorizationRepo.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/AuthorizationRepo.java
@@ -5,7 +5,9 @@ import local.example.trolley.api.model.Authorization;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.UUID;
+
 @RepositoryRestResource
 public interface AuthorizationRepo
-        extends CrudRepository<Authorization, Long> {
+        extends CrudRepository<Authorization, UUID> {
 }
