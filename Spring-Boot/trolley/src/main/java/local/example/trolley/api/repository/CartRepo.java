376021cdd860eef5e993c4e492a777674541189b
paolomococci <paolomococci@gmail.com>
2022-01-08 17:22:43 +0100

    edited interface CartRepo belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/CartRepo.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/CartRepo.java
@@ -5,7 +5,9 @@ import local.example.trolley.api.model.Cart;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.UUID;
+
 @RepositoryRestResource
 public interface CartRepo
-        extends CrudRepository<Cart, Long> {
+        extends CrudRepository<Cart, UUID> {
 }
