a23742ac1b4d374f99c3cdc35bb9e90d0f17eb3b
paolomococci <paolomococci@gmail.com>
2022-01-08 17:24:52 +0100

    edited interface SupplierRepo belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/SupplierRepo.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/SupplierRepo.java
@@ -5,7 +5,9 @@ import local.example.trolley.api.model.Supplier;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.UUID;
+
 @RepositoryRestResource
 public interface SupplierRepo
-        extends CrudRepository<Supplier, Long> {
+        extends CrudRepository<Supplier, UUID> {
 }
