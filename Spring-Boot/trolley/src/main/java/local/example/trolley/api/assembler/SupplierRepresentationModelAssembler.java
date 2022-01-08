6bf008fb2b44f466eb3fac9ddbb80495225b2b8e
paolomococci <paolomococci@gmail.com>
2022-01-08 06:17:35 +0100

    edited class SupplierRepresentationModelAssembler belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/assembler/SupplierRepresentationModelAssembler.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/assembler/SupplierRepresentationModelAssembler.java
@@ -1,4 +1,25 @@
 package local.example.trolley.api.assembler;
 
-public class SupplierRepresentationModelAssembler {
+import local.example.trolley.api.model.Supplier;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SupplierRepresentationModelAssembler
+        implements RepresentationModelAssembler<Supplier, EntityModel<Supplier>> {
+
+    @Override
+    public EntityModel<Supplier> toModel(Supplier supplier) {
+        return null;
+    }
+
+    @Override
+    public CollectionModel<EntityModel<Supplier>> toCollectionModel(
+            Iterable<? extends Supplier> suppliers
+    ) {
+        return RepresentationModelAssembler.super.toCollectionModel(suppliers);
+    }
 }
