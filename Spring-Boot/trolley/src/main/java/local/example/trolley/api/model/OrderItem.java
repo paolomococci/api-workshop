0309e52bb989ab1b16f633df7a34cf8dcde73201
paolomococci <paolomococci@gmail.com>
2022-01-08 09:53:34 +0100

    renamed class OrderItem belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/model/OrderItem.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/model/OrderItem.java
@@ -8,7 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class OderItem {
+public class OrderItem {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
