ae67ea6150dc93d43bf63de79f4f65a68ae987d4
paolomococci <paolomococci@gmail.com>
2022-01-08 10:05:14 +0100

    edited interface CardRepo belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/CardRepo.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/CardRepo.java
@@ -1,4 +1,11 @@
 package local.example.trolley.api.repository;
 
-public interface CardRepo {
+import local.example.trolley.api.model.Card;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CardRepo
+        extends CrudRepository<Card, Long> {
 }
