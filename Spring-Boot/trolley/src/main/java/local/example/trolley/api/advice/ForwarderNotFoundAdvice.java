3470327903546b5fdd595b2589a5871891477c90
paolomococci <paolomococci@gmail.com>
2022-01-08 05:08:42 +0100

    edited class ForwarderNotFoundAdvice belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/advice/ForwarderNotFoundAdvice.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/advice/ForwarderNotFoundAdvice.java
@@ -1,4 +1,22 @@
 package local.example.trolley.api.advice;
 
+import local.example.trolley.api.exception.ForwarderNotFoundException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
 public class ForwarderNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String forwarderNotFoundHandler(
+            ForwarderNotFoundException forwarderNotFoundException
+    ) {
+        return forwarderNotFoundException.getMessage();
+    }
 }
