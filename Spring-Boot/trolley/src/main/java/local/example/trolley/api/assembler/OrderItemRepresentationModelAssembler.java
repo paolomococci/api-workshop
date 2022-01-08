0e521733548b68a8dda108c122633c9a8a826596
paolomococci <paolomococci@gmail.com>
2022-01-08 06:15:45 +0100

    edited class OrderItemRepresentationModelAssembler belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/assembler/OrderItemRepresentationModelAssembler.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/assembler/OrderItemRepresentationModelAssembler.java
@@ -1,4 +1,24 @@
 package local.example.trolley.api.assembler;
 
-public class OrderItemRepresentationModelAssembler {
+import local.example.trolley.api.model.OderItem;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderItemRepresentationModelAssembler
+        implements RepresentationModelAssembler<OderItem, EntityModel<OderItem>> {
+    @Override
+    public EntityModel<OderItem> toModel(OderItem correlation) {
+        return null;
+    }
+
+    @Override
+    public CollectionModel<EntityModel<OderItem>> toCollectionModel(
+            Iterable<? extends OderItem> correlations
+    ) {
+        return RepresentationModelAssembler.super.toCollectionModel(correlations);
+    }
 }
