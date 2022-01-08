a05430845283c74073381c42349cdccf1a74263b
paolomococci <paolomococci@gmail.com>
2022-01-08 17:23:15 +0100

    edited interface OrderRepo belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/OrderRepo.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/OrderRepo.java
@@ -5,7 +5,9 @@ import local.example.trolley.api.model.Order;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.UUID;
+
 @RepositoryRestResource
 public interface OrderRepo
-        extends CrudRepository<Order, Long> {
+        extends CrudRepository<Order, UUID> {
 }
