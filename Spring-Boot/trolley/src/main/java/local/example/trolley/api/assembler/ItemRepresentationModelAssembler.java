e9046cd4b25287f69755fe40007d6c7942553597
paolomococci <paolomococci@gmail.com>
2022-01-08 06:15:35 +0100

    edited class ItemRepresentationModelAssembler belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/assembler/ItemRepresentationModelAssembler.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/assembler/ItemRepresentationModelAssembler.java
@@ -1,4 +1,24 @@
 package local.example.trolley.api.assembler;
 
-public class ItemRepresentationModelAssembler {
+import local.example.trolley.api.model.Item;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ItemRepresentationModelAssembler
+        implements RepresentationModelAssembler<Item, EntityModel<Item>> {
+    @Override
+    public EntityModel<Item> toModel(Item item) {
+        return null;
+    }
+
+    @Override
+    public CollectionModel<EntityModel<Item>> toCollectionModel(
+            Iterable<? extends Item> items
+    ) {
+        return RepresentationModelAssembler.super.toCollectionModel(items);
+    }
 }
