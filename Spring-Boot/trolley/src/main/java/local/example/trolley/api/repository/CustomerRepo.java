9bef32730b246574bff3ffda2e9fde5e597f5c2d
paolomococci <paolomococci@gmail.com>
2022-01-08 17:23:55 +0100

    edited interface CustomerRepo belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/CustomerRepo.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/CustomerRepo.java
@@ -5,7 +5,9 @@ import local.example.trolley.api.model.Customer;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.UUID;
+
 @RepositoryRestResource
 public interface CustomerRepo
-        extends CrudRepository<Customer, Long> {
+        extends CrudRepository<Customer, UUID> {
 }
