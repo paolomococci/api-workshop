46f59d40721cb66beedf1999b8b2b69a9f4a71a0
paolomococci <paolomococci@gmail.com>
2022-01-08 17:24:28 +0100

    edited interface ProductRepo belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/ProductRepo.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/ProductRepo.java
@@ -5,7 +5,9 @@ import local.example.trolley.api.model.Product;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.UUID;
+
 @RepositoryRestResource
 public interface ProductRepo
-        extends CrudRepository<Product, Long> {
+        extends CrudRepository<Product, UUID> {
 }
