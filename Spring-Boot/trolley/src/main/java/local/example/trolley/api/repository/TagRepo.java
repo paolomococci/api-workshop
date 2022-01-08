0a79bca2431cdc584ba3557a68d71add26848bff
paolomococci <paolomococci@gmail.com>
2022-01-08 17:23:24 +0100

    edited interface TagRepo belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/TagRepo.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/TagRepo.java
@@ -5,7 +5,9 @@ import local.example.trolley.api.model.Tag;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.UUID;
+
 @RepositoryRestResource
 public interface TagRepo
-        extends CrudRepository<Tag, Long> {
+        extends CrudRepository<Tag, UUID> {
 }
