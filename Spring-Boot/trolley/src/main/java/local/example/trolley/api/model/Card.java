c3f4f4d28e25f50dd0faefe3ab97f95ef1099498
paolomococci <paolomococci@gmail.com>
2022-01-09 12:31:51 +0100

    edited class Card belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/model/Card.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/model/Card.java
@@ -2,14 +2,19 @@ package local.example.trolley.api.model;
 
 import local.example.trolley.api.template.EntityTemplate;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 import java.io.Serializable;
 
+@Data
 @Entity
 @Table(name = "CARDS")
+@EqualsAndHashCode(callSuper = true)
 public class Card
         extends EntityTemplate
         implements Serializable {
