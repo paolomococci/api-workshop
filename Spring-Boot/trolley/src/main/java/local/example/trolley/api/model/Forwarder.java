f17c613a3ed7a854dd1aed1c0cdcc4bb51453f30
paolomococci <paolomococci@gmail.com>
2022-01-09 12:32:21 +0100

    edited class Forwarder belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/model/Forwarder.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/model/Forwarder.java
@@ -1,6 +1,10 @@
 package local.example.trolley.api.model;
 
 import local.example.trolley.api.template.EntityTemplate;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.Column;
@@ -8,8 +12,10 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.io.Serializable;
 
+@Data
 @Entity
 @Table(name = "FORWARDERS")
+@EqualsAndHashCode(callSuper = true)
 public class Forwarder
         extends EntityTemplate
         implements Serializable {
