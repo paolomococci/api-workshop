54d793aa27b2c489579659b36ded8e2bd45ff9ae
paolomococci <paolomococci@gmail.com>
2022-01-09 12:24:19 +0100

    edited class OrderItem belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/model/OrderItem.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/model/OrderItem.java
@@ -1,43 +1,15 @@
 package local.example.trolley.api.model;
 
-import lombok.Getter;
+import local.example.trolley.api.template.EntityTemplate;
 
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Type;
-import org.hibernate.annotations.UpdateTimestamp;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
-import javax.persistence.*;
-
-import java.sql.Timestamp;
-import java.util.UUID;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "CORRELATIONS")
-public class OrderItem {
-
-    @Id
-    @GeneratedValue(generator = "UUID")
-    @GenericGenerator(
-            name = "UUID",
-            strategy = "org.hibernate.id.UUIDGenerator"
-    )
-    @Column(
-            name = "ID",
-            updatable = false,
-            nullable = false
-    )
-    @Type(type = "uuid-char")
-    @Getter
-    private UUID id;
-
-    @CreationTimestamp
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "REGISTERED")
-    private Timestamp registered;
-
-    @UpdateTimestamp
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "UPDATED")
-    private Timestamp updated;
+public class OrderItem
+        extends EntityTemplate
+        implements Serializable {
 }
