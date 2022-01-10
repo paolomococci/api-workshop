9f121d25c4ecf0a9b7f03404c89b863e01c838c7
paolomococci <paolomococci@gmail.com>
2022-01-10 20:49:09 +0100

    edited class Customer belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/model/Customer.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/model/Customer.java
@@ -7,9 +7,11 @@ import lombok.EqualsAndHashCode;
 
 import org.hibernate.annotations.ColumnDefault;
 
+import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import java.io.Serializable;
 
@@ -20,6 +22,21 @@ import java.io.Serializable;
 public class Customer
         extends EntityTemplate
         implements Serializable {
+
+    @Column(name = "NAME")
+    @Basic(optional = false)
+    @NotNull(message = "the name field cannot be null")
+    private String name;
+
+    @Column(name = "SURNAME")
+    @Basic(optional = false)
+    @NotNull(message = "the surname field cannot be null")
+    private String surname;
+
+    @Column(name = "PHONE")
+    @Basic(optional = false)
+    @NotNull(message = "the phone field cannot be null")
+    private String phone;
 
     @Column(name = "ACTIVE")
     @ColumnDefault(value = "false")
