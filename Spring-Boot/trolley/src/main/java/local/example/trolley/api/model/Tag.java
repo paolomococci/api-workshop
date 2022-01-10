6579e44c2400801d12bb73704202464b55e39e3c
paolomococci <paolomococci@gmail.com>
2022-01-10 20:43:14 +0100

    edited class Tag belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/model/Tag.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/model/Tag.java
@@ -2,8 +2,11 @@ package local.example.trolley.api.model;
 
 import local.example.trolley.api.template.EntityTemplate;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import java.io.Serializable;
 
@@ -12,4 +15,9 @@ import java.io.Serializable;
 public class Tag
         extends EntityTemplate
         implements Serializable {
+
+    @Column(name = "NAME")
+    @Basic(optional = false)
+    @NotNull(message = "the name field cannot be null")
+    private String name;
 }
