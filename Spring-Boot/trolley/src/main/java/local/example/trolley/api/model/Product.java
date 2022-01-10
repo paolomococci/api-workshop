86a7b8a51e14793f88c1e4c5dd5d2a53c0cdc256
paolomococci <paolomococci@gmail.com>
2022-01-10 20:35:29 +0100

    edited class Product belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/model/Product.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/model/Product.java
@@ -12,6 +12,7 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 
 @Data
 @Entity
@@ -20,6 +21,21 @@ import java.io.Serializable;
 public class Product
         extends EntityTemplate
         implements Serializable {
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    @Column(name = "PRICE")
+    private BigDecimal price;
+
+    @Column(name = "STOCK")
+    private String stock;
+
+    @Column(name = "IMAGE")
+    private String imageUri;
 
     @Column(name = "AVAILABLE")
     @ColumnDefault(value = "false")
