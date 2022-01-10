ebce8cda361dd737fe23a708021ea12006bd3dde
paolomococci <paolomococci@gmail.com>
2022-01-10 20:47:25 +0100

    edited class User belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/model/User.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/model/User.java
@@ -7,9 +7,11 @@ import lombok.EqualsAndHashCode;
 
 import org.hibernate.annotations.ColumnDefault;
 
+import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import java.io.Serializable;
 
@@ -20,6 +22,21 @@ import java.io.Serializable;
 public class User
         extends EntityTemplate
         implements Serializable {
+
+    @Column(name = "USERNAME")
+    @Basic(optional = false)
+    @NotNull(message = "the username field cannot be null")
+    private String username;
+
+    @Column(name = "PASSWORD")
+    @Basic(optional = false)
+    @NotNull(message = "the password field cannot be null")
+    private String password;
+
+    @Column(name = "EMAIL")
+    @Basic(optional = false)
+    @NotNull(message = "the email field cannot be null")
+    private String email;
 
     @Column(name = "ACTIVE")
     @ColumnDefault(value = "false")
