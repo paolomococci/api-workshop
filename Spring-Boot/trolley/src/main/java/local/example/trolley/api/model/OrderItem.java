5e7eaab613dfcd0d2ec563795510cfdc2887b662
paolomococci <paolomococci@gmail.com>
2022-01-10 20:26:11 +0100

    edited class OrderItem belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/model/OrderItem.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/model/OrderItem.java
@@ -2,14 +2,22 @@ package local.example.trolley.api.model;
 
 import local.example.trolley.api.template.EntityTemplate;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 @Entity
 @Table(name = "CORRELATIONS")
 public class OrderItem
         extends EntityTemplate
         implements Serializable {
+
+    @Column(name = "ITEM")
+    private UUID itemId;
+
+    @Column(name = "ORDER")
+    private UUID orderId;
 }
