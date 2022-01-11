0e6166460f3b46ed9fb4b0b0171fe219db299919
paolomococci <paolomococci@gmail.com>
2022-01-11 02:37:36 +0100

    edited class Supplier belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/model/Supplier.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/model/Supplier.java
@@ -7,9 +7,11 @@ import lombok.EqualsAndHashCode;
 
 import org.hibernate.annotations.ColumnDefault;
 
+import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import java.io.Serializable;
 
@@ -20,6 +22,21 @@ import java.io.Serializable;
 public class Supplier
         extends EntityTemplate
         implements Serializable {
+
+    @Column(name = "NAME")
+    @Basic(optional = false)
+    @NotNull(message = "the name field cannot be null")
+    private String name;
+
+    @Column(name = "EMAIL")
+    @Basic(optional = false)
+    @NotNull(message = "the email field cannot be null")
+    private String email;
+
+    @Column(name = "PHONE")
+    @Basic(optional = false)
+    @NotNull(message = "the phone field cannot be null")
+    private String phone;
 
     @Column(name = "ACTIVE")
     @ColumnDefault(value = "false")
