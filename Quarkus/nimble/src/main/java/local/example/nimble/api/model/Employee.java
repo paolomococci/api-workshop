4f9bd056acbf8716b97a2a858b09beabb47bf5d0
paolomococci <paolomococci@gmail.com>
2022-01-13 04:26:39 +0100

    edited class Employee belonging to project nimble

--- Quarkus/nimble/src/main/java/local/example/nimble/api/model/Employee.java
+++ Quarkus/nimble/src/main/java/local/example/nimble/api/model/Employee.java
@@ -1,4 +1,16 @@
 package local.example.nimble.api.model;
 
-public class Employee {
+import io.quarkus.hibernate.reactive.panache.PanacheEntity;
+
+import javax.persistence.Entity;
+import java.time.LocalDate;
+
+@Entity
+public class Employee
+        extends PanacheEntity {
+
+    private String name;
+    private String surname;
+    private LocalDate birthday;
+    private LocalDate recruited;
 }
