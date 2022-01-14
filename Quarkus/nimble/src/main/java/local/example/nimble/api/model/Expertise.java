3a100343cb24d5af3fc438a88349328dcd78e970
paolomococci <paolomococci@gmail.com>
2022-01-15 00:23:49 +0100

    edited class Expertise belonging to project nimble

--- Quarkus/nimble/src/main/java/local/example/nimble/api/model/Expertise.java
+++ Quarkus/nimble/src/main/java/local/example/nimble/api/model/Expertise.java
@@ -3,8 +3,42 @@ package local.example.nimble.api.model;
 import io.quarkus.hibernate.reactive.panache.PanacheEntity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Expertise
         extends PanacheEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
