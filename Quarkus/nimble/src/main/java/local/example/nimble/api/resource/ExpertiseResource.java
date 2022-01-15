3ef541c5f2672f35cfa15225e10d5fba0845aac3
paolomococci <paolomococci@gmail.com>
2022-01-15 21:06:54 +0100

    edited class ExpertiseResource belonging to project nimble

--- Quarkus/nimble/src/main/java/local/example/nimble/api/resource/ExpertiseResource.java
+++ Quarkus/nimble/src/main/java/local/example/nimble/api/resource/ExpertiseResource.java
@@ -1,12 +1,20 @@
 package local.example.nimble.api.resource;
 
+import io.quarkus.panache.common.Sort;
+import io.smallrye.mutiny.Uni;
+
+import local.example.nimble.api.model.Expertise;
+
 import org.jboss.resteasy.reactive.ResponseStatus;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
 import java.net.HttpURLConnection;
+import java.util.List;
 
 @Path("/api/v1/expertise")
 public class ExpertiseResource {
@@ -17,5 +25,13 @@ public class ExpertiseResource {
     @ResponseStatus(HttpURLConnection.HTTP_OK)
     public String probe() {
         return "expertise";
+    }
+
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ResponseStatus(HttpURLConnection.HTTP_OK)
+    public Uni<List<Expertise>> readAll(){
+        return Expertise.listAll(Sort.by("name"));
     }
 }
