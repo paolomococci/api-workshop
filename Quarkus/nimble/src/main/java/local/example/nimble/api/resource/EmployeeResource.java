2717c16dc90ffa612e789a3850e64dc5fd37b964
paolomococci <paolomococci@gmail.com>
2022-01-15 21:07:08 +0100

    edited class EmployeeResource belonging to project nimble

--- Quarkus/nimble/src/main/java/local/example/nimble/api/resource/EmployeeResource.java
+++ Quarkus/nimble/src/main/java/local/example/nimble/api/resource/EmployeeResource.java
@@ -1,12 +1,20 @@
 package local.example.nimble.api.resource;
 
+import io.quarkus.panache.common.Sort;
+import io.smallrye.mutiny.Uni;
+
+import local.example.nimble.api.model.Employee;
+
 import org.jboss.resteasy.reactive.ResponseStatus;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
 import java.net.HttpURLConnection;
+import java.util.List;
 
 @Path("/api/v1/employees")
 public class EmployeeResource {
@@ -17,5 +25,13 @@ public class EmployeeResource {
     @ResponseStatus(HttpURLConnection.HTTP_OK)
     public String probe() {
         return "employees";
+    }
+
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ResponseStatus(HttpURLConnection.HTTP_OK)
+    public Uni<List<Employee>> readAll(){
+        return Employee.listAll(Sort.by("surname"));
     }
 }
