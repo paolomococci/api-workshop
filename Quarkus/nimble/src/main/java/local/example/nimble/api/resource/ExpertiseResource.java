91b35334759134c4984db84cd60f5f16cc6eca71
paolomococci <paolomococci@gmail.com>
2022-01-15 22:41:11 +0100

    edited class ExpertiseResource belonging to project nimble

--- Quarkus/nimble/src/main/java/local/example/nimble/api/resource/ExpertiseResource.java
+++ Quarkus/nimble/src/main/java/local/example/nimble/api/resource/ExpertiseResource.java
@@ -54,4 +54,28 @@ public class ExpertiseResource {
     public Uni<Expertise> read(@RestPath Long id) {
         return Expertise.findById(id);
     }
+
+    @PUT
+    @Path(value = "{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Uni<Response> update(
+            @RestPath Long id,
+            Expertise expertise
+    ) {
+        if (expertise == null || expertise.id != null)
+            throw new WebApplicationException("Unprocessable Entity", 422);
+
+        return Panache.withTransaction(
+                () -> Expertise.<Expertise> findById(id)
+                        .onItem().ifNotNull().invoke(
+                                temp -> {
+                                    temp.setName(expertise.getName());
+                                    temp.setDescription(expertise.getDescription());
+                                }
+                        )
+                        .onItem().ifNotNull().transform(temp -> Response.ok(temp).build())
+                        .onItem().ifNull().continueWith(Response.noContent().status(Response.Status.NOT_FOUND)::build)
+        );
+    }
 }
