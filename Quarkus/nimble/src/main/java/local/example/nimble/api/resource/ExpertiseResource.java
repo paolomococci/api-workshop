3b748bbb87a7c95e304615616965212ba8fd3d57
paolomococci <paolomococci@gmail.com>
2022-01-15 20:08:16 +0100

    edited class ExpertiseResource belonging to project nimble

--- Quarkus/nimble/src/main/java/local/example/nimble/api/resource/ExpertiseResource.java
+++ Quarkus/nimble/src/main/java/local/example/nimble/api/resource/ExpertiseResource.java
@@ -1,15 +1,19 @@
 package local.example.nimble.api.resource;
 
+import org.jboss.resteasy.reactive.ResponseStatus;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.net.HttpURLConnection;
 
 @Path("/api/v1/expertise")
 public class ExpertiseResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
+    @ResponseStatus(HttpURLConnection.HTTP_OK)
     public String probe() {
         return "expertise";
     }
