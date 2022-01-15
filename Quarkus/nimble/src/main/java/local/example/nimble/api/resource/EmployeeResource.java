6742f6b251f631e4d614f4860a81cdb4a5c1dbb5
paolomococci <paolomococci@gmail.com>
2022-01-15 21:46:37 +0100

    edited class EmployeeResource belonging to project nimble

--- Quarkus/nimble/src/main/java/local/example/nimble/api/resource/EmployeeResource.java
+++ Quarkus/nimble/src/main/java/local/example/nimble/api/resource/EmployeeResource.java
@@ -6,6 +6,7 @@ import io.smallrye.mutiny.Uni;
 import local.example.nimble.api.model.Employee;
 
 import org.jboss.resteasy.reactive.ResponseStatus;
+import org.jboss.resteasy.reactive.RestPath;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -33,5 +34,14 @@ public class EmployeeResource {
     @ResponseStatus(HttpURLConnection.HTTP_OK)
     public Uni<List<Employee>> readAll(){
         return Employee.listAll(Sort.by("surname"));
+    }
+
+    @GET
+    @Path(value = "{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @ResponseStatus(HttpURLConnection.HTTP_OK)
+    public Uni<Employee> read(@RestPath Long id) {
+        return Employee.findById(id);
     }
 }
