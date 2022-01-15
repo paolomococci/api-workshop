f84d08c7a2de1317f74819b8ddbbb4f1f8c15435
paolomococci <paolomococci@gmail.com>
2022-01-15 07:45:56 +0100

    edited class ExpertiseResource belonging to project nimble

--- Quarkus/nimble/src/main/java/local/example/nimble/api/resource/ExpertiseResource.java
+++ Quarkus/nimble/src/main/java/local/example/nimble/api/resource/ExpertiseResource.java
@@ -1,4 +1,16 @@
 package local.example.nimble.api.resource;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/api/v1/expertise")
 public class ExpertiseResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String probe() {
+        return "expertise";
+    }
 }
