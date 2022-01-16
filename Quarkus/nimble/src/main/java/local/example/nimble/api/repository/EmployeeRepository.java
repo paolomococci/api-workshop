b192515508ccfe09dadcc3556568dfad9592d1ce
paolomococci <paolomococci@gmail.com>
2022-01-16 19:18:11 +0100

    edited class EmployeeRepository belonging to project nimble

--- Quarkus/nimble/src/main/java/local/example/nimble/api/repository/EmployeeRepository.java
+++ Quarkus/nimble/src/main/java/local/example/nimble/api/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package local.example.nimble.api.repository;
 
 import io.quarkus.hibernate.reactive.panache.PanacheRepository;
 
+import io.smallrye.mutiny.Uni;
+
 import local.example.nimble.api.model.Employee;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -9,4 +11,16 @@ import javax.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class EmployeeRepository
         implements PanacheRepository<Employee> {
+
+    public Uni<Employee> findByName(String name) {
+        return find("name", name).firstResult();
+    }
+
+    public Uni<Employee> findBySurname(String surname) {
+        return find("surname", surname).firstResult();
+    }
+
+    public Uni<Employee> findByUsername(String username) {
+        return find("username", username).firstResult();
+    }
 }
