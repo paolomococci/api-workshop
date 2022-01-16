737cff43c5acd71de66fc9dd66fdbe509e992923
paolomococci <paolomococci@gmail.com>
2022-01-16 19:49:02 +0100

    edited class EmployeeService belonging to project nimble

--- Quarkus/nimble/src/main/java/local/example/nimble/api/service/EmployeeService.java
+++ Quarkus/nimble/src/main/java/local/example/nimble/api/service/EmployeeService.java
@@ -1,7 +1,19 @@
 package local.example.nimble.api.service;
 
+import io.smallrye.mutiny.Uni;
+import io.vertx.mutiny.sqlclient.Row;
+import io.vertx.mutiny.sqlclient.RowSet;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 @ApplicationScoped
 public class EmployeeService {
+
+    @Inject
+    io.vertx.mutiny.pgclient.PgPool client;
+
+    public Uni<RowSet<Row>> listAll() {
+        return client.query("SELECT ID, NAME FROM EMPLOYEE ORDER BY NAME ASC").execute();
+    }
 }
