33d58dd3a1dab507e8f5f4c97ba804e4f7480419
paolomococci <paolomococci@gmail.com>
2022-01-16 20:47:28 +0100

    edited class ExpertiseResource belonging to project nimble

--- Quarkus/nimble/src/main/java/local/example/nimble/api/resource/ExpertiseResource.java
+++ Quarkus/nimble/src/main/java/local/example/nimble/api/resource/ExpertiseResource.java
@@ -1,13 +1,14 @@
 package local.example.nimble.api.resource;
 
 import io.quarkus.hibernate.reactive.panache.Panache;
-import io.quarkus.panache.common.Sort;
 import io.quarkus.vertx.web.Route;
+import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 
 import local.example.nimble.api.model.Expertise;
 
-import org.hibernate.reactive.mutiny.Mutiny;
+import local.example.nimble.api.service.ExpertiseService;
+
 import org.jboss.resteasy.reactive.RestPath;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -16,14 +17,12 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import java.util.List;
-
 @ApplicationScoped
 @Path("/api/v1/expertise")
 public class ExpertiseResource {
 
     @Inject
-    Mutiny.SessionFactory sessionFactory;
+    ExpertiseService expertiseService;
 
     @Route(
             path = "/api/v1/expertise/probe",
@@ -50,21 +49,14 @@ public class ExpertiseResource {
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Uni<Expertise> read(Long id) {
-        return sessionFactory.withTransaction(
-                (session, transaction) -> session.find(Expertise.class, id)
-        );
+    public Uni<Response> read(Long id) {
+        return null;
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Uni<List<Expertise>> readAll() {
-        return sessionFactory.withTransaction(
-                (session, transaction) -> session.createNamedQuery(
-                        "Expertise.findAll",
-                        Expertise.class
-                ).getResultList()
-        );
+    public Multi<Response> readAll() {
+        return null;
     }
 
     @PUT
