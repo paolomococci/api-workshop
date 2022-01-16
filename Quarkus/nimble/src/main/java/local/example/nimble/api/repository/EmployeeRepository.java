fe5444b7a90bdd00d11498ae21181cdc4c79d7f3
paolomococci <paolomococci@gmail.com>
2022-01-16 19:11:29 +0100

    edited class EmployeeRepository belonging to project nimble

--- Quarkus/nimble/src/main/java/local/example/nimble/api/repository/EmployeeRepository.java
+++ Quarkus/nimble/src/main/java/local/example/nimble/api/repository/EmployeeRepository.java
@@ -1,4 +1,12 @@
 package local.example.nimble.api.repository;
 
-public class EmployeeRepository {
+import io.quarkus.hibernate.reactive.panache.PanacheRepository;
+
+import local.example.nimble.api.model.Employee;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class EmployeeRepository
+        implements PanacheRepository<Employee> {
 }
