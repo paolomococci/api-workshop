96e8b7f83e545fba8266c7385d213d2322d70f9b
paolomococci <paolomococci@gmail.com>
2022-01-16 19:11:39 +0100

    edited class ExpertiseRepository belonging to project nimble

--- Quarkus/nimble/src/main/java/local/example/nimble/api/repository/ExpertiseRepository.java
+++ Quarkus/nimble/src/main/java/local/example/nimble/api/repository/ExpertiseRepository.java
@@ -1,4 +1,12 @@
 package local.example.nimble.api.repository;
 
-public class ExpertiseRepository {
+import io.quarkus.hibernate.reactive.panache.PanacheRepository;
+
+import local.example.nimble.api.model.Expertise;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class ExpertiseRepository
+        implements PanacheRepository<Expertise> {
 }
