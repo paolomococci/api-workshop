0ad6e8188c7c7cf17e261c21bc4ca72753d9b412
paolomococci <paolomococci@gmail.com>
2022-01-17 18:59:48 +0100

    edited interface ItemRepo belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/ItemRepo.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/ItemRepo.java
@@ -11,6 +11,6 @@ import java.util.UUID;
 public interface ItemRepo
         extends CrudRepository<Item, UUID> {
 
-    String QUERY = "";
-    String DELETE_QUERY = "";
+    String SELECT_QUERY = "SELECT i.* FROM trolley_db.carts c, trolley_db.items i, trolley_db.users u, trolley_db.cart_item ci WHERE u.id = :customerId AND c.user_id = u.id AND c.id = ci.cart_id AND i.id = ci.item_id";
+    String DELETE_QUERY = "DELETE FROM trolley_db.cart_item WHERE item_id IN (:uuidList) AND cart_id = :cartId";
 }
