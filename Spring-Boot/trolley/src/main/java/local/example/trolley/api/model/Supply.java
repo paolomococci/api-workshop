8e826f1ec8a4bf8858b1706f0a345bf1d6703f88
paolomococci <paolomococci@gmail.com>
2022-01-17 07:33:40 +0100

    edited class Supply belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/model/Supply.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/model/Supply.java
@@ -1,4 +1,20 @@
 package local.example.trolley.api.model;
 
-public class Supply {
+import local.example.trolley.api.template.EntityTemplate;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "SUPPLIES")
+@EqualsAndHashCode(callSuper = true)
+public class Supply
+        extends EntityTemplate
+        implements Serializable {
 }
