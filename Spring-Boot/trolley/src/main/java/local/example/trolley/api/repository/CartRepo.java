a04c335e535424e46da1877995ad01e43b24a1e8
paolomococci <paolomococci@gmail.com>
2022-01-18 14:48:54 +0100

    edited interface CartRepo belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/CartRepo.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/CartRepo.java
@@ -2,9 +2,12 @@ package local.example.trolley.api.repository;
 
 import local.example.trolley.api.model.Cart;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @RepositoryRestResource
@@ -12,4 +15,10 @@ public interface CartRepo
         extends CrudRepository<Cart, UUID> {
 
     String SELECT_QUERY = "SELECT c FROM carts c JOIN c.user_id WHERE c.id = :customerId";
+
+    @Query(
+            value = SELECT_QUERY,
+            nativeQuery = true
+    )
+    Optional<Cart> findByCustomerId(@Param("customerId") UUID customerId);
 }
