7ba79bd0918779b901501a8ff343f962ef922658
paolomococci <paolomococci@gmail.com>
2022-01-18 14:52:55 +0100

    edited class OrderRepoImpl belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/impl/OrderRepoImpl.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/impl/OrderRepoImpl.java
@@ -1,12 +1,23 @@
 package local.example.trolley.api.repository.impl;
 
+import local.example.trolley.api.model.Order;
+import local.example.trolley.api.repository.ext.OrderRepoExt;
+
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 
+import java.util.Optional;
+
 @Repository
 @Transactional
-public class OrderRepoImpl {
+public class OrderRepoImpl
+        implements OrderRepoExt {
 
     private static final String SELECT_QUERY = "SELECT o.* FROM trolley_db.orders WHERE o.customer_id = ? AND o.registered >= ?";
+
+    @Override
+    public Optional<Order> insert(Order order) {
+        return Optional.empty();
+    }
 }
