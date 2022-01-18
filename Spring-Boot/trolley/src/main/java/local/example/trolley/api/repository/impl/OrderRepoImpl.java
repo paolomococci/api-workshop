dce2cd8f13c9190bfcda91e1237178e2a79b3ec2
paolomococci <paolomococci@gmail.com>
2022-01-18 15:17:23 +0100

    edited interface OrderRepoImpl belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/impl/OrderRepoImpl.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/impl/OrderRepoImpl.java
@@ -1,10 +1,13 @@
 package local.example.trolley.api.repository.impl;
 
 import local.example.trolley.api.model.Order;
+import local.example.trolley.api.repository.ItemRepo;
 import local.example.trolley.api.repository.ext.OrderRepoExt;
 
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 import java.util.Optional;
@@ -15,6 +18,11 @@ public class OrderRepoImpl
         implements OrderRepoExt {
 
     private static final String SELECT_QUERY = "SELECT o.* FROM trolley_db.orders WHERE o.customer_id = ? AND o.registered >= ?";
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private ItemRepo itemRepo;
 
     @Override
     public Optional<Order> insert(Order order) {
