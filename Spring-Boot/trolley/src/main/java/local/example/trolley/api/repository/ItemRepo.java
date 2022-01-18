efac8a4dcd513298b5ee32c9e63217c3356ee1d2
paolomococci <paolomococci@gmail.com>
2022-01-18 14:57:35 +0100

    edited interface ItemRepo belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/ItemRepo.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/ItemRepo.java
@@ -2,7 +2,9 @@ package local.example.trolley.api.repository;
 
 import local.example.trolley.api.model.Item;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.UUID;
@@ -13,4 +15,10 @@ public interface ItemRepo
 
     String SELECT_QUERY = "SELECT i.* FROM trolley_db.carts c, trolley_db.items i, trolley_db.users u, trolley_db.cart_item ci WHERE u.id = :customerId AND c.user_id = u.id AND c.id = ci.cart_id AND i.id = ci.item_id";
     String DELETE_QUERY = "DELETE FROM trolley_db.cart_item WHERE item_id IN (:uuidList) AND cart_id = :cartId";
+
+    @Query(
+            value = SELECT_QUERY,
+            nativeQuery = true
+    )
+    Iterable<Item> findByCustomerId(@Param("customerId") UUID customerId);
 }
