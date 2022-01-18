aa8658b45c1b115522afd670ed369f619c08ac4a
paolomococci <paolomococci@gmail.com>
2022-01-18 15:03:52 +0100

    edited interface OrderRepo belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/OrderRepo.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/repository/OrderRepo.java
@@ -2,7 +2,9 @@ package local.example.trolley.api.repository;
 
 import local.example.trolley.api.model.Order;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.UUID;
@@ -12,4 +14,10 @@ public interface OrderRepo
         extends CrudRepository<Order, UUID> {
 
     String SELECT_QUERY = "SELECT o FROM orders o JOIN o.customer_id u WHERE u.id = :customerId";
+
+    @Query(
+            value = SELECT_QUERY,
+            nativeQuery = true
+    )
+    Iterable<Order> findByCustomerId(@Param("customerId") UUID customerId);
 }
