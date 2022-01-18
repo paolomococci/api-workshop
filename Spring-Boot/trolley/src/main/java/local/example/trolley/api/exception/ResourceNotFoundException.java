04d37834f60da05f13daf908685b5e7337af2224
paolomococci <paolomococci@gmail.com>
2022-01-18 16:24:08 +0100

    edited class ResourceNotFoundException belonging to project trolley

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/ResourceNotFoundException.java
+++ Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/ResourceNotFoundException.java
@@ -1,9 +1,22 @@
 package local.example.trolley.api.exception;
 
+import local.example.trolley.api.error.ErrorCode;
+
 public class ResourceNotFoundException
         extends RuntimeException {
 
-    public ResourceNotFoundException(String id) {
-        super("could not find resource with id: " + id);
+    private final String errorMessageKye;
+    private final String errorCode;
+
+    public ResourceNotFoundException(ErrorCode errorCode) {
+        super(errorCode.getErrorMessageKey());
+        this.errorMessageKye = errorCode.getErrorMessageKey();
+        this.errorCode = errorCode.getErrorCode();
+    }
+
+    public ResourceNotFoundException(final String message) {
+        super(message);
+        this.errorMessageKye = ErrorCode.RESOURCE_NOT_FOUND.getErrorMessageKey();
+        this.errorCode = ErrorCode.RESOURCE_NOT_FOUND.getErrorCode();
     }
 }
