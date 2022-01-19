7d84b2dfdd0da6af478400e1a753e5dcd1697e0f
paolomococci <paolomococci@gmail.com>
2022-01-19 12:18:56 +0100

    edited entity class Employee belonging to project nimble

--- Quarkus/nimble/src/main/java/local/example/nimble/api/model/Employee.java
+++ Quarkus/nimble/src/main/java/local/example/nimble/api/model/Employee.java
@@ -41,62 +41,6 @@ public class Employee
     @Enumerated(EnumType.STRING)
     private Level level;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username.toLowerCase();
-    }
-
-    public LocalDate getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(LocalDate birthday) {
-        this.birthday = birthday;
-    }
-
-    public LocalDate getRecruited() {
-        return recruited;
-    }
-
-    public void setRecruited(LocalDate recruited) {
-        this.recruited = recruited;
-    }
-
-    public List<Expertise> getExpertise() {
-        return expertise;
-    }
-
-    public void setExpertise(List<Expertise> expertise) {
-        this.expertise = expertise;
-    }
-
-    public Level getLevel() {
-        return level;
-    }
-
-    public void setLevel(Level level) {
-        this.level = level;
-    }
-
     public static Uni<List<Employee>> findByName(String name) {
         return list("name", name);
     }
