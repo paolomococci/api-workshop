30a05e874ca87ec8a46bb16af0db9ecbeb0aa2b5
paolomococci <paolomococci@gmail.com>
2022-01-23 18:35:28 +0100

    edited class Impatiens belonging to project impatiens

--- Quarkus/impatiens/src/main/java/local/example/impatiens/entity/Impatiens.java
+++ Quarkus/impatiens/src/main/java/local/example/impatiens/entity/Impatiens.java
@@ -4,7 +4,14 @@ import io.quarkus.hibernate.reactive.panache.PanacheEntity;
 
 import javax.persistence.Entity;
 
+import java.time.LocalDate;
+
 @Entity
 public class Impatiens
         extends PanacheEntity {
+
+    public String cultivarName;
+    public LocalDate developedDate;
+    public LocalDate marketingDate;
+    public CultivarStatus cultivarStatus;
 }
