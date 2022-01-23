044571d364dbd40343fe60b66edc050cb4d49c28
paolomococci <paolomococci@gmail.com>
2022-01-23 18:50:58 +0100

    edited class ImpatiensResourceTest belonging to project impatiens

--- Quarkus/impatiens/src/test/java/local/example/impatiens/ImpatiensResourceTest.java
+++ Quarkus/impatiens/src/test/java/local/example/impatiens/ImpatiensResourceTest.java
@@ -1,9 +1,11 @@
 package local.example.impatiens;
 
 import io.quarkus.test.junit.QuarkusTest;
+
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
@@ -17,6 +19,5 @@ public class ImpatiensResourceTest {
              .statusCode(200)
              .body(is("-- impatiens API --"));
     }
-
 }
 
