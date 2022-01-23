b6ff581718b6dae0c8cacc2c319a6a6f4708e7d7
paolomococci <paolomococci@gmail.com>
2022-01-23 18:41:29 +0100

    edited class ImpatiensResource belonging to project impatiens

--- Quarkus/impatiens/src/main/java/local/example/impatiens/ImpatiensResource.java
+++ Quarkus/impatiens/src/main/java/local/example/impatiens/ImpatiensResource.java
@@ -1,9 +1,13 @@
 package local.example.impatiens;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import io.quarkus.hibernate.reactive.panache.Panache;
+import io.smallrye.mutiny.Uni;
+
+import local.example.impatiens.entity.Impatiens;
+
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/impatiens")
 public class ImpatiensResource {
@@ -12,5 +16,31 @@ public class ImpatiensResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String feedback() {
         return "-- impatiens API --";
+    }
+
+    @POST
+    @Path("/api/v1/create")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Uni<Response> create(Impatiens impatiens) {
+        if (impatiens == null) {
+            throw new WebApplicationException(
+                    "Unprocessable Entity: null entity that cannot be processed",
+                    422
+            );
+        }
+
+        if (impatiens.id != null) {
+            throw new WebApplicationException(
+                    "Unprocessable Entity: one the entity to which an identifier has already been assigned cannot be processed",
+                    422
+            );
+        }
+
+        return Panache
+                .withTransaction(impatiens::persist)
+                .replaceWith(
+                        Response.ok(impatiens).status(Response.Status.CREATED)::build
+                );
     }
 }
