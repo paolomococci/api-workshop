683dcd1b123864d8dc348b0226578e6956da26fe
paolomococci <paolomococci@gmail.com>
2022-01-24 06:25:29 +0100

    edited class Expertise belonging to project nimble

--- Quarkus/nimble/src/main/java/local/example/nimble/api/model/Expertise.java
+++ Quarkus/nimble/src/main/java/local/example/nimble/api/model/Expertise.java
@@ -1,9 +1,11 @@
 package local.example.nimble.api.model;
 
 import io.quarkus.hibernate.reactive.panache.PanacheEntity;
+
 import io.smallrye.mutiny.Uni;
 
 import javax.persistence.*;
+
 import java.util.List;
 
 @Entity
@@ -24,9 +26,5 @@ public class Expertise
 
     public static Uni<Expertise> findByName(String name) {
         return find("name", name).firstResult();
-    }
-
-    public static Uni<List<Expertise>> findByDescription(String description) {
-        return list("description", description);
     }
 }
