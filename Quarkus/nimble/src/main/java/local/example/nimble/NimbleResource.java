be2a41e190b877d02ae4ad149aaf25a75f391f83
paolomococci <paolomococci@gmail.com>
2022-01-24 09:49:30 +0100

    edited class NimbleResource belonging to project nimble

--- Quarkus/nimble/src/main/java/local/example/nimble/NimbleResource.java
+++ Quarkus/nimble/src/main/java/local/example/nimble/NimbleResource.java
@@ -1,9 +1,14 @@
 package local.example.nimble;
 
+import io.quarkus.vertx.web.Route;
+import io.smallrye.mutiny.Uni;
+import local.example.nimble.api.model.Expertise;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/nimble")
 public class NimbleResource {
@@ -12,5 +17,14 @@ public class NimbleResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from nimble";
+    }
+
+    @Route(
+            path = "/api/v2/expertise/itemise",
+            methods = Route.HttpMethod.GET,
+            produces = MediaType.APPLICATION_JSON
+    )
+    public Uni<List<Expertise>> expertiseListUni() {
+        return Expertise.itemise();
     }
 }
