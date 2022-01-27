633e1c7d2cf7d3bf4386c795159966da3f677a4b
paolomococci <paolomococci@gmail.com>
2022-01-27 10:12:20 +0100

    edited class BillingResourceTest belonging to project billing

--- Quarkus/billing/src/test/java/local/example/billing/BillingResourceTest.java
+++ Quarkus/billing/src/test/java/local/example/billing/BillingResourceTest.java
@@ -17,6 +17,6 @@ public class BillingResourceTest {
           .when().get("/billing")
           .then()
              .statusCode(200)
-             .body(is("-- Billing REST API feedback endpoint --"));
+             .body(is("-- Billing REST API feedback that returns some plain text --"));
     }
 }
