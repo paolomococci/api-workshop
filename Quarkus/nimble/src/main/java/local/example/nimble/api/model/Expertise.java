193ab597961416fb6d2b465c8df1b6628be44c27
paolomococci <paolomococci@gmail.com>
2022-01-28 10:59:38 +0100

    edited class Expertise belonging to project nimble

--- Quarkus/nimble/src/main/java/local/example/nimble/api/model/Expertise.java
+++ Quarkus/nimble/src/main/java/local/example/nimble/api/model/Expertise.java
@@ -21,7 +21,11 @@ import java.util.List;
                 @NamedQuery(
                         name = "Expertise.getEmployee",
                         query = """
-                                
+                                SELECT e.id, e.name, e.surname, e.username, e.birthday, e.recruited
+                                FROM employee e
+                                INNER JOIN employee_expertise ep ON e.id = ep.employee_id
+                                INNER JOIN expertise p ON ep.expertise_id = ?1
+                                LIMIT 1
                                 """
                 ),
                 @NamedQuery(
