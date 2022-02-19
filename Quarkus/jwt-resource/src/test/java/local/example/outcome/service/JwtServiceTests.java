16430a6347b28df6eccc882c5bc19dfe898c18da
paolomococci <paolomococci@gmail.com>
2022-02-19 08:05:36 +0100

    edited class JwtServiceTests belonging to project jwt-resource

--- Quarkus/jwt-resource/src/test/java/local/example/outcome/service/JwtServiceTests.java
+++ Quarkus/jwt-resource/src/test/java/local/example/outcome/service/JwtServiceTests.java
@@ -1,4 +1,13 @@
 package local.example.outcome.service;
 
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
 public class JwtServiceTests {
+
+    @Test
+    public void sampleTest() {
+        // TODO
+    }
 }
