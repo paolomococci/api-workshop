c9d1253d6e25dd4a1006f8ea3cf838e7578227f9
paolomococci <paolomococci@gmail.com>
2022-02-20 16:58:27 +0100

    edited class JwtService belonging to project jwt-resource

--- Quarkus/jwt-resource/src/main/java/local/example/outcome/service/JwtService.java
+++ Quarkus/jwt-resource/src/main/java/local/example/outcome/service/JwtService.java
@@ -1,4 +1,14 @@
 package local.example.outcome.service;
 
 public class JwtService {
+
+    public static String create() {
+        // TODO
+        return "";
+    }
+
+    public static String decode() {
+        // TODO
+        return "";
+    }
 }
