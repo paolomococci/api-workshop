e78b06233964fc0d33961a5b8ce0ad33e8531135
paolomococci <paolomococci@gmail.com>
2022-02-20 19:09:26 +0100

    edited class ResourceRetriever belonging to project jwt-resource

--- Quarkus/jwt-resource/src/main/java/local/example/outcome/util/ResourceRetriever.java
+++ Quarkus/jwt-resource/src/main/java/local/example/outcome/util/ResourceRetriever.java
@@ -1,4 +1,14 @@
 package local.example.outcome.util;
 
+import java.io.InputStream;
+
 public class ResourceRetriever {
+
+    public InputStream retrieveStream(final String fileName) {
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(fileName);
+        if (inputStream == null) {
+            throw new IllegalArgumentException(fileName + "not found");
+        }
+        return inputStream;
+    }
 }
