183c7ed049414c9c42326ffdd6a0534b41ecb217
paolomococci <paolomococci@gmail.com>
2022-02-20 16:56:12 +0100

    edited class JwtServiceTests belonging to project jwt-resource

--- Quarkus/jwt-resource/src/test/java/local/example/outcome/service/JwtServiceTests.java
+++ Quarkus/jwt-resource/src/test/java/local/example/outcome/service/JwtServiceTests.java
@@ -1,13 +1,18 @@
 package local.example.outcome.service;
 
 import io.quarkus.test.junit.QuarkusTest;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
 public class JwtServiceTests {
 
     @Test
-    public void sampleTest() {
+    @Disabled
+    @DisplayName("create and decode JWT")
+    public void jwtTest() {
         // TODO
     }
 }
