bb4ebd78b147b36aeed2302265274deee1066006
paolomococci <paolomococci@gmail.com>
2022-02-21 20:09:31 +0100

    edited class JwtService belonging to project jwt-resource

--- Quarkus/jwt-resource/src/main/java/local/example/outcome/service/JwtService.java
+++ Quarkus/jwt-resource/src/main/java/local/example/outcome/service/JwtService.java
@@ -4,20 +4,20 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-
 import local.example.outcome.util.ResourceRetriever;
 
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
-
 import java.io.IOException;
+import java.security.Key;
 import java.util.Date;
 
 public class JwtService {
 
     private static String key;
 
-    public JwtService() throws IOException {
+    public JwtService()
+            throws IOException {
         String publicKey = ResourceRetriever.content("publickey.cer");
         key = publicKey
                 .replace("-----BEGIN CERTIFICATE-----", "")
@@ -25,7 +25,7 @@ public class JwtService {
                 .replace("-----END CERTIFICATE-----", "");
     }
 
-    public static String create(
+    public String create(
             String id,
             String issuer,
             String subject,
@@ -33,26 +33,24 @@ public class JwtService {
     ) {
         SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.ES256;
         long nowInMilliseconds = System.currentTimeMillis();
+        Date date = new Date(nowInMilliseconds);
+        byte[] keyInBytes = DatatypeConverter.parseBase64Binary(getKey());
+        Key keyForSignature = new SecretKeySpec(keyInBytes, signatureAlgorithm.getJcaName());
         JwtBuilder jwtBuilder = Jwts.builder()
                 .setId(id)
-                .setIssuedAt(new Date(nowInMilliseconds))
+                .setIssuedAt(date)
                 .setIssuer(issuer)
                 .setSubject(subject)
-                .signWith(
-                        signatureAlgorithm,
-                        new SecretKeySpec(
-                                DatatypeConverter.parseBase64Binary(key),
-                                signatureAlgorithm.getJcaName()
-                        )
-                );
+                .signWith(signatureAlgorithm, keyForSignature);
         if (lifeTimeInMilliseconds >= 0) {
-            Date expirationDate = new Date(nowInMilliseconds + lifeTimeInMilliseconds);
+            long expirationInMilliseconds = nowInMilliseconds + lifeTimeInMilliseconds;
+            Date expirationDate = new Date(expirationInMilliseconds);
             jwtBuilder.setExpiration(expirationDate);
         }
         return jwtBuilder.compact();
     }
 
-    public static Claims decode(String jwt) {
+    public Claims decode(String jwt) {
         return Jwts.parser()
                 .setSigningKey(DatatypeConverter.parseBase64Binary(key))
                 .parseClaimsJws(jwt).getBody();
