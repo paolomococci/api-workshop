3537f21f6be26ebb3fe08055933eee0f75057db6
paolomococci <paolomococci@gmail.com>
2022-02-21 19:31:20 +0100

    edited class JwtService belonging to project jwt-resource

--- Quarkus/jwt-resource/src/main/java/local/example/outcome/service/JwtService.java
+++ Quarkus/jwt-resource/src/main/java/local/example/outcome/service/JwtService.java
@@ -1,11 +1,17 @@
 package local.example.outcome.service;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
 import local.example.outcome.util.ResourceRetriever;
 
+import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
+
 import java.io.IOException;
+import java.util.Date;
 
 public class JwtService {
 
@@ -25,8 +31,25 @@ public class JwtService {
             String subject,
             long lifeTimeInMilliseconds
     ) {
-        // TODO
-        return "";
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.ES256;
+        long nowInMilliseconds = System.currentTimeMillis();
+        JwtBuilder jwtBuilder = Jwts.builder()
+                .setId(id)
+                .setIssuedAt(new Date(nowInMilliseconds))
+                .setIssuer(issuer)
+                .setSubject(subject)
+                .signWith(
+                        signatureAlgorithm,
+                        new SecretKeySpec(
+                                DatatypeConverter.parseBase64Binary(key),
+                                signatureAlgorithm.getJcaName()
+                        )
+                );
+        if (lifeTimeInMilliseconds >= 0) {
+            Date expirationDate = new Date(nowInMilliseconds + lifeTimeInMilliseconds);
+            jwtBuilder.setExpiration(expirationDate);
+        }
+        return jwtBuilder.compact();
     }
 
     public static Claims decode(String jwt) {
