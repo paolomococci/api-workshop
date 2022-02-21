ca1a2d1e95da41a306f5a6b8c5d22eab8af20e84
paolomococci <paolomococci@gmail.com>
2022-02-21 20:11:04 +0100

    edited class JwtServiceTests belonging to project jwt-resource

--- Quarkus/jwt-resource/src/test/java/local/example/outcome/service/JwtServiceTests.java
+++ Quarkus/jwt-resource/src/test/java/local/example/outcome/service/JwtServiceTests.java
@@ -3,26 +3,32 @@ package local.example.outcome.service;
 import io.quarkus.test.junit.QuarkusTest;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.security.SignatureException;
 
 @QuarkusTest
 public class JwtServiceTests {
 
     @Test
-    public void cerExistenceTest() throws IOException {
+    public void cerExistenceTest()
+            throws IOException {
         JwtService jwtService = new JwtService();
         String key = jwtService.getKey();
         Assertions.assertNotNull(key);
     }
 
     @Test
-    @Disabled
-    @DisplayName("create and decode JWT")
-    public void jwtTest() {
-        // TODO
+    public void jwtCreateTest()
+            throws SignatureException, IOException {
+        JwtService jwtService = new JwtService();
+        String jwt = jwtService.create(
+                "SOMEONEID9876543210",
+                "sampleJWT",
+                "johndoe",
+                300000
+        );
+        Assertions.assertNotNull(jwt);
     }
 }
