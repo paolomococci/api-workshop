3b5e23cb09d7a26ffbf8f0d2902d29463fe68575
paolomococci <paolomococci@gmail.com>
2022-02-21 19:34:43 +0100

    edited class ResourceRetriever belonging to project jwt-resource

--- Quarkus/jwt-resource/src/main/java/local/example/outcome/util/ResourceRetriever.java
+++ Quarkus/jwt-resource/src/main/java/local/example/outcome/util/ResourceRetriever.java
@@ -2,16 +2,21 @@ package local.example.outcome.util;
 
 import java.io.IOException;
 import java.io.InputStream;
+
 import java.nio.charset.StandardCharsets;
 
 public class ResourceRetriever {
 
     public static String content(final String path)
             throws IOException {
-        InputStream inputStream = ResourceRetriever.class.getClassLoader().getResourceAsStream(path);
+        InputStream inputStream = ResourceRetriever
+                .class.getClassLoader().getResourceAsStream(path);
         if (inputStream == null) {
-            throw new IllegalArgumentException(path + "not found");
+            throw new IllegalArgumentException("Path " + path + " does not exist!");
         }
-        return new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+        return new String(
+                inputStream.readAllBytes(),
+                StandardCharsets.UTF_8
+        );
     }
 }
