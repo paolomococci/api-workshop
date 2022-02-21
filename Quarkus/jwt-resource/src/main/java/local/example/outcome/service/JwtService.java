4f86944a24b54011eaa577f58ba7b39ceaf3b345
paolomococci <paolomococci@gmail.com>
2022-02-21 08:10:22 +0100

    edited class JwtService belonging to project jwt-resource

--- Quarkus/jwt-resource/src/main/java/local/example/outcome/service/JwtService.java
+++ Quarkus/jwt-resource/src/main/java/local/example/outcome/service/JwtService.java
@@ -1,6 +1,20 @@
 package local.example.outcome.service;
 
+import local.example.outcome.util.ResourceRetriever;
+
+import java.io.IOException;
+
 public class JwtService {
+
+    private String key;
+
+    public JwtService() throws IOException {
+        String publicKey = ResourceRetriever.content("publickey.cer");
+        key = publicKey
+                .replace("-----BEGIN CERTIFICATE-----", "")
+                .replace(System.lineSeparator(), "")
+                .replace("-----END CERTIFICATE-----", "");
+    }
 
     public static String create() {
         // TODO
@@ -10,5 +24,9 @@ public class JwtService {
     public static String decode() {
         // TODO
         return "";
+    }
+
+    public String getKey() {
+        return key;
     }
 }
