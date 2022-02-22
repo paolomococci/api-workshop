4997d67f0e50f1d682c5f073e55134999da0ea70
paolomococci <37364757+paolomococci@users.noreply.github.com>
2022-02-22 21:55:52 +0100

    Delete NativeJwtOutcomeResourceIT.java

--- Quarkus/jwt-resource/src/test/java/local/example/outcome/NativeJwtOutcomeResourceIT.java
+++
@@ -1,8 +0,0 @@
-package local.example.outcome;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeJwtOutcomeResourceIT
-        extends JwtOutcomeResourceTest {
-}
