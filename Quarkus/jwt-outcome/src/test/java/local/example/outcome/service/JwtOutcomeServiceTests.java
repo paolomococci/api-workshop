26d3e6b2fd38ce0eb91c1459c59234cd00fb7db2
paolomococci <paolomococci@gmail.com>
2022-02-26 08:15:09 +0100

    edited class JwtOutcomeServiceTests belonging to project jwt-outcome

--- Quarkus/jwt-outcome/src/test/java/local/example/outcome/service/JwtOutcomeServiceTests.java
+++ Quarkus/jwt-outcome/src/test/java/local/example/outcome/service/JwtOutcomeServiceTests.java
@@ -1,4 +1,15 @@
 package local.example.outcome.service;
 
+import io.quarkus.test.junit.QuarkusTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
 public class JwtOutcomeServiceTests {
+
+    @Test
+    public void sampleTest() {
+        Assertions.assertTrue(true);
+    }
 }
