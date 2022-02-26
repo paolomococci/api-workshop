5c001bc7f5af3fd6e4bcc7d7b3717b6952451fda
paolomococci <paolomococci@gmail.com>
2022-02-26 11:05:12 +0100

    edited class NativeHMacGeneratorIT belonging to project jwt-outcome

--- Quarkus/jwt-outcome/src/test/java/local/example/outcome/generator/NativeHMacGeneratorIT.java
+++ Quarkus/jwt-outcome/src/test/java/local/example/outcome/generator/NativeHMacGeneratorIT.java
@@ -3,6 +3,6 @@ package local.example.outcome.generator;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativeHmacGeneratorIT
-        extends HmacGeneratorTests {
+public class NativeHMacGeneratorIT
+        extends HMacGeneratorTests {
 }
