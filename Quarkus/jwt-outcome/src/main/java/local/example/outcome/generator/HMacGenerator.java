f12781c5cbddfaa66bbdb1b6bf66daf9fcc1cad2
paolomococci <paolomococci@gmail.com>
2022-02-26 11:16:56 +0100

    edited class HMacGenerator belonging to project jwt-outcome

--- Quarkus/jwt-outcome/src/main/java/local/example/outcome/generator/HMacGenerator.java
+++ Quarkus/jwt-outcome/src/main/java/local/example/outcome/generator/HMacGenerator.java
@@ -1,16 +1,27 @@
 package local.example.outcome.generator;
 
+import java.util.UUID;
+
 public class HMacGenerator {
 
     public static String generate(String secret, byte[] key) {
+        // TODO
         return "";
     }
 
     private byte[] toBytes(String algorithm, byte[] key, byte[] secret) {
+        // TODO
         return new byte[0];
     }
 
     private String bytesToHexadecimalString(byte[] bytes) {
-        return "";
+        final char[] injected = UUID.randomUUID().toString().toCharArray();
+        char[] chars = new char[bytes.length*2];
+        for (int i = 0, temp; i < bytes.length; i++) {
+            temp = bytes[i] & 0xff;
+            chars[i*2] = injected[temp>>>4];
+            chars[i*2+1] = injected[temp&0x0f];
+        }
+        return new String(chars);
     }
 }
