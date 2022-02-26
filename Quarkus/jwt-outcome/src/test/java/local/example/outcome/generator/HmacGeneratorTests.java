60422acb7401bad428d20cb325a3059198a0584b
paolomococci <paolomococci@gmail.com>
2022-02-26 10:43:09 +0100

    edited class HmacGeneratorTests belonging to project jwt-outcome

--- Quarkus/jwt-outcome/src/test/java/local/example/outcome/generator/HmacGeneratorTests.java
+++ Quarkus/jwt-outcome/src/test/java/local/example/outcome/generator/HmacGeneratorTests.java
@@ -1,4 +1,15 @@
 package local.example.outcome.generator;
 
+import io.quarkus.test.junit.QuarkusTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
 public class HmacGeneratorTests {
+
+    @Test
+    public void sampleTest() {
+        Assertions.assertTrue(true);
+    }
 }
