b20d322825fb85b2083fd7ba60e043cdd73031fa
paolomococci <paolomococci@gmail.com>
2022-02-26 12:22:12 +0100

    edited class HMacGeneratorTests belonging to project jwt-outcome

--- Quarkus/jwt-outcome/src/test/java/local/example/outcome/generator/HMacGeneratorTests.java
+++ Quarkus/jwt-outcome/src/test/java/local/example/outcome/generator/HMacGeneratorTests.java
@@ -5,11 +5,29 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
+
 @QuarkusTest
 public class HMacGeneratorTests {
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void generatorTest()
+            throws NoSuchAlgorithmException, InvalidKeyException {
+        Random random = new Random();
+        StringBuilder stringBuilder = new StringBuilder(256);
+        for (int i = 0; i < 256; i++) {
+            int randomCharacter = 33 + (int) (random.nextDouble()*93);
+            stringBuilder.append((char) randomCharacter);
+        }
+        HMacGenerator hMacGenerator = new HMacGenerator();
+        String result = hMacGenerator.generate(
+                stringBuilder.toString(),
+                "sample".getBytes(StandardCharsets.UTF_8)
+        );
+        //System.out.println(result);
+        Assertions.assertTrue(result.chars().count() > 0);
     }
 }
