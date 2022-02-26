4dc80cb25edb7c7b16007f3b66c89ed8b654a11d
paolomococci <paolomococci@gmail.com>
2022-02-26 08:15:19 +0100

    edited class ResourceRetrieverUtilTests belonging to project jwt-outcome

--- Quarkus/jwt-outcome/src/test/java/local/example/outcome/util/ResourceRetrieverUtilTests.java
+++ Quarkus/jwt-outcome/src/test/java/local/example/outcome/util/ResourceRetrieverUtilTests.java
@@ -1,4 +1,15 @@
 package local.example.outcome.util;
 
+import io.quarkus.test.junit.QuarkusTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
 public class ResourceRetrieverUtilTests {
+
+    @Test
+    public void sampleTest() {
+        Assertions.assertTrue(true);
+    }
 }
