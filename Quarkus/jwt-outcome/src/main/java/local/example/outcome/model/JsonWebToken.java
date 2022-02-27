2b074e4ca9225ff54f5324a29fb8dd8903f65f88
paolomococci <paolomococci@gmail.com>
2022-02-27 19:46:26 +0100

    edited class JsonWebToken belonging to project jwt-outcome

--- Quarkus/jwt-outcome/src/main/java/local/example/outcome/model/JsonWebToken.java
+++ Quarkus/jwt-outcome/src/main/java/local/example/outcome/model/JsonWebToken.java
@@ -6,4 +6,18 @@ public class JsonWebToken {
     public String name;
     public String email;
     public String subject;
+
+    public JsonWebToken(
+            String country,
+            String city,
+            String name,
+            String email,
+            String subject
+    ) {
+        this.country = country;
+        this.city = city;
+        this.name = name;
+        this.email = email;
+        this.subject = subject;
+    }
 }
