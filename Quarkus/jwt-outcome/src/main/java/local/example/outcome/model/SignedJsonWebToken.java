f318de61d124a0f8f08c92ea28f9aa04f2a1341f
paolomococci <paolomococci@gmail.com>
2022-02-27 21:10:51 +0100

    edited class SignedJsonWebToken belonging to project jwt-outcome

--- Quarkus/jwt-outcome/src/main/java/local/example/outcome/model/SignedJsonWebToken.java
+++ Quarkus/jwt-outcome/src/main/java/local/example/outcome/model/SignedJsonWebToken.java
@@ -1,5 +1,8 @@
 package local.example.outcome.model;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
 public class SignedJsonWebToken {
 
     public String country;
@@ -7,18 +10,22 @@ public class SignedJsonWebToken {
     public String name;
     public String email;
     public String subject;
+    public HMac hMac;
 
     public SignedJsonWebToken(
             String country,
             String city,
             String name,
             String email,
-            String subject
-    ) {
+            String subject,
+            String secret,
+            byte[] key
+    ) throws NoSuchAlgorithmException, InvalidKeyException {
         this.country = country;
         this.city = city;
         this.name = name;
         this.email = email;
         this.subject = subject;
+        this.hMac = new HMac(secret, key);
     }
 }
