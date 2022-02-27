d905bb2bc647a4bcd6304812123a6d9ea840a5d6
paolomococci <paolomococci@gmail.com>
2022-02-27 21:04:27 +0100

    edited class HMac belonging to project jwt-outcome

--- Quarkus/jwt-outcome/src/main/java/local/example/outcome/model/HMac.java
+++ Quarkus/jwt-outcome/src/main/java/local/example/outcome/model/HMac.java
@@ -1,4 +1,16 @@
 package local.example.outcome.model;
 
+import local.example.outcome.generator.HMacGenerator;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
 public class HMac {
+
+    String hMac;
+
+    public HMac(String secret, byte[] key)
+            throws NoSuchAlgorithmException, InvalidKeyException {
+        this.hMac = HMacGenerator.generate(secret, key);
+    }
 }
