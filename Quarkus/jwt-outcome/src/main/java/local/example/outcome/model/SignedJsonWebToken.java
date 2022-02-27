a4b093199c0bfd80d4cf2633da673d3fb109016c
paolomococci <paolomococci@gmail.com>
2022-02-27 21:03:18 +0100

    edited class SignedJsonWebToken belonging to project jwt-outcome

--- Quarkus/jwt-outcome/src/main/java/local/example/outcome/model/SignedJsonWebToken.java
+++ Quarkus/jwt-outcome/src/main/java/local/example/outcome/model/SignedJsonWebToken.java
@@ -1,9 +1,24 @@
 package local.example.outcome.model;
 
 public class SignedJsonWebToken {
+
     public String country;
     public String city;
     public String name;
     public String email;
     public String subject;
+
+    public SignedJsonWebToken(
+            String country,
+            String city,
+            String name,
+            String email,
+            String subject
+    ) {
+        this.country = country;
+        this.city = city;
+        this.name = name;
+        this.email = email;
+        this.subject = subject;
+    }
 }
