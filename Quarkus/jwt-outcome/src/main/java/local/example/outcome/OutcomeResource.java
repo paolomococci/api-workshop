f6914365ab3ffa6e78a49f01de1d83db2400a23d
paolomococci <paolomococci@gmail.com>
2022-02-28 07:34:12 +0100

    edited class OutcomeResource belonging to project jwt-outcome

--- Quarkus/jwt-outcome/src/main/java/local/example/outcome/OutcomeResource.java
+++ Quarkus/jwt-outcome/src/main/java/local/example/outcome/OutcomeResource.java
@@ -1,12 +1,25 @@
 package local.example.outcome;
 
+import local.example.outcome.model.UnsignedJsonWebToken;
+import local.example.outcome.service.JwtOutcomeService;
+
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Set;
 
 @Path("/outcome")
 public class OutcomeResource {
+
+    private final Set<UnsignedJsonWebToken> assortments = Collections.newSetFromMap(
+        Collections.synchronizedMap(
+                new LinkedHashMap<>()
+        )
+);
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -14,15 +27,9 @@ public class OutcomeResource {
         return "--- jwt-outcome feedback ---";
     }
 
-    @GET
+    @POST
     @Produces(MediaType.TEXT_PLAIN)
-    public String unsigned() {
-        return "--- jwt-outcome unsigned ---";
-    }
-
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String signed() {
-        return "--- jwt-outcome signed ---";
+    public String unsignedJwt(UnsignedJsonWebToken unsignedJsonWebToken) {
+        return JwtOutcomeService.createUnsignedJwtTokenFromObject(unsignedJsonWebToken);
     }
 }
