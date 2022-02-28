85544c9dfbd0071885e8a32b47031b0d166e6e2b
paolomococci <paolomococci@gmail.com>
2022-02-28 18:12:09 +0100

    edited class SignedClaim belonging to project jwt-outcome

--- Quarkus/jwt-outcome/src/main/java/local/example/outcome/model/SignedClaim.java
+++ Quarkus/jwt-outcome/src/main/java/local/example/outcome/model/SignedClaim.java
@@ -7,4 +7,21 @@ public class SignedClaim {
     public String name;
     public String email;
     public String subject;
+
+    public SignedClaim() {
+    }
+
+    public SignedClaim(
+            String country,
+            String city,
+            String name,
+            String email,
+            String subject
+    ) {
+        this.country = country;
+        this.city = city;
+        this.name = name;
+        this.email = email;
+        this.subject = subject;
+    }
 }
