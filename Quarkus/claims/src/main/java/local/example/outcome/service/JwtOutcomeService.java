42e3ab470b316fb1c79982bb829392f8a0813915
paolomococci <paolomococci@gmail.com>
2022-03-13 21:29:50 +0100

    edited class JwtOutcomeService belonging to project claims

--- Quarkus/claims/src/main/java/local/example/outcome/service/JwtOutcomeService.java
+++ Quarkus/claims/src/main/java/local/example/outcome/service/JwtOutcomeService.java
@@ -1,5 +1,6 @@
 package local.example.outcome.service;
 
+import local.example.outcome.model.SignedClaim;
 import local.example.outcome.retriever.ResourceRetriever;
 
 import java.io.IOException;
@@ -9,6 +10,10 @@ import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.UUID;
 
 public class JwtOutcomeService {
 
@@ -19,6 +24,22 @@ public class JwtOutcomeService {
         InputStream privateInputStream = ResourceRetriever.raw("privateKey.der");
         privateDerKey = new byte[privateInputStream.available()];
         privateDerKey = privateInputStream.readAllBytes();
+    }
+
+    public String createSignedHMACJwtFromObject(SignedClaim signedClaim)
+            throws NoSuchAlgorithmException,
+            InvalidKeySpecException {
+        return Jwts.builder()
+                .claim("country", signedClaim.country)
+                .claim("city", signedClaim.city)
+                .claim("name", signedClaim.name)
+                .claim("email", signedClaim.email)
+                .setSubject(signedClaim.subject)
+                .setId(UUID.randomUUID().toString())
+                .setIssuedAt(Date.from(Instant.now()))
+                .setExpiration(Date.from(Instant.now().plus(30, ChronoUnit.MINUTES)))
+                .signWith(rsaPrivateKey())
+                .compact();
     }
 
     private RSAPrivateKey rsaPrivateKey()
