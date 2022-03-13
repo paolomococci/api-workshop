0f3dec4303b5eb2dac82867a6a3ca34d13661e6f
paolomococci <paolomococci@gmail.com>
2022-03-13 21:27:59 +0100

    edited class JwtOutcomeService belonging to project claims

--- Quarkus/claims/src/main/java/local/example/outcome/service/JwtOutcomeService.java
+++ Quarkus/claims/src/main/java/local/example/outcome/service/JwtOutcomeService.java
@@ -4,6 +4,7 @@ import local.example.outcome.retriever.ResourceRetriever;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.spec.PKCS8EncodedKeySpec;
 
 public class JwtOutcomeService {
 
@@ -14,5 +15,9 @@ public class JwtOutcomeService {
         InputStream privateInputStream = ResourceRetriever.raw("privateKey.der");
         privateDerKey = new byte[privateInputStream.available()];
         privateDerKey = privateInputStream.readAllBytes();
+    }
+
+    private PKCS8EncodedKeySpec pkcs8EncodedKeySpec() {
+        return new PKCS8EncodedKeySpec(privateDerKey);
     }
 }
