23addde9786363cb1d68d2c8b9d43b24e3fc0c21
paolomococci <paolomococci@gmail.com>
2022-03-13 21:15:55 +0100

    edited class SignedJwtList belonging to project claims

--- Quarkus/claims/src/main/java/local/example/outcome/model/SignedJwtList.java
+++ Quarkus/claims/src/main/java/local/example/outcome/model/SignedJwtList.java
@@ -1,4 +1,17 @@
 package local.example.outcome.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SignedJwtList {
+
+    public final List<String> signedStrings;
+
+    public SignedJwtList() {
+        signedStrings = new ArrayList<>();
+    }
+
+    public void add(String unsigned) {
+        this.signedStrings.add(unsigned);
+    }
 }
