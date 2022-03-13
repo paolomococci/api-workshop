5923b6324df3b489034de2848e9edac84eda9ee1
paolomococci <paolomococci@gmail.com>
2022-03-13 21:37:49 +0100

    edited class OutcomeResource belonging to project claims

--- Quarkus/claims/src/main/java/local/example/outcome/OutcomeResource.java
+++ Quarkus/claims/src/main/java/local/example/outcome/OutcomeResource.java
@@ -1,5 +1,7 @@
 package local.example.outcome;
 
+import local.example.outcome.model.SignedClaim;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -7,6 +9,12 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/outcome")
 public class OutcomeResource {
+
+    private final SignedClaim signed;
+
+    public OutcomeResource() {
+        signed = new SignedClaim();
+    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
