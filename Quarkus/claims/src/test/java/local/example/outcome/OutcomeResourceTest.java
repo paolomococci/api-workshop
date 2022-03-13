fd92455e6660f81d0d09b21e8ff67e4842d84edf
paolomococci <paolomococci@gmail.com>
2022-03-13 21:20:41 +0100

    edited class OutcomeResourceTest belonging to project claims

--- Quarkus/claims/src/test/java/local/example/outcome/OutcomeResourceTest.java
+++ Quarkus/claims/src/test/java/local/example/outcome/OutcomeResourceTest.java
@@ -1,10 +1,14 @@
 package local.example.outcome;
 
+import io.netty.handler.codec.http.HttpResponseStatus;
 import io.quarkus.test.junit.QuarkusTest;
+
+import io.restassured.http.ContentType;
 
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
@@ -19,5 +23,14 @@ public class OutcomeResourceTest {
           .then()
              .statusCode(200)
              .body(is("<!--feedback endpoint-->"));
+    }
+
+    @Test
+    public void signedEndpointTest() {
+        given()
+                .contentType(ContentType.JSON)
+                .body(JSON_STRING)
+                .when().post("/outcome/signed")
+                .then().statusCode(HttpResponseStatus.OK.code());
     }
 }
