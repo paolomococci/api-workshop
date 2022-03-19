81289d1e1958171eef5aa8ec54921c889a15da9a
paolomococci <paolomococci@gmail.com>
2022-03-19 06:27:30 +0100

    edited class Tale belonging to project lupus-in-fabula

--- Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/model/Tale.java
+++ Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/model/Tale.java
@@ -1,4 +1,11 @@
 package local.example.fabula.model;
 
+import java.time.LocalDate;
+
 public class Tale {
+
+    public Integer id;
+    public String title;
+    public String description;
+    public LocalDate release;
 }
