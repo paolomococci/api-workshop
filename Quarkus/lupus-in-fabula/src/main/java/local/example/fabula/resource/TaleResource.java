7723d7d37d5784fbb7c389e4cc1930a872c53461
paolomococci <paolomococci@gmail.com>
2022-03-19 07:48:50 +0100

    edited class TaleResource belonging to project lupus-in-fabula

--- Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/resource/TaleResource.java
+++ Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/resource/TaleResource.java
@@ -1,13 +1,25 @@
 package local.example.fabula.resource;
 
+import local.example.fabula.model.Tale;
 import local.example.fabula.service.TaleExamplesTestService;
+
+import org.eclipse.microprofile.graphql.Description;
 import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Query;
 
 import javax.inject.Inject;
+import java.util.List;
 
 @GraphQLApi
 public class TaleResource {
 
     @Inject
     TaleExamplesTestService taleExamplesTestService;
+
+    @Query("readAllTales")
+    @Description("read all list of tales stored in this system")
+    public List<Tale> readAllTales() {
+        // TODO
+        return null;
+    }
 }
