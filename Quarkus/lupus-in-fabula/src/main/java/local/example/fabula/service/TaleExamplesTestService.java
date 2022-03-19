e90ec880f8531a43cec154f59cb0af8d89aa5824
paolomococci <paolomococci@gmail.com>
2022-03-19 07:40:53 +0100

    edited class TaleExamplesTestService belonging to project lupus-in-fabula

--- Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/service/TaleExamplesTestService.java
+++ Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/service/TaleExamplesTestService.java
@@ -1,4 +1,13 @@
 package local.example.fabula.service;
 
+import local.example.fabula.model.Tale;
+
+import java.util.List;
+
 public class TaleExamplesTestService {
+
+    private List<Tale> tales;
+
+    public TaleExamplesTestService() {
+    }
 }
