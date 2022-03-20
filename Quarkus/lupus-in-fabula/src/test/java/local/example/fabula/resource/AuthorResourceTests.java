631a778af8e5700fa43d04a166198c3d499170a4
paolomococci <paolomococci@gmail.com>
2022-03-20 21:37:28 +0100

    edited class AuthorResourceTests belonging to project lupus-in-fabula

--- Quarkus/lupus-in-fabula/src/test/java/local/example/fabula/resource/AuthorResourceTests.java
+++ Quarkus/lupus-in-fabula/src/test/java/local/example/fabula/resource/AuthorResourceTests.java
@@ -1,4 +1,15 @@
 package local.example.fabula.resource;
 
+import io.quarkus.test.junit.QuarkusTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
 public class AuthorResourceTests {
+
+    @Test
+    public void createAuthorTest() {
+        Assertions.assertTrue(true);
+    }
 }
