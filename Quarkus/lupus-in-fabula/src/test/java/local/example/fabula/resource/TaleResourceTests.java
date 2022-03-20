19d16443dd217b34fd17a9545a04e62843ea20e4
paolomococci <paolomococci@gmail.com>
2022-03-20 10:43:28 +0100

    edited class TaleResourceTests belonging to project lupus-in-fabula

--- Quarkus/lupus-in-fabula/src/test/java/local/example/fabula/resource/TaleResourceTests.java
+++ Quarkus/lupus-in-fabula/src/test/java/local/example/fabula/resource/TaleResourceTests.java
@@ -1,4 +1,15 @@
 package local.example.fabula.resource;
 
+import io.quarkus.test.junit.QuarkusTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
 public class TaleResourceTests {
+
+    @Test
+    public void sampleTest() {
+        Assertions.assertTrue(true);
+    }
 }
