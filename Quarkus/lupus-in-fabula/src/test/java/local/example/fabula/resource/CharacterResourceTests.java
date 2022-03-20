f7a5e35e16f263c2b0a34f69037ace6033138e0e
paolomococci <paolomococci@gmail.com>
2022-03-20 21:37:16 +0100

    edited class CharacterResourceTests belonging to project lupus-in-fabula

--- Quarkus/lupus-in-fabula/src/test/java/local/example/fabula/resource/CharacterResourceTests.java
+++ Quarkus/lupus-in-fabula/src/test/java/local/example/fabula/resource/CharacterResourceTests.java
@@ -1,4 +1,15 @@
 package local.example.fabula.resource;
 
+import io.quarkus.test.junit.QuarkusTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
 public class CharacterResourceTests {
+
+    @Test
+    public void createCharacterTest() {
+        Assertions.assertTrue(true);
+    }
 }
