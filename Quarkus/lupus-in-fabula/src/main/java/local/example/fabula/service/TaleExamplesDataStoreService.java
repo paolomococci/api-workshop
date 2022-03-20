f2b9aec605dddf751eedf6c012be6e2c83e1330c
paolomococci <paolomococci@gmail.com>
2022-03-20 11:15:46 +0100

    edited class TaleExamplesDataStoreService belonging to project lupus-in-fabula

--- Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/service/TaleExamplesDataStoreService.java
+++ Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/service/TaleExamplesDataStoreService.java
@@ -6,6 +6,7 @@ import local.example.fabula.model.Character;
 import local.example.fabula.model.Hero;
 import local.example.fabula.model.Tale;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TaleExamplesDataStoreService {
@@ -15,4 +16,12 @@ public class TaleExamplesDataStoreService {
     private List<Hero> heroes;
     private List<Antagonist> antagonists;
     private List<Character> characters;
+
+    public TaleExamplesDataStoreService() {
+        this.tales = new ArrayList<>();
+        this.authors = new ArrayList<>();
+        this.heroes = new ArrayList<>();
+        this.antagonists = new ArrayList<>();
+        this.characters = new ArrayList<>();
+    }
 }
