a3b7bba7f3f51e0ae30941aaba8cc0826a17cca1
paolomococci <paolomococci@gmail.com>
2022-03-20 20:34:42 +0100

    edited file TaleResource belonging to project lupus-in-fabula

--- Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/resource/TaleResource.java
+++ Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/resource/TaleResource.java
@@ -1,6 +1,7 @@
 package local.example.fabula.resource;
 
 import local.example.fabula.model.Author;
+import local.example.fabula.model.Hero;
 import local.example.fabula.model.Tale;
 import local.example.fabula.service.TaleExamplesDataStoreService;
 
@@ -9,6 +10,7 @@ import org.eclipse.microprofile.graphql.GraphQLApi;
 import org.eclipse.microprofile.graphql.Query;
 
 import javax.inject.Inject;
+
 import java.util.List;
 
 @GraphQLApi
@@ -27,6 +29,13 @@ public class TaleResource {
     @Query("readAllAuthors")
     @Description("read all list of authors stored in this system")
     public List<Author> readAllAuthors() {
+        // TODO
+        return null;
+    }
+
+    @Query("readAllHeroes")
+    @Description("read all list of heroes stored in this system")
+    public List<Hero> readAllHeroes() {
         // TODO
         return null;
     }
