31987f341865b22020550b36666cc9df94bf651d
paolomococci <paolomococci@gmail.com>
2022-03-21 07:06:13 +0100

    edited class HeroResource belonging to project lupus-in-fabula

--- Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/resource/HeroResource.java
+++ Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/resource/HeroResource.java
@@ -1,13 +1,26 @@
 package local.example.fabula.resource;
 
+import local.example.fabula.model.Hero;
 import local.example.fabula.service.HeroDataStoreService;
+
+import org.eclipse.microprofile.graphql.Description;
 import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Query;
 
 import javax.inject.Inject;
+
+import java.util.List;
 
 @GraphQLApi
 public class HeroResource {
 
     @Inject
     HeroDataStoreService heroDataStoreService;
+
+    @Query("readAllHeroes")
+    @Description("read all list of heroes stored in this system")
+    public List<Hero> readAllHeroes() {
+        // TODO
+        return null;
+    }
 }
