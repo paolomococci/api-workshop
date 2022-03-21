7d404ac17005323158290c6c77534788be98829a
paolomococci <paolomococci@gmail.com>
2022-03-21 06:00:23 +0100

    edited class DataStoreService belonging to project lupus-in-fabula

--- Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/service/DataStoreService.java
+++ Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/service/DataStoreService.java
@@ -1,4 +1,47 @@
 package local.example.fabula.service;
 
+import local.example.fabula.model.Antagonist;
+import local.example.fabula.model.Author;
+import local.example.fabula.model.Character;
+import local.example.fabula.model.Hero;
+import local.example.fabula.model.Tale;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataStoreService {
+
+    private final List<Tale> tales;
+    private final List<Author> authors;
+    private final List<Hero> heroes;
+    private final List<Antagonist> antagonists;
+    private final List<Character> characters;
+
+    public DataStoreService() {
+        this.tales = new ArrayList<>();
+        this.authors = new ArrayList<>();
+        this.heroes = new ArrayList<>();
+        this.antagonists = new ArrayList<>();
+        this.characters = new ArrayList<>();
+    }
+
+    public List<Tale> getTales() {
+        return tales;
+    }
+
+    public List<Author> getAuthors() {
+        return authors;
+    }
+
+    public List<Hero> getHeroes() {
+        return heroes;
+    }
+
+    public List<Antagonist> getAntagonists() {
+        return antagonists;
+    }
+
+    public List<Character> getCharacters() {
+        return characters;
+    }
 }
