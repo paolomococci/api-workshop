7a2272b3cd4af08095b3d6918f60b286f1c0f653
paolomococci <paolomococci@gmail.com>
2022-03-21 07:06:35 +0100

    edited class CharacterResource belonging to project lupus-in-fabula

--- Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/resource/CharacterResource.java
+++ Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/resource/CharacterResource.java
@@ -1,13 +1,25 @@
 package local.example.fabula.resource;
 
+import local.example.fabula.model.Character;
 import local.example.fabula.service.CharacterDataStoreService;
+
+import org.eclipse.microprofile.graphql.Description;
 import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Query;
 
 import javax.inject.Inject;
+import java.util.List;
 
 @GraphQLApi
 public class CharacterResource {
 
     @Inject
     CharacterDataStoreService characterDataStoreService;
+
+    @Query("readAllCharacters")
+    @Description("read all list of characters stored in this system")
+    public List<Character> readAllCharacters() {
+        // TODO
+        return null;
+    }
 }
