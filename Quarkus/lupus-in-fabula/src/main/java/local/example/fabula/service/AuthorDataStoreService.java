d2350d01232a27bf04e098634d098bb48ad8528f
paolomococci <paolomococci@gmail.com>
2022-03-21 06:00:00 +0100

    edited class AuthorDataStoreService belonging to project lupus-in-fabula

--- Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/service/AuthorDataStoreService.java
+++ Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/service/AuthorDataStoreService.java
@@ -1,4 +1,15 @@
 package local.example.fabula.service;
 
+import local.example.fabula.model.Author;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AuthorDataStoreService {
+    
+    private final List<Author> authors;
+
+    public AuthorDataStoreService() {
+        authors = new ArrayList<>();
+    }
 }
