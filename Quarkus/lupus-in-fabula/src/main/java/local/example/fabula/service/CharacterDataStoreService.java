7825c8539ce624112e21297d2b6f5c78a823d961
paolomococci <paolomococci@gmail.com>
2022-03-21 06:00:14 +0100

    edited class CharacterDataStoreService belonging to project lupus-in-fabula

--- Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/service/CharacterDataStoreService.java
+++ Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/service/CharacterDataStoreService.java
@@ -1,4 +1,15 @@
 package local.example.fabula.service;
 
+import local.example.fabula.model.Character;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CharacterDataStoreService {
+
+    private final List<Character> characters;
+
+    public CharacterDataStoreService() {
+        characters = new ArrayList<>();
+    }
 }
