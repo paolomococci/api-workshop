0a1da8e213e27db3bb6895023fa2e2fdae460ccc
paolomococci <paolomococci@gmail.com>
2022-03-21 07:06:46 +0100

    edited class AuthorResource belonging to project lupus-in-fabula

--- Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/resource/AuthorResource.java
+++ Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/resource/AuthorResource.java
@@ -1,13 +1,26 @@
 package local.example.fabula.resource;
 
+import local.example.fabula.model.Author;
 import local.example.fabula.service.AuthorDataStoreService;
+
+import org.eclipse.microprofile.graphql.Description;
 import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Query;
 
 import javax.inject.Inject;
+
+import java.util.List;
 
 @GraphQLApi
 public class AuthorResource {
 
     @Inject
     AuthorDataStoreService authorDataStoreService;
+
+    @Query("readAllAuthors")
+    @Description("read all list of authors stored in this system")
+    public List<Author> readAllAuthors() {
+        // TODO
+        return null;
+    }
 }
