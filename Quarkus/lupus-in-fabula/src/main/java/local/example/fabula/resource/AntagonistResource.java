937fa9c14b97a1c9899d678771afe06ce9221e85
paolomococci <paolomococci@gmail.com>
2022-03-21 07:06:56 +0100

    edited class AntagonistResource belonging to project lupus-in-fabula

--- Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/resource/AntagonistResource.java
+++ Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/resource/AntagonistResource.java
@@ -1,13 +1,26 @@
 package local.example.fabula.resource;
 
+import local.example.fabula.model.Antagonist;
 import local.example.fabula.service.AntagonistDataStoreService;
+
+import org.eclipse.microprofile.graphql.Description;
 import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Query;
 
 import javax.inject.Inject;
+
+import java.util.List;
 
 @GraphQLApi
 public class AntagonistResource {
 
     @Inject
     AntagonistDataStoreService antagonistDataStoreService;
+
+    @Query("readAllAntagonists")
+    @Description("read all list of antagonists stored in this system")
+    public List<Antagonist> readAllAntagonists() {
+        // TODO
+        return null;
+    }
 }
