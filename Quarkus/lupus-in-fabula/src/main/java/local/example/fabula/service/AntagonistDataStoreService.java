f383f79761338d51e2600c78a603cd00dc7cd792
paolomococci <paolomococci@gmail.com>
2022-03-21 05:59:45 +0100

    edited class AntagonistDataStoreService belonging to project lupus-in-fabula

--- Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/service/AntagonistDataStoreService.java
+++ Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/service/AntagonistDataStoreService.java
@@ -1,4 +1,15 @@
 package local.example.fabula.service;
 
+import local.example.fabula.model.Antagonist;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AntagonistDataStoreService {
+
+    private final List<Antagonist> antagonists;
+
+    public AntagonistDataStoreService() {
+        antagonists = new ArrayList<>();
+    }
 }
