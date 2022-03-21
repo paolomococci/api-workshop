813819388f83ceab79431ea264ae7873e1f0fc1e
paolomococci <paolomococci@gmail.com>
2022-03-21 05:59:26 +0100

    edited class TaleDataStoreService belonging to project lupus-in-fabula

--- Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/service/TaleDataStoreService.java
+++ Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/service/TaleDataStoreService.java
@@ -1,9 +1,5 @@
 package local.example.fabula.service;
 
-import local.example.fabula.model.Antagonist;
-import local.example.fabula.model.Author;
-import local.example.fabula.model.Character;
-import local.example.fabula.model.Hero;
 import local.example.fabula.model.Tale;
 
 import java.util.ArrayList;
@@ -13,36 +9,11 @@ public class TaleDataStoreService {
 
     private final List<Tale> tales;
 
-    private final List<Author> authors;
-    private final List<Hero> heroes;
-    private final List<Antagonist> antagonists;
-    private final List<Character> characters;
-
     public TaleDataStoreService() {
         this.tales = new ArrayList<>();
-        this.authors = new ArrayList<>();
-        this.heroes = new ArrayList<>();
-        this.antagonists = new ArrayList<>();
-        this.characters = new ArrayList<>();
     }
 
     public List<Tale> getTales() {
         return tales;
-    }
-
-    public List<Author> getAuthors() {
-        return authors;
-    }
-
-    public List<Hero> getHeroes() {
-        return heroes;
-    }
-
-    public List<Antagonist> getAntagonists() {
-        return antagonists;
-    }
-
-    public List<Character> getCharacters() {
-        return characters;
     }
 }
