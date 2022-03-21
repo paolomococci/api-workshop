851f14d3d47bc5d0be8acc73f2eeb438ce92735c
paolomococci <paolomococci@gmail.com>
2022-03-21 07:06:23 +0100

    edited class TaleResource belonging to project lupus-in-fabula

--- Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/resource/TaleResource.java
+++ Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/resource/TaleResource.java
@@ -1,9 +1,5 @@
 package local.example.fabula.resource;
 
-import local.example.fabula.model.Antagonist;
-import local.example.fabula.model.Author;
-import local.example.fabula.model.Character;
-import local.example.fabula.model.Hero;
 import local.example.fabula.model.Tale;
 import local.example.fabula.service.TaleDataStoreService;
 
@@ -24,34 +20,6 @@ public class TaleResource {
     @Query("readAllTales")
     @Description("read all list of tales stored in this system")
     public List<Tale> readAllTales() {
-        // TODO
-        return null;
-    }
-
-    @Query("readAllAuthors")
-    @Description("read all list of authors stored in this system")
-    public List<Author> readAllAuthors() {
-        // TODO
-        return null;
-    }
-
-    @Query("readAllHeroes")
-    @Description("read all list of heroes stored in this system")
-    public List<Hero> readAllHeroes() {
-        // TODO
-        return null;
-    }
-
-    @Query("readAllAntagonists")
-    @Description("read all list of antagonists stored in this system")
-    public List<Antagonist> readAllAntagonists() {
-        // TODO
-        return null;
-    }
-
-    @Query("readAllCharacters")
-    @Description("read all list of characters stored in this system")
-    public List<Character> readAllCharacters() {
         // TODO
         return null;
     }
