6354fd1639451307b6f8e9d888485015e0be4817
paolomococci <paolomococci@gmail.com>
2022-03-22 17:41:08 +0100

    edited class Hero belonging to project lupus-in-fabula

--- Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/model/Hero.java
+++ Quarkus/lupus-in-fabula/src/main/java/local/example/fabula/model/Hero.java
@@ -1,5 +1,7 @@
 package local.example.fabula.model;
 
+import java.util.UUID;
+
 public class Hero {
 
     public String id;
@@ -11,5 +13,9 @@ public class Hero {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public void setId() {
+        this.id = UUID.randomUUID().toString();
     }
 }
