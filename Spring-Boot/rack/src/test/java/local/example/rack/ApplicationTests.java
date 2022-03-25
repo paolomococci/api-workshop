4f43154ba9ea5e15a01ea07f599171ca7296bcae
paolomococci <paolomococci@gmail.com>
2022-03-25 16:27:09 +0100

    added class ApplicationTests belonging to project rack

---
+++ Spring-Boot/rack/src/test/java/local/example/rack/ApplicationTests.java
@@ -0,0 +1,12 @@
+package local.example.rack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void sampleTest() {
+	}
+}
