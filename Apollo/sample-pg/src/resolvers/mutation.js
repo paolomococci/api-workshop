0102a78a01bd649ad2674dc36c7ccd408dca984a
paolomococci <paolomococci@gmail.com>
2022-04-06 19:20:48 +0200

    edited file mutation.js belonging to project sample-pg

--- Apollo/sample-pg/src/resolvers/mutation.js
+++ Apollo/sample-pg/src/resolvers/mutation.js
@@ -1,12 +1,14 @@
 const { prisma } = require('../database.js');
 
 const Mutation = {
+  // Applicant
   addApplicant: (parent, args) => {
     return prisma.applicant.create({});
   },
   matriculate: (parent, args) => {
-
+    return prisma.applicant.update({});
   },
+  // Tutor
 };
 
 module.exports = {
