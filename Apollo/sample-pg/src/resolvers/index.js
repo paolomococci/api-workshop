0ce13a64cc158b1634a4f2fbd16902b2cbd35aaf
paolomococci <paolomococci@gmail.com>
2022-04-06 20:58:03 +0200

    edited file index.js belonging to project sample-pg

--- Apollo/sample-pg/src/resolvers/index.js
+++ Apollo/sample-pg/src/resolvers/index.js
@@ -2,6 +2,15 @@ const { prisma } = require('../database.js');
 const { Query } = require('./query.js');
 const { Mutation } = require('./mutation.js');
 
+const resolvers {
+  Applicant,
+  Tutor,
+  Ambit,
+  Academy,
+  Query,
+  Mutation
+};
+
 module.exports = {
   resolvers
 };
