92b6c8c9c4f2f8ca62ae707c8f764dd65ef37f0b
paolomococci <paolomococci@gmail.com>
2022-04-06 20:27:00 +0200

    edited file query.js belonging to project sample-pg

--- Apollo/sample-pg/src/resolvers/query.js
+++ Apollo/sample-pg/src/resolvers/query.js
@@ -17,10 +17,12 @@ const Query {
     });
   },
   applicants: (parent, args) => {
-    return prisma.applicant.findall({});
+    return prisma.applicant.findAll({});
   },
   // Tutor
-  tutor: (parent, args) => {},
+  tutor: (parent, args) => {
+
+  },
   tutors: (parent, args) => {},
   // Ambit
   ambit: (parent, args) => {},
