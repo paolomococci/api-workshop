f0f2899061fba2707eb17bf7675a140a94142f1e
paolomococci <paolomococci@gmail.com>
2022-04-06 21:42:56 +0200

    edited file index.js belonging to project sample-pg

--- Apollo/sample-pg/src/index.js
+++ Apollo/sample-pg/src/index.js
@@ -4,3 +4,5 @@ const { resolvers } = require('./resolvers');
 
 const port = process.env.PORT || 8080;
 const server = new ApolloServer({ resolvers, typeDefs });
+
+server.listen({ port }, () => console.log(`server at work http://localhost:${port}`));
