5a111fcfd0a38f4cfa96a944d0412a7f6b0f44d1
paolomococci <paolomococci@gmail.com>
2022-04-14 17:38:37 +0200

    edited file src/database/data.ts belonging to project sample

---
+++ GraphQL/sample/src/database/data.ts
@@ -0,0 +1,11 @@
+export interface IUser {}
+
+export interface IUserUpdate {}
+
+export interface IPost {}
+
+export interface IPostUpdate {}
+
+export interface IComment {}
+
+export interface ICommentUpdate {}
