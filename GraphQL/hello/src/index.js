475b12c15646b69be886434b1df54db4cf24eb90
paolomococci <paolomococci@gmail.com>
2022-04-14 10:30:12 +0200

    edited file src/index.js belonging to project hello

--- GraphQL/hello/src/index.js
+++ GraphQL/hello/src/index.js
@@ -1,2 +1,19 @@
 import { execute, parse } from 'graphql'
 import { schema } from './schema.js'
+
+async function index() {
+  const sampleQuery = parse( `
+    query {
+      hello
+    }
+  `)
+
+  const response = await execute({
+    schema,
+    document: sampleQuery,
+  })
+
+  console.log(response)
+}
+
+index()
