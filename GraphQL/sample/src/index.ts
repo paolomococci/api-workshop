2fb9e82cf484bfe8f8f34df010fd6bc717756657
paolomococci <paolomococci@gmail.com>
2022-04-16 17:43:17 +0200

    edited file src/index.js belonging to project sample

--- GraphQL/sample/src/index.ts
+++ GraphQL/sample/src/index.ts
@@ -1,1 +1,31 @@
 import { GraphQLServer, PubSub } from 'graphql-yoga'
+import db from './database/db';
+import Query from './resolvers/Query';
+import Mutation from './resolvers/Mutation';
+import User from './resolvers/User';
+import Post from './resolvers/Post';
+import Comment from './resolvers/Comment';
+import Subscription from './resolvers/Subscription';
+
+const pubsub = new PubSub()
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers: {
+    Query,
+    Mutation,
+    Subscription,
+    User,
+    Post,
+    Comment,
+  },
+  context: {
+    db,
+    pubsub
+  },
+})
+
+server.options.port = 5000;
+server.start(() => {
+  console.log('server at work on port: ' + 5000);
+})
