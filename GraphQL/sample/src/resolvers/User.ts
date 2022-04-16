b4498c3aa6ef2abc613f66d0161a3737f87da07b
paolomococci <paolomococci@gmail.com>
2022-04-16 22:12:14 +0200

    edited file src/resolvers/User.ts belonging to project sample

--- GraphQL/sample/src/resolvers/User.ts
+++ GraphQL/sample/src/resolvers/User.ts
@@ -2,5 +2,36 @@ import {
   IUser,
   IPost,
   IComment
-} from '../database/data'
+} from '../database/db'
 import { TResolvers } from './Query'
+
+const User = {
+  posts: (
+    (
+      parent,
+      args,
+      {db},
+      info
+    ) => {
+      return db.posts.filter(
+        (post) => {
+          return post.author === parent.id
+        }
+      )
+    }
+  ) as TResolvers<IUser, {}, IPost[]>,
+  comments: (
+    (
+      parent,
+      args,
+      {db},
+      info
+    ) => {
+      return db.comments.filter(
+        (comment) => comment.author === parent.id
+      )
+    }
+  ) as TResolvers<IUser, {}, IComment[]>,
+}
+
+export default User
