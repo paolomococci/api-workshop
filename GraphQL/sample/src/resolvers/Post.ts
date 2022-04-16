e2ab1f5132305150e4aa1009eba92b854946b3f5
paolomococci <paolomococci@gmail.com>
2022-04-16 22:13:03 +0200

    edited file src/resolvers/Post.ts belonging to project sample

--- GraphQL/sample/src/resolvers/Post.ts
+++ GraphQL/sample/src/resolvers/Post.ts
@@ -2,5 +2,36 @@ import {
   IUser,
   IPost,
   IComment
-} from '../database/data'
+} from '../database/db'
 import { TResolvers } from './Query'
+
+const Post = {
+  /* author */
+  author: (
+    (
+      parent,
+      args,
+      {db},
+      info
+    ) => {
+      return db.users.find(
+        (user) => user.id === parent.author
+      )
+    }
+  ) as TResolvers<IPost, {}, IUser>,
+  /* comments */
+  comments: (
+    (
+      parent,
+      args,
+      {db},
+      info
+    ) => {
+      return db.comments.filter(
+        (comment) => comment.post === parent.id
+      )
+    }
+  ) as TResolvers<IPost, {}, IComment[]>,
+}
+
+export default Post
