22c3c00a1b5f8e0a95f1d9224530c3f4f1a9199b
paolomococci <paolomococci@gmail.com>
2022-04-16 22:12:43 +0200

    edited file src/resolvers/Subscription.ts belonging to project sample

--- GraphQL/sample/src/resolvers/Subscription.ts
+++ GraphQL/sample/src/resolvers/Subscription.ts
@@ -1,5 +1,43 @@
 import {
   IPost,
   IComment
-} from '../database/data'
+} from '../database/db'
 import { TResolvers } from './Query'
+
+const commentSubscription: TResolvers<
+  any, {postId:string}, AsyncIterator<IComment>
+> = (
+  parent,
+  args,
+  context,
+  info
+) => {
+  const {pubsub, db} = context
+  const { postId } = args
+  const post = db.posts.find(
+    post => post.id === postId && post.published
+  )
+  if (!post) throw new Error("post not found")
+
+  return pubsub.asyncIterator<IComment>(`comment:${postId}`)
+}
+
+const postSubscription: TResolvers<any, {}, AsyncIterator<IPost>> = (
+  parent,
+  args,
+  context,
+  info
+) => {
+  const {pubsub} = context
+
+  return pubsub.asyncIterator('post')
+}
+
+export default {
+  comment: {
+    subscribe: commentSubscription
+  },
+  post: {
+    subscribe: postSubscription
+  }
+}
