9eaf11d08e3000d6d585b9b801af257a25e545a1
paolomococci <paolomococci@gmail.com>
2022-07-27 15:22:27 +0200

    I edited class OutcomeResource to project account

--- Quarkus/account/src/main/kotlin/local/example/outcome/OutcomeResource.kt
+++ Quarkus/account/src/main/kotlin/local/example/outcome/OutcomeResource.kt
@@ -1,5 +1,8 @@
 package local.example.outcome
 
+import local.example.outcome.model.Account
+
+import java.util.*
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
@@ -7,6 +10,12 @@ import javax.ws.rs.core.MediaType
 
 @Path("/outcome")
 class OutcomeResource {
+
+    private val accounts = Collections.newSetFromMap(
+        Collections.synchronizedMap(
+            LinkedHashMap<Account, Boolean>()
+        )
+    )
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
