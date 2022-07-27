81f3f54d5f3fcf356d945862e9cc80c694fcb9f5
paolomococci <paolomococci@gmail.com>
2022-07-27 15:45:18 +0200

    I edited class OutcomeResourceTest to project account

--- Quarkus/account/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
+++ Quarkus/account/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
@@ -4,9 +4,12 @@ import io.quarkus.test.junit.QuarkusTest
 import io.restassured.RestAssured.given
 
 import org.hamcrest.CoreMatchers.`is`
+import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 class OutcomeResourceTest {
 
     @Test
