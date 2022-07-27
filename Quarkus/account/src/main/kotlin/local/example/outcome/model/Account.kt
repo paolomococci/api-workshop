8d5b7330c2076f762b139c3a3bc20890f2d36477
paolomococci <paolomococci@gmail.com>
2022-07-27 15:05:23 +0200

    I added class Account to project account

--- Quarkus/account/src/main/kotlin/local/example/outcome/model/Account.kt
+++ Quarkus/account/src/main/kotlin/local/example/outcome/model/Account.kt
@@ -1,4 +1,12 @@
 package local.example.outcome.model
 
-class Account {
+import java.math.BigDecimal
+
+class Account() {
+
+    var accountNumber: Long? = null
+    var customerNumber: Long? = null
+    var customerName: String? = null
+    var balance: BigDecimal? = null
+    var accountStatus = AccountStatus.OPEN
 }
