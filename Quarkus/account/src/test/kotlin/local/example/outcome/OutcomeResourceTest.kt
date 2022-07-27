e85ff7a191352098d0b12003ffd63489b25a69a9
paolomococci <paolomococci@gmail.com>
2022-07-27 15:48:09 +0200

    I edited class OutcomeResourceTest to project account

--- Quarkus/account/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
+++ Quarkus/account/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
@@ -1,10 +1,17 @@
 package local.example.outcome
 
 import io.quarkus.test.junit.QuarkusTest
+import io.restassured.RestAssured
 import io.restassured.RestAssured.given
+import local.example.outcome.model.Account
+import org.apache.http.HttpStatus
 
 import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert
+import org.hamcrest.collection.IsCollectionWithSize
+import org.hamcrest.collection.IsEmptyCollection
 import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestMethodOrder
 
@@ -13,12 +20,15 @@ import org.junit.jupiter.api.TestMethodOrder
 class OutcomeResourceTest {
 
     @Test
-    fun testFeedbackEndpoint() {
-        given()
-          .`when`().get("/outcome")
-          .then()
-             .statusCode(200)
-             .body(`is`("feedback reactive"))
+    @Order(1)
+    fun testReadAll() {
+        val response = RestAssured.given()
+            .`when`()[BASE_PATH]
+            .then().statusCode(HttpStatus.SC_OK)
+            .extract().response()
+        val accounts = response.jsonPath().getList<Account>("$")
+        MatcherAssert.assertThat(accounts, IsEmptyCollection.empty())
+        MatcherAssert.assertThat(accounts, IsCollectionWithSize.hasSize(0))
     }
 
     companion object {
