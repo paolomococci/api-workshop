c26ac554c758f2d529f088eb1e125531c369d8bc
paolomococci <paolomococci@gmail.com>
2022-07-28 14:56:13 +0200

    I edited class OutcomeResource to project account

--- Quarkus/account/src/main/kotlin/local/example/outcome/OutcomeResource.kt
+++ Quarkus/account/src/main/kotlin/local/example/outcome/OutcomeResource.kt
@@ -8,6 +8,7 @@ import javax.ws.rs.GET
 import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.Consumes
+import javax.ws.rs.PATCH
 import javax.ws.rs.PUT
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
@@ -50,6 +51,13 @@ class OutcomeResource {
     @PUT
     @Path("{accountNumber}/deposit")
     fun deposit() {
+
+    }
+
+    @PATCH
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    fun update() {
 
     }
 }
