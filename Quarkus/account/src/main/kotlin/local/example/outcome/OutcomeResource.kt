88235da7a5b3ef4a8d2a2a68fc27204d0a31b455
paolomococci <paolomococci@gmail.com>
2022-07-28 14:53:14 +0200

    I edited class OutcomeResource to project account

--- Quarkus/account/src/main/kotlin/local/example/outcome/OutcomeResource.kt
+++ Quarkus/account/src/main/kotlin/local/example/outcome/OutcomeResource.kt
@@ -8,6 +8,7 @@ import javax.ws.rs.GET
 import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.Consumes
+import javax.ws.rs.PUT
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 
@@ -31,6 +32,12 @@ class OutcomeResource {
     @Path("/{accountNumber}")
     @Produces(MediaType.APPLICATION_JSON)
     fun read() {
+
+    }
+
+    @PUT
+    @Path("{accountNumber}/withdrawal")
+    fun withdrawal() {
 
     }
 
