abbbb24bb6364a4519ce9224f06fefb653783341
paolomococci <paolomococci@gmail.com>
2022-07-29 15:24:15 +0200

    I edited class OutcomeResource to project account

--- Quarkus/account/src/main/kotlin/local/example/outcome/OutcomeResource.kt
+++ Quarkus/account/src/main/kotlin/local/example/outcome/OutcomeResource.kt
@@ -1,19 +1,11 @@
 package local.example.outcome
 
 import local.example.outcome.model.Account
-
 import java.util.*
-
-import javax.ws.rs.GET
-import javax.ws.rs.POST
-import javax.ws.rs.Path
-import javax.ws.rs.Consumes
-import javax.ws.rs.DELETE
-import javax.ws.rs.PATCH
-import javax.ws.rs.PUT
-import javax.ws.rs.Produces
+import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
+
 
 @Path("/outcome")
 class OutcomeResource {
@@ -27,8 +19,10 @@ class OutcomeResource {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    fun create(account: Account) {
-        // TODO
+    fun create(account: Account): Response {
+        if (account.customerName == null)
+            throw WebApplicationException("Bad Request: the customer name was not specified", 400)
+        return Response.status(201).entity(account).build()
     }
 
     @GET
