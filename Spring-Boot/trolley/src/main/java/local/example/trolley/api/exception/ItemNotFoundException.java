f23600090adf64624b615655e9fdf567149e4f5d
paolomococci <paolomococci@gmail.com>
2022-09-04 20:22:00 +0200

    deleted:    Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/ItemNotFoundException.java

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/ItemNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package local.example.trolley.api.exception;
-
-public class ItemNotFoundException
-        extends RuntimeException {
-
-    public ItemNotFoundException(Long id) {
-        super("could not find item with id: " + id);
-    }
-}
