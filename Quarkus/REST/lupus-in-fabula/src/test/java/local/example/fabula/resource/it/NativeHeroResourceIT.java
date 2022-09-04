6f1334d20eba9434395796fe86713d65fca7ec6a
paolomococci <paolomococci@gmail.com>
2022-09-04 20:58:55 +0200

    I moved:   Quarkus/REST/lupus-in-fabula/src/test/java/local/example/fabula/resource/it/NativeHeroResourceIT.java

---
+++ Quarkus/REST/lupus-in-fabula/src/test/java/local/example/fabula/resource/it/NativeHeroResourceIT.java
@@ -0,0 +1,10 @@
+package local.example.fabula.resource.it;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+import local.example.fabula.resource.HeroResourceTests;
+
+@NativeImageTest
+public class NativeHeroResourceIT
+        extends HeroResourceTests {
+}
