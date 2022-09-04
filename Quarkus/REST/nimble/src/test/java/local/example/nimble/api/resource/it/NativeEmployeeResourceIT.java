ff34cbb131f7ea71d77c5dfa66baa1582063c57b
paolomococci <paolomococci@gmail.com>
2022-09-04 21:02:08 +0200

    I moved:   Quarkus/REST/nimble/src/test/java/local/example/nimble/api/resource/it/NativeEmployeeResourceIT.java

---
+++ Quarkus/REST/nimble/src/test/java/local/example/nimble/api/resource/it/NativeEmployeeResourceIT.java
@@ -0,0 +1,10 @@
+package local.example.nimble.api.resource.it;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+import local.example.nimble.api.resource.EmployeeResourceTests;
+
+@NativeImageTest
+public class NativeEmployeeResourceIT
+        extends EmployeeResourceTests {
+}
