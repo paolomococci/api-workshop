bb5cd0bf12942f4a2754e8c56e0bfa30c227fb43
paolomococci <paolomococci@gmail.com>
2022-09-04 20:58:29 +0200

    I moved:   Quarkus/REST/lupus-in-fabula/src/test/java/local/example/fabula/resource/TaleResourceTests.java

---
+++ Quarkus/REST/lupus-in-fabula/src/test/java/local/example/fabula/resource/TaleResourceTests.java
@@ -0,0 +1,50 @@
+package local.example.fabula.resource;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+public class TaleResourceTests {
+
+    @Test
+    public void readTaleTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void readAllTalesTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void readAllTalesByAuthorTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void readAllTalesByHeroTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void readAllTalesByAntagonistTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void readAllTalesByCharacterTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void createTaleTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void deleteTaleTest() {
+        Assertions.assertTrue(true);
+    }
+}
