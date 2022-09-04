06f569fb4714f04d76418fdf94788743af566e10
paolomococci <paolomococci@gmail.com>
2022-09-04 20:58:11 +0200

    I moved:   Quarkus/REST/lupus-in-fabula/src/test/java/local/example/fabula/resource/AuthorResourceTests.java

---
+++ Quarkus/REST/lupus-in-fabula/src/test/java/local/example/fabula/resource/AuthorResourceTests.java
@@ -0,0 +1,20 @@
+package local.example.fabula.resource;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+public class AuthorResourceTests {
+
+    @Test
+    public void createAuthorTest() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void deleteAuthorTest() {
+        Assertions.assertTrue(true);
+    }
+}
