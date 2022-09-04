2d17e1b39a0ce786a7abb0335b70fd8ce4e96a88
paolomococci <paolomococci@gmail.com>
2022-09-04 20:54:32 +0200

    I moved:   Quarkus/REST/jwt-outcome/src/test/java/local/example/outcome/retriever/ResourceRetrieverTests.java

---
+++ Quarkus/REST/jwt-outcome/src/test/java/local/example/outcome/retriever/ResourceRetrieverTests.java
@@ -0,0 +1,15 @@
+package local.example.outcome.retriever;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+public class ResourceRetrieverTests {
+
+    @Test
+    public void sampleTest() {
+        Assertions.assertTrue(true);
+    }
+}
