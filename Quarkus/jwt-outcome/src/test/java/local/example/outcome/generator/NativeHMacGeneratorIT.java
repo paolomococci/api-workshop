d2b0d13d3cd4993b054329f693cd68dd44059a09
paolomococci <paolomococci@gmail.com>
2022-09-04 19:48:11 +0200

    deleted:    Quarkus/jwt-outcome/src/test/java/local/example/outcome/generator/NativeHMacGeneratorIT.java

--- Quarkus/jwt-outcome/src/test/java/local/example/outcome/generator/NativeHMacGeneratorIT.java
+++
@@ -1,8 +0,0 @@
-package local.example.outcome.generator;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeHMacGeneratorIT
-        extends HMacGeneratorTests {
-}
