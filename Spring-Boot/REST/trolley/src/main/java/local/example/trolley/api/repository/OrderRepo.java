dcd045fcb713a721bfcd1a3d6cd9d51e5e04256a
paolomococci <paolomococci@gmail.com>
2022-09-04 21:32:36 +0200

    I moved:   Spring-Boot/REST/trolley/src/main/java/local/example/trolley/api/repository/OrderRepo.java

---
+++ Spring-Boot/REST/trolley/src/main/java/local/example/trolley/api/repository/OrderRepo.java
@@ -0,0 +1,25 @@
+package local.example.trolley.api.repository;
+
+import local.example.trolley.api.model.Order;
+
+import local.example.trolley.api.repository.ext.OrderRepoExt;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.UUID;
+
+@RepositoryRestResource
+public interface OrderRepo
+        extends CrudRepository<Order, UUID>,
+        OrderRepoExt {
+
+    String SELECT_QUERY = "SELECT o FROM orders o JOIN o.customer_id u WHERE u.id = :customerId";
+
+    @Query(
+            value = SELECT_QUERY,
+            nativeQuery = true
+    )
+    Iterable<Order> findByCustomerId(@Param("customerId") UUID customerId);
+}
