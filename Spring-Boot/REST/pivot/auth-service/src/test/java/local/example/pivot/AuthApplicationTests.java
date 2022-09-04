79fbf617efd153795b25ef0b57f5a44930a89821
paolomococci <paolomococci@gmail.com>
2022-09-04 21:16:38 +0200

    I moved:   Spring-Boot/REST/pivot/auth-service/src/test/java/local/example/pivot/AuthApplicationTests.java

---
+++ Spring-Boot/REST/pivot/auth-service/src/test/java/local/example/pivot/AuthApplicationTests.java
@@ -0,0 +1,12 @@
+package local.example.pivot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void someTest() {
+	}
+}
