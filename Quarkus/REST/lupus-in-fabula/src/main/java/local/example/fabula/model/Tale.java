7bc1a32906121d96f239f005acb670276d0e6e9f
paolomococci <paolomococci@gmail.com>
2022-09-04 20:56:30 +0200

    I moved:   Quarkus/REST/lupus-in-fabula/src/main/java/local/example/fabula/model/Tale.java

---
+++ Quarkus/REST/lupus-in-fabula/src/main/java/local/example/fabula/model/Tale.java
@@ -0,0 +1,29 @@
+package local.example.fabula.model;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+public class Tale {
+
+    public String id;
+    public String title;
+    public String description;
+    public LocalDate release;
+    public List<Author> authors;
+    public List<Hero> heroes;
+    public List<Antagonist> antagonists;
+    public List<Character> characters;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setId() {
+        this.id = UUID.randomUUID().toString();
+    }
+}
