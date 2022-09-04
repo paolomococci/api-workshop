223595707652a16d0ba4522432fbcb9b6606e551
paolomococci <paolomococci@gmail.com>
2022-09-04 20:54:18 +0200

    I moved:   Quarkus/REST/jwt-outcome/src/test/java/local/example/outcome/generator/NativeHMacGeneratorIT.java

---
+++ Quarkus/REST/jwt-outcome/src/test/java/local/example/outcome/generator/NativeHMacGeneratorIT.java
@@ -0,0 +1,8 @@
+package local.example.outcome.generator;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeHMacGeneratorIT
+        extends HMacGeneratorTests {
+}
