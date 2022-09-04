3e53a29c01db83a6293efc79548886449e62c41b
paolomococci <paolomococci@gmail.com>
2022-09-04 20:21:17 +0200

    deleted:    Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/AddressNotFoundException.java

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/AddressNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package local.example.trolley.api.exception;
-
-public class AddressNotFoundException
-        extends RuntimeException {
-
-    public AddressNotFoundException(Long id) {
-        super("could not find address with id: " + id);
-    }
-}
