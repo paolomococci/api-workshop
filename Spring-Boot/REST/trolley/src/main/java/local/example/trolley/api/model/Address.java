985dc6cad96eba58caeaa827e7e44eaa051ec341
paolomococci <paolomococci@gmail.com>
2022-09-04 21:29:53 +0200

    I moved:   Spring-Boot/REST/trolley/src/main/java/local/example/trolley/api/model/Address.java

---
+++ Spring-Boot/REST/trolley/src/main/java/local/example/trolley/api/model/Address.java
@@ -0,0 +1,58 @@
+package local.example.trolley.api.model;
+
+import local.example.trolley.api.template.EntityTemplate;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "ADDRESSES")
+@EqualsAndHashCode(callSuper = true)
+public class Address
+        extends EntityTemplate
+        implements Serializable {
+
+    @Column(name = "COUNTRY")
+    private String country;
+
+    @Column(name = "STATE")
+    private String state;
+
+    @Column(name = "CITY")
+    private String city;
+
+    @Column(name = "TOWN")
+    private String town;
+
+    @Column(name = "ZIPCODE")
+    private String zipcode;
+
+    @Column(name = "STREET")
+    private String street;
+
+    @Column(name = "NUMBER")
+    private String number;
+
+    @Column(name = "RESIDENTIAL")
+    private boolean residential;
+
+    @OneToMany(
+            mappedBy = "address",
+            fetch = FetchType.LAZY,
+            orphanRemoval = true
+    )
+    private List<Order> orders;
+
+    @OneToMany(
+            mappedBy = "supplyAddress",
+            fetch = FetchType.LAZY,
+            orphanRemoval = true
+    )
+    private List<Supply> supplies;
+}
