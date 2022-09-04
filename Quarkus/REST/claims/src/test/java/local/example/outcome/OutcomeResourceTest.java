0779b8840c541a7b61ee41bc53f7ef2f5106817e
paolomococci <paolomococci@gmail.com>
2022-09-04 20:48:40 +0200

    I moved:   Quarkus/REST/claims/src/test/java/local/example/outcome/OutcomeResourceTest.java

---
+++ Quarkus/REST/claims/src/test/java/local/example/outcome/OutcomeResourceTest.java
@@ -0,0 +1,53 @@
+package local.example.outcome;
+
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.quarkus.test.junit.QuarkusTest;
+
+import io.restassured.http.ContentType;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class OutcomeResourceTest {
+
+    private static final String JSON_STRING = "{\"country\":\"Fantasy\",\"city\":\"Someone\",\"name\":\"James Doe\",\"email\":\"james.doe@example.local\",\"subject\":\"some sample subject\"}";
+
+    @Test
+    public void feedbackEndpointTest() {
+        given()
+          .when().get("/outcome")
+          .then()
+             .statusCode(200)
+             .body(is("<!--feedback endpoint-->"));
+    }
+
+    @Test
+    public void readSignedJwtListEndpointTest() {
+        given()
+                .when().get("/outcome/jwt/list")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void createAndReadSignedJwtListEndpointTest() {
+        given()
+                .contentType(ContentType.JSON)
+                .body(JSON_STRING)
+                .when().post("/outcome/jwt/list")
+                .then().statusCode(HttpResponseStatus.OK.code());
+    }
+
+    @Test
+    public void createSignedJwtEndpointTest() {
+        given()
+                .contentType(ContentType.JSON)
+                .body(JSON_STRING)
+                .when().post("/outcome/jwt")
+                .then().statusCode(HttpResponseStatus.OK.code());
+    }
+}
