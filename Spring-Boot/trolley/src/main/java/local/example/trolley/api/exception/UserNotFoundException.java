94c4b4520799655ee8261ad348f66219ed98a669
paolomococci <paolomococci@gmail.com>
2022-09-04 20:23:10 +0200

    deleted:    Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/UserNotFoundException.java

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package local.example.trolley.api.exception;
-
-public class UserNotFoundException
-        extends RuntimeException {
-
-    public UserNotFoundException(Long id) {
-        super("could not find user with id: " + id);
-    }
-}
