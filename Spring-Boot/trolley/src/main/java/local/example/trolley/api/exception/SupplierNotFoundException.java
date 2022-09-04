05dab09056dfd34ed812df5810f6f5d83db4bad4
paolomococci <paolomococci@gmail.com>
2022-09-04 20:22:56 +0200

    deleted:    Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/SupplierNotFoundException.java

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/SupplierNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package local.example.trolley.api.exception;
-
-public class SupplierNotFoundException
-        extends RuntimeException {
-
-    public SupplierNotFoundException(Long id) {
-        super("could not find supplier with id: " + id);
-    }
-}
