4130e89b7b22ab3fcaee51a593fab6ae23cb077f
paolomococci <paolomococci@gmail.com>
2022-09-04 20:21:23 +0200

    deleted:    Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/AuthorizationNotFoundException.java

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/AuthorizationNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package local.example.trolley.api.exception;
-
-public class AuthorizationNotFoundException
-        extends RuntimeException {
-
-    public AuthorizationNotFoundException(Long id) {
-        super("could not find authorization with id: " + id);
-    }
-}
