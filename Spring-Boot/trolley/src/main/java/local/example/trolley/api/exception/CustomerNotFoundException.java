3d0c4f7773facf0bb0a9dc619be6b8698093879f
paolomococci <paolomococci@gmail.com>
2022-09-04 20:21:41 +0200

    deleted:    Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/CustomerNotFoundException.java

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/CustomerNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package local.example.trolley.api.exception;
-
-public class CustomerNotFoundException
-        extends RuntimeException {
-
-    public CustomerNotFoundException(Long id) {
-        super("could not find customer with id: " + id);
-    }
-}
