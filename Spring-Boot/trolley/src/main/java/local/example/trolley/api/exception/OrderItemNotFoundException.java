c9aa94472d75490b710d05602005e2155ee99cfe
paolomococci <paolomococci@gmail.com>
2022-09-04 20:22:07 +0200

    deleted:    Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/OrderItemNotFoundException.java

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/OrderItemNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package local.example.trolley.api.exception;
-
-public class OrderItemNotFoundException
-        extends RuntimeException {
-
-    public OrderItemNotFoundException(Long id) {
-        super("could not find correlation with id: " + id);
-    }
-}
