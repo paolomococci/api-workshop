17dec264d10b427d5d82c67f64507f35b57976fe
paolomococci <paolomococci@gmail.com>
2022-09-04 20:22:50 +0200

    deleted:    Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/ShipmentNotFoundException.java

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/ShipmentNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package local.example.trolley.api.exception;
-
-public class ShipmentNotFoundException
-        extends RuntimeException {
-
-    public ShipmentNotFoundException(Long id) {
-        super("could not find shipment with id: " + id);
-    }
-}
