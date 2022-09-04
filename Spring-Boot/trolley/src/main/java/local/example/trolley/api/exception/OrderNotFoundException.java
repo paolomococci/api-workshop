0f28db5bebe996c897f6ed3d7865618f49c6d0d1
paolomococci <paolomococci@gmail.com>
2022-09-04 20:22:13 +0200

    deleted:    Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/OrderNotFoundException.java

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/OrderNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package local.example.trolley.api.exception;
-
-public class OrderNotFoundException
-        extends RuntimeException {
-
-    public OrderNotFoundException(Long id) {
-        super("could not find order with id: " + id);
-    }
-}
