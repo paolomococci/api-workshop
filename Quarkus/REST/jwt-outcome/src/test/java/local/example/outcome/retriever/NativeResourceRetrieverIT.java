dddad2b5fe0e6103e67dce53bc288ed9dba2fd8d
paolomococci <paolomococci@gmail.com>
2022-09-04 20:54:26 +0200

    I moved:   Quarkus/REST/jwt-outcome/src/test/java/local/example/outcome/retriever/NativeResourceRetrieverIT.java

---
+++ Quarkus/REST/jwt-outcome/src/test/java/local/example/outcome/retriever/NativeResourceRetrieverIT.java
@@ -0,0 +1,8 @@
+package local.example.outcome.retriever;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeResourceRetrieverIT
+        extends ResourceRetrieverTests {
+}
