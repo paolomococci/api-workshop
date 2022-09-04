68ea2214fb933004c1810de234808e773c722281
paolomococci <paolomococci@gmail.com>
2022-09-04 19:48:18 +0200

    deleted:    Quarkus/jwt-outcome/src/test/java/local/example/outcome/retriever/NativeResourceRetrieverIT.java

--- Quarkus/jwt-outcome/src/test/java/local/example/outcome/retriever/NativeResourceRetrieverIT.java
+++
@@ -1,8 +0,0 @@
-package local.example.outcome.retriever;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeResourceRetrieverIT
-        extends ResourceRetrieverTests {
-}
