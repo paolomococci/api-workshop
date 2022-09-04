1e2deb5eda5322b5ac633cc0cb281a732adf3ec3
paolomococci <paolomococci@gmail.com>
2022-09-04 21:37:07 +0200

    I moved:   Spring-Boot/REST/trolley/src/main/java/local/example/trolley/api/template/EntityTemplate.java

---
+++ Spring-Boot/REST/trolley/src/main/java/local/example/trolley/api/template/EntityTemplate.java
@@ -0,0 +1,51 @@
+package local.example.trolley.api.template;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.UUID;
+
+@MappedSuperclass
+public abstract class EntityTemplate {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(
+            name = "ID",
+            updatable = false,
+            nullable = false
+    )
+    @Type(type = "uuid-char")
+    @Getter
+    private UUID id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "REGISTERED", nullable = false)
+    @Getter
+    private Date registered;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "UPDATED", nullable = false)
+    @Getter
+    @Setter
+    private Date updated;
+
+    @PrePersist
+    protected void onInit() {
+        this.registered = Timestamp.valueOf(LocalDateTime.now());
+    }
+}
