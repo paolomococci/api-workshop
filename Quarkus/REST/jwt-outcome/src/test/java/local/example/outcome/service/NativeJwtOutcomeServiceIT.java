f88ac76a24493413b3139abb5322ccffa20df81c
paolomococci <paolomococci@gmail.com>
2022-09-04 20:54:44 +0200

    I moved:   Quarkus/REST/jwt-outcome/src/test/java/local/example/outcome/service/NativeJwtOutcomeServiceIT.java

---
+++ Quarkus/REST/jwt-outcome/src/test/java/local/example/outcome/service/NativeJwtOutcomeServiceIT.java
@@ -0,0 +1,8 @@
+package local.example.outcome.service;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeJwtOutcomeServiceIT
+        extends JwtOutcomeServiceTests {
+}
