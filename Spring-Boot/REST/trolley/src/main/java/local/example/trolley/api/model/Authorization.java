c069336b52a71506c200cd42380f84232f762f06
paolomococci <paolomococci@gmail.com>
2022-09-04 21:30:01 +0200

    I moved:   Spring-Boot/REST/trolley/src/main/java/local/example/trolley/api/model/Authorization.java

---
+++ Spring-Boot/REST/trolley/src/main/java/local/example/trolley/api/model/Authorization.java
@@ -0,0 +1,36 @@
+package local.example.trolley.api.model;
+
+import local.example.trolley.api.template.EntityTemplate;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "AUTHORIZATIONS")
+@EqualsAndHashCode(callSuper = true)
+public class Authorization
+        extends EntityTemplate
+        implements Serializable {
+
+    @Column(name = "MESSAGE")
+    private String message;
+
+    @Column(name = "ERROR")
+    private String error;
+
+    @Column(name = "AUTHORIZED")
+    private boolean authorized;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "ORDER_ID", referencedColumnName = "ID")
+    private Order order;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "SUPPLY_ID", referencedColumnName = "ID")
+    private Supply supply;
+}
