0fc79467f0854506e06ca73a153c6e0007de5348
paolomococci <paolomococci@gmail.com>
2022-09-04 21:30:09 +0200

    I moved:   Spring-Boot/REST/trolley/src/main/java/local/example/trolley/api/model/Card.java

---
+++ Spring-Boot/REST/trolley/src/main/java/local/example/trolley/api/model/Card.java
@@ -0,0 +1,46 @@
+package local.example.trolley.api.model;
+
+import local.example.trolley.api.template.EntityTemplate;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "CARDS")
+@EqualsAndHashCode(callSuper = true)
+public class Card
+        extends EntityTemplate
+        implements Serializable {
+
+    @Column(name = "NUMBER")
+    private String number;
+
+    @Column(name = "EXPIRES")
+    private String expires;
+
+    @Column(name = "CSC")
+    private String csc;
+
+    @ManyToOne(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    @JoinColumn(
+            name = "USER_ID",
+            referencedColumnName = "ID"
+    )
+    private User user;
+
+    @OneToMany(
+            mappedBy = "card",
+            fetch = FetchType.LAZY,
+            orphanRemoval = true
+    )
+    private List<Order> orders;
+}
