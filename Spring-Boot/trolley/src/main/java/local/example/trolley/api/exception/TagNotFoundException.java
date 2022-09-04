be70a53c694d074cdbd0184f750d9d0bcb91a9ce
paolomococci <paolomococci@gmail.com>
2022-09-04 20:23:04 +0200

    deleted:    Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/TagNotFoundException.java

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/TagNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package local.example.trolley.api.exception;
-
-public class TagNotFoundException
-        extends RuntimeException {
-
-    public TagNotFoundException(Long id) {
-        super("could not find tag with id: " + id);
-    }
-}
