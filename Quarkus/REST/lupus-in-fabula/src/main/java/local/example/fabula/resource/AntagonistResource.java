53ae1905b9cf51a027ff3d868dbd756bed690409
paolomococci <paolomococci@gmail.com>
2022-09-04 20:56:36 +0200

    I moved:   Quarkus/REST/lupus-in-fabula/src/main/java/local/example/fabula/resource/AntagonistResource.java

---
+++ Quarkus/REST/lupus-in-fabula/src/main/java/local/example/fabula/resource/AntagonistResource.java
@@ -0,0 +1,55 @@
+package local.example.fabula.resource;
+
+import local.example.fabula.model.Antagonist;
+import local.example.fabula.service.AntagonistDataStoreService;
+
+import org.eclipse.microprofile.graphql.*;
+
+import javax.inject.Inject;
+
+import java.util.List;
+
+@GraphQLApi
+public class AntagonistResource {
+
+    @Inject
+    AntagonistDataStoreService antagonistDataStoreService;
+
+    @Query("readAllAntagonists")
+    @Description("read all list of antagonists stored in this system")
+    public List<Antagonist> readAllAntagonists() {
+        // TODO
+        return null;
+    }
+
+    @Query
+    @Description("read a antagonist from id")
+    public Antagonist readAntagonist(@Name("idOfAntagonist") String id) {
+        // TODO
+        return null;
+    }
+
+    @Mutation
+    public Antagonist createAntagonist(Antagonist antagonist) {
+        // TODO
+        return null;
+    }
+
+    @Mutation
+    public Antagonist updateAntagonist(@Name("idOfAntagonist") String id, Antagonist antagonist) {
+        // TODO
+        return null;
+    }
+
+    @Mutation
+    public Antagonist partialUpdateAntagonist(@Name("idOfAntagonist") String id, Antagonist antagonist) {
+        // TODO
+        return null;
+    }
+
+    @Mutation
+    public Antagonist deleteAntagonist(@Name("idOfAntagonist") String id) {
+        // TODO
+        return null;
+    }
+}
