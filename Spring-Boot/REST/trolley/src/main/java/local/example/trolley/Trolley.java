1e21bc130539895592a58dc43392269b3248ed27
paolomococci <paolomococci@gmail.com>
2022-09-04 21:21:54 +0200

    I moved:   Spring-Boot/REST/trolley/src/main/java/local/example/trolley/Trolley.java

---
+++ Spring-Boot/REST/trolley/src/main/java/local/example/trolley/Trolley.java
@@ -0,0 +1,12 @@
+package local.example.trolley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trolley {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trolley.class, args);
+	}
+}
