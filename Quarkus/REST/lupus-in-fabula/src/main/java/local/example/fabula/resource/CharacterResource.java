abfb5f7ef996ab1f20804c566695b54c5ec6ab63
paolomococci <paolomococci@gmail.com>
2022-09-04 20:56:49 +0200

    I moved:   Quarkus/REST/lupus-in-fabula/src/main/java/local/example/fabula/resource/CharacterResource.java

---
+++ Quarkus/REST/lupus-in-fabula/src/main/java/local/example/fabula/resource/CharacterResource.java
@@ -0,0 +1,55 @@
+package local.example.fabula.resource;
+
+import local.example.fabula.model.Character;
+import local.example.fabula.service.CharacterDataStoreService;
+
+import org.eclipse.microprofile.graphql.*;
+
+import javax.inject.Inject;
+
+import java.util.List;
+
+@GraphQLApi
+public class CharacterResource {
+
+    @Inject
+    CharacterDataStoreService characterDataStoreService;
+
+    @Query("readAllCharacters")
+    @Description("read all list of characters stored in this system")
+    public List<Character> readAllCharacters() {
+        // TODO
+        return null;
+    }
+
+    @Query
+    @Description("read a character from id")
+    public Character readCharacter(@Name("idOfCharacter") String id) {
+        // TODO
+        return null;
+    }
+
+    @Mutation
+    public Character createCharacter(Character character) {
+        // TODO
+        return null;
+    }
+
+    @Mutation
+    public Character updateCharacter(@Name("idOfCharacter") String id, Character character) {
+        // TODO
+        return null;
+    }
+
+    @Mutation
+    public Character partialUpdateCharacter(@Name("idOfCharacter") String id, Character character) {
+        // TODO
+        return null;
+    }
+
+    @Mutation
+    public Character deleteCharacter(@Name("idOfCharacter") String id) {
+        // TODO
+        return null;
+    }
+}
