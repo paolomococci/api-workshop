0c517ec7169e13ae23e4011dcb8b0bdb8a1004fa
paolomococci <paolomococci@gmail.com>
2022-09-04 20:21:29 +0200

    deleted:    Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/CardNotFoundException.java

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/CardNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package local.example.trolley.api.exception;
-
-public class CardNotFoundException
-        extends RuntimeException {
-
-    public CardNotFoundException(Long id) {
-        super("could not find card with id: " + id);
-    }
-}
