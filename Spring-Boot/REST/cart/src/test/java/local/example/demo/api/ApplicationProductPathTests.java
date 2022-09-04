4d608d9359066528a21f40f4dba66c75dd0e2ecc
paolomococci <paolomococci@gmail.com>
2022-09-04 21:11:37 +0200

    I moved:   Spring-Boot/REST/cart/src/test/java/local/example/demo/api/ApplicationProductPathTests.java

---
+++ Spring-Boot/REST/cart/src/test/java/local/example/demo/api/ApplicationProductPathTests.java
@@ -0,0 +1,30 @@
+package local.example.demo.api;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ApplicationProductPathTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private static final String API_V1_BASE_PATH = "/api/v1/products/";
+
+    @Test
+    void readTest()
+            throws Exception {
+        // TODO
+    }
+
+    @Test
+    void readAllTest()
+            throws Exception {
+        // TODO
+    }
+}
