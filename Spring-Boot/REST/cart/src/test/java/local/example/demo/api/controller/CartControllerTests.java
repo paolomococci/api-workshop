e0bcacdc737779ce14e58836178901e3005c2cfe
paolomococci <paolomococci@gmail.com>
2022-09-04 21:11:56 +0200

    I moved:   Spring-Boot/REST/cart/src/test/java/local/example/demo/api/controller/CartControllerTests.java

---
+++ Spring-Boot/REST/cart/src/test/java/local/example/demo/api/controller/CartControllerTests.java
@@ -0,0 +1,22 @@
+package local.example.demo.api.controller;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CartControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void someTest()
+            throws Exception {
+        // TODO
+    }
+}
