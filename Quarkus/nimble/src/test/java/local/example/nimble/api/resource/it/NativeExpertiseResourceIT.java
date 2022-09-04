fe59ae07c470156d0e4595dbfd17f76eb6a2ec19
paolomococci <paolomococci@gmail.com>
2022-09-04 19:55:52 +0200

    deleted:    Quarkus/nimble/src/test/java/local/example/nimble/api/resource/it/NativeExpertiseResourceIT.java

--- Quarkus/nimble/src/test/java/local/example/nimble/api/resource/it/NativeExpertiseResourceIT.java
+++
@@ -1,9 +0,0 @@
-package local.example.nimble.api.resource.it;
-
-import io.quarkus.test.junit.NativeImageTest;
-import local.example.nimble.api.resource.ExpertiseResourceTests;
-
-@NativeImageTest
-public class NativeExpertiseResourceIT
-        extends ExpertiseResourceTests {
-}
