b82ff4dc5b69d1b05559461a9052bde5bfca30d3
paolomococci <paolomococci@gmail.com>
2022-09-04 21:14:57 +0200

    I moved:   Spring-Boot/REST/pet-clinic/src/main/java/local/example/demo/model/Error.java

---
+++ Spring-Boot/REST/pet-clinic/src/main/java/local/example/demo/model/Error.java
@@ -0,0 +1,95 @@
+package local.example.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.validation.constraints.NotNull;
+
+import java.util.Objects;
+
+public class Error {
+
+  @JsonProperty("code")
+  private Integer code;
+
+  @JsonProperty("message")
+  private String message;
+
+  public Error code(Integer code) {
+    this.code = code;
+    return this;
+  }
+
+  /**
+   * Get code
+   * @return code
+  */
+  @ApiModelProperty(required = true)
+  @NotNull
+  public Integer getCode() {
+    return code;
+  }
+
+  public void setCode(Integer code) {
+    this.code = code;
+  }
+
+  public Error message(String message) {
+    this.message = message;
+    return this;
+  }
+
+  /**
+   * Get message
+   * @return message
+  */
+  @ApiModelProperty(required = true)
+  @NotNull
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Error error = (Error) o;
+    return Objects.equals(this.code, error.code) &&
+        Objects.equals(this.message, error.message);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(code, message);
+  }
+
+  @Override
+  public String toString() {
+
+    return "class Error {\n" +
+            "    code: " + toIndentedString(code) + "\n" +
+            "    message: " + toIndentedString(message) + "\n" +
+            "}";
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
