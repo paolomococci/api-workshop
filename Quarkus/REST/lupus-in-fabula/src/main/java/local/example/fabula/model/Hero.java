202237d2105e33cf5b3d2f97a0273f956a64ed72
paolomococci <paolomococci@gmail.com>
2022-09-04 20:56:16 +0200

    I moved:   Quarkus/REST/lupus-in-fabula/src/main/java/local/example/fabula/model/Hero.java

---
+++ Quarkus/REST/lupus-in-fabula/src/main/java/local/example/fabula/model/Hero.java
@@ -0,0 +1,21 @@
+package local.example.fabula.model;
+
+import java.util.UUID;
+
+public class Hero {
+
+    public String id;
+    public String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setId() {
+        this.id = UUID.randomUUID().toString();
+    }
+}
