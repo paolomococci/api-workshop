aa92be4483ef572b19fb9a722239585186331d9f
paolomococci <paolomococci@gmail.com>
2022-09-04 20:21:35 +0200

    deleted:    Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/CartNotFoundException.java

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/CartNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package local.example.trolley.api.exception;
-
-public class CartNotFoundException
-        extends RuntimeException {
-
-    public CartNotFoundException(Long id) {
-        super("could not find cart with id: " + id);
-    }
-}
