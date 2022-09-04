053a3fff7a58b5466f37dd34ca2c540128bca48a
paolomococci <paolomococci@gmail.com>
2022-09-04 20:21:47 +0200

    deleted:    Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/ForwarderNotFoundException.java

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/ForwarderNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package local.example.trolley.api.exception;
-
-public class ForwarderNotFoundException
-        extends RuntimeException {
-
-    public ForwarderNotFoundException(Long id) {
-        super("could not find forwarder with id: " + id);
-    }
-}
