fb8f899a54f05aadae11adf57c36e0973d1d8bd3
paolomococci <paolomococci@gmail.com>
2022-09-04 20:22:24 +0200

    deleted:    Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/PaymentNotFoundException.java

--- Spring-Boot/trolley/src/main/java/local/example/trolley/api/exception/PaymentNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package local.example.trolley.api.exception;
-
-public class PaymentNotFoundException
-        extends RuntimeException {
-
-    public PaymentNotFoundException(Long id) {
-        super("could not find payment with id: " + id);
-    }
-}
