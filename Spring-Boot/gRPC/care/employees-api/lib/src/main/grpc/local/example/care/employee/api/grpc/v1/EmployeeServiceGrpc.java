97319e5c9472afac0d2b65e1a6e8a9dfaac990ea
paolomococci <paolomococci@gmail.com>
2022-09-17 19:42:04 +0200

    I added EmployeeServiceGrpc.java belonging to the employees-api of care project

---
+++ Spring-Boot/gRPC/care/employees-api/lib/src/main/grpc/local/example/care/employee/api/grpc/v1/EmployeeServiceGrpc.java
@@ -0,0 +1,276 @@
+package local.example.care.employee.api.grpc.v1;
+
+import static io.grpc.MethodDescriptor.generateFullMethodName;
+
+/**
+ */
+@javax.annotation.Generated(
+    value = "by gRPC proto compiler (version 1.49.0)",
+    comments = "Source: Employee.proto")
+@io.grpc.stub.annotations.GrpcGenerated
+public final class EmployeeServiceGrpc {
+
+  private EmployeeServiceGrpc() {}
+
+  public static final String SERVICE_NAME = "local.example.care.employee.api.v1.EmployeeService";
+
+  // Static method descriptors that strictly reflect the proto.
+  private static volatile io.grpc.MethodDescriptor<local.example.care.employee.api.grpc.v1.Employee,
+      local.example.care.employee.api.grpc.v1.EmployeeCreateResponse> getCreateMethod;
+
+  @io.grpc.stub.annotations.RpcMethod(
+      fullMethodName = SERVICE_NAME + '/' + "Create",
+      requestType = local.example.care.employee.api.grpc.v1.Employee.class,
+      responseType = local.example.care.employee.api.grpc.v1.EmployeeCreateResponse.class,
+      methodType = io.grpc.MethodDescriptor.MethodType.UNARY)
+  public static io.grpc.MethodDescriptor<local.example.care.employee.api.grpc.v1.Employee,
+      local.example.care.employee.api.grpc.v1.EmployeeCreateResponse> getCreateMethod() {
+    io.grpc.MethodDescriptor<local.example.care.employee.api.grpc.v1.Employee, local.example.care.employee.api.grpc.v1.EmployeeCreateResponse> getCreateMethod;
+    if ((getCreateMethod = EmployeeServiceGrpc.getCreateMethod) == null) {
+      synchronized (EmployeeServiceGrpc.class) {
+        if ((getCreateMethod = EmployeeServiceGrpc.getCreateMethod) == null) {
+          EmployeeServiceGrpc.getCreateMethod = getCreateMethod =
+              io.grpc.MethodDescriptor.<local.example.care.employee.api.grpc.v1.Employee, local.example.care.employee.api.grpc.v1.EmployeeCreateResponse>newBuilder()
+              .setType(io.grpc.MethodDescriptor.MethodType.UNARY)
+              .setFullMethodName(generateFullMethodName(SERVICE_NAME, "Create"))
+              .setSampledToLocalTracing(true)
+              .setRequestMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  local.example.care.employee.api.grpc.v1.Employee.getDefaultInstance()))
+              .setResponseMarshaller(io.grpc.protobuf.ProtoUtils.marshaller(
+                  local.example.care.employee.api.grpc.v1.EmployeeCreateResponse.getDefaultInstance()))
+              .setSchemaDescriptor(new EmployeeServiceMethodDescriptorSupplier("Create"))
+              .build();
+        }
+      }
+    }
+    return getCreateMethod;
+  }
+
+  /**
+   * Creates a new async stub that supports all call types for the service
+   */
+  public static EmployeeServiceStub newStub(io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<EmployeeServiceStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<EmployeeServiceStub>() {
+        @java.lang.Override
+        public EmployeeServiceStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new EmployeeServiceStub(channel, callOptions);
+        }
+      };
+    return EmployeeServiceStub.newStub(factory, channel);
+  }
+
+  /**
+   * Creates a new blocking-style stub that supports unary and streaming output calls on the service
+   */
+  public static EmployeeServiceBlockingStub newBlockingStub(
+      io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<EmployeeServiceBlockingStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<EmployeeServiceBlockingStub>() {
+        @java.lang.Override
+        public EmployeeServiceBlockingStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new EmployeeServiceBlockingStub(channel, callOptions);
+        }
+      };
+    return EmployeeServiceBlockingStub.newStub(factory, channel);
+  }
+
+  /**
+   * Creates a new ListenableFuture-style stub that supports unary calls on the service
+   */
+  public static EmployeeServiceFutureStub newFutureStub(
+      io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<EmployeeServiceFutureStub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<EmployeeServiceFutureStub>() {
+        @java.lang.Override
+        public EmployeeServiceFutureStub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new EmployeeServiceFutureStub(channel, callOptions);
+        }
+      };
+    return EmployeeServiceFutureStub.newStub(factory, channel);
+  }
+
+  /**
+   */
+  public static abstract class EmployeeServiceImplBase implements io.grpc.BindableService {
+
+    /**
+     */
+    public void create(local.example.care.employee.api.grpc.v1.Employee request,
+        io.grpc.stub.StreamObserver<local.example.care.employee.api.grpc.v1.EmployeeCreateResponse> responseObserver) {
+      io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall(getCreateMethod(), responseObserver);
+    }
+
+    @java.lang.Override public final io.grpc.ServerServiceDefinition bindService() {
+      return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
+          .addMethod(
+            getCreateMethod(),
+            io.grpc.stub.ServerCalls.asyncUnaryCall(
+              new MethodHandlers<
+                local.example.care.employee.api.grpc.v1.Employee,
+                local.example.care.employee.api.grpc.v1.EmployeeCreateResponse>(
+                  this, METHODID_CREATE)))
+          .build();
+    }
+  }
+
+  /**
+   */
+  public static final class EmployeeServiceStub extends io.grpc.stub.AbstractAsyncStub<EmployeeServiceStub> {
+    private EmployeeServiceStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected EmployeeServiceStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new EmployeeServiceStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public void create(local.example.care.employee.api.grpc.v1.Employee request,
+        io.grpc.stub.StreamObserver<local.example.care.employee.api.grpc.v1.EmployeeCreateResponse> responseObserver) {
+      io.grpc.stub.ClientCalls.asyncUnaryCall(
+          getChannel().newCall(getCreateMethod(), getCallOptions()), request, responseObserver);
+    }
+  }
+
+  /**
+   */
+  public static final class EmployeeServiceBlockingStub extends io.grpc.stub.AbstractBlockingStub<EmployeeServiceBlockingStub> {
+    private EmployeeServiceBlockingStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected EmployeeServiceBlockingStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new EmployeeServiceBlockingStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public local.example.care.employee.api.grpc.v1.EmployeeCreateResponse create(local.example.care.employee.api.grpc.v1.Employee request) {
+      return io.grpc.stub.ClientCalls.blockingUnaryCall(
+          getChannel(), getCreateMethod(), getCallOptions(), request);
+    }
+  }
+
+  /**
+   */
+  public static final class EmployeeServiceFutureStub extends io.grpc.stub.AbstractFutureStub<EmployeeServiceFutureStub> {
+    private EmployeeServiceFutureStub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected EmployeeServiceFutureStub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new EmployeeServiceFutureStub(channel, callOptions);
+    }
+
+    /**
+     */
+    public com.google.common.util.concurrent.ListenableFuture<local.example.care.employee.api.grpc.v1.EmployeeCreateResponse> create(
+        local.example.care.employee.api.grpc.v1.Employee request) {
+      return io.grpc.stub.ClientCalls.futureUnaryCall(
+          getChannel().newCall(getCreateMethod(), getCallOptions()), request);
+    }
+  }
+
+  private static final int METHODID_CREATE = 0;
+
+  private static final class MethodHandlers<Req, Resp> implements
+      io.grpc.stub.ServerCalls.UnaryMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ServerStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.ClientStreamingMethod<Req, Resp>,
+      io.grpc.stub.ServerCalls.BidiStreamingMethod<Req, Resp> {
+    private final EmployeeServiceImplBase serviceImpl;
+    private final int methodId;
+
+    MethodHandlers(EmployeeServiceImplBase serviceImpl, int methodId) {
+      this.serviceImpl = serviceImpl;
+      this.methodId = methodId;
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public void invoke(Req request, io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        case METHODID_CREATE:
+          serviceImpl.create((local.example.care.employee.api.grpc.v1.Employee) request,
+              (io.grpc.stub.StreamObserver<local.example.care.employee.api.grpc.v1.EmployeeCreateResponse>) responseObserver);
+          break;
+        default:
+          throw new AssertionError();
+      }
+    }
+
+    @java.lang.Override
+    @java.lang.SuppressWarnings("unchecked")
+    public io.grpc.stub.StreamObserver<Req> invoke(
+        io.grpc.stub.StreamObserver<Resp> responseObserver) {
+      switch (methodId) {
+        default:
+          throw new AssertionError();
+      }
+    }
+  }
+
+  private static abstract class EmployeeServiceBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoFileDescriptorSupplier, io.grpc.protobuf.ProtoServiceDescriptorSupplier {
+    EmployeeServiceBaseDescriptorSupplier() {}
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.FileDescriptor getFileDescriptor() {
+      return local.example.care.employee.api.grpc.v1.EmployeeOuterClass.getDescriptor();
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.ServiceDescriptor getServiceDescriptor() {
+      return getFileDescriptor().findServiceByName("EmployeeService");
+    }
+  }
+
+  private static final class EmployeeServiceFileDescriptorSupplier
+      extends EmployeeServiceBaseDescriptorSupplier {
+    EmployeeServiceFileDescriptorSupplier() {}
+  }
+
+  private static final class EmployeeServiceMethodDescriptorSupplier
+      extends EmployeeServiceBaseDescriptorSupplier
+      implements io.grpc.protobuf.ProtoMethodDescriptorSupplier {
+    private final String methodName;
+
+    EmployeeServiceMethodDescriptorSupplier(String methodName) {
+      this.methodName = methodName;
+    }
+
+    @java.lang.Override
+    public com.google.protobuf.Descriptors.MethodDescriptor getMethodDescriptor() {
+      return getServiceDescriptor().findMethodByName(methodName);
+    }
+  }
+
+  private static volatile io.grpc.ServiceDescriptor serviceDescriptor;
+
+  public static io.grpc.ServiceDescriptor getServiceDescriptor() {
+    io.grpc.ServiceDescriptor result = serviceDescriptor;
+    if (result == null) {
+      synchronized (EmployeeServiceGrpc.class) {
+        result = serviceDescriptor;
+        if (result == null) {
+          serviceDescriptor = result = io.grpc.ServiceDescriptor.newBuilder(SERVICE_NAME)
+              .setSchemaDescriptor(new EmployeeServiceFileDescriptorSupplier())
+              .addMethod(getCreateMethod())
+              .build();
+        }
+      }
+    }
+    return result;
+  }
+}
