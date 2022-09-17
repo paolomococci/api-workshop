b3e330c2ff09f8f8b52257fb691c617c66b5405d
paolomococci <paolomococci@gmail.com>
2022-09-17 21:41:00 +0200

    I added LibraryTest.java belonging to the items-api of care project

---
+++ Spring-Boot/gRPC/care/items-api/lib/src/test/java/items/api/LibraryTest.java
@@ -0,0 +1,14 @@
+package items.api;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibraryTest {
+    @Test void someLibraryMethodReturnsTrue() {
+        Library classUnderTest = new Library();
+        assertTrue(
+                classUnderTest.someLibraryMethod(),
+                "someLibraryMethod should return 'true'"
+        );
+    }
+}
