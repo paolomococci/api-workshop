1761a4dd4b4b61e0e0ad5183c827243a9d36a65b
paolomococci <paolomococci@gmail.com>
2022-10-17 20:46:44 +0200

    edited LibraryTest.java

--- Gradle/composite/spread/spread-api/lib/src/test/java/local/example/spread/api/LibraryTest.java
+++ Gradle/composite/spread/spread-api/lib/src/test/java/local/example/spread/api/LibraryTest.java
@@ -7,8 +7,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibraryTest {
+
     @Test void someLibraryMethodReturnsTrue() {
         Library classUnderTest = new Library();
-        assertTrue(classUnderTest.someLibraryMethod(), "someLibraryMethod should return 'true'");
+        assertTrue(
+                classUnderTest.someLibraryMethod(),
+                "someLibraryMethod should return 'true'"
+        );
     }
 }
