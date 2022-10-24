d1979fd41079b025d067ef6ab83e988bc563c451
paolomococci <paolomococci@gmail.com>
2022-10-24 11:20:49 +0200

    modified:   Quarkus/REST/dummy-users/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt

--- Quarkus/REST/dummy-users/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
+++ Quarkus/REST/dummy-users/src/test/kotlin/local/example/outcome/OutcomeResourceTest.kt
@@ -20,8 +20,20 @@ class OutcomeResourceTest {
           .then().statusCode(HttpStatus.SC_OK)
     }
 
+    @Test
+    fun testCreateEndpoint() {
+        given().contentType(ContentType.JSON)
+            .body(JSON_DATA).`when`().post(BASE_PATH)
+            .then().statusCode(HttpStatus.SC_OK)
+            .body(`is`(JSON_OUTCOME))
+    }
+
     companion object {
         private const val BASE_PATH = "/outcome"
+        private const val JSON_DATA =
+            "{\"login\":{\"uuid\":\"c066898d-828a-4af8-bc52-f3d1d06156d9\"},\"name\":{\"title\":\"\",\"first\":\"\",\"second\":\"\"},\"email\":\"\",\"phone\":\"\",\"cell\":\"\",\"picture\":{\"large\":\"\",\"medium\":\"\",\"thumbnail\":\"\"}}"
+        private const val JSON_OUTCOME =
+            "[{\"login\":{\"uuid\":\"c066898d-828a-4af8-bc52-f3d1d06156d9\"},\"name\":{\"title\":\"\",\"first\":\"\",\"second\":\"\"},\"email\":\"\",\"phone\":\"\",\"cell\":\"\",\"picture\":{\"large\":\"\",\"medium\":\"\",\"thumbnail\":\"\"}}]"
     }
 
 }
