7716e61b66a307ac4015fe20f2b40aa94d51e84d
paolomococci <paolomococci@gmail.com>
2022-10-24 08:58:34 +0200

    modified:   Quarkus/REST/dummy-users/src/main/kotlin/local/example/outcome/OutcomeResource.kt

--- Quarkus/REST/dummy-users/src/main/kotlin/local/example/outcome/OutcomeResource.kt
+++ Quarkus/REST/dummy-users/src/main/kotlin/local/example/outcome/OutcomeResource.kt
@@ -2,13 +2,14 @@ package local.example.outcome
 
 import javax.ws.rs.GET
 import javax.ws.rs.Path
-import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
 
 @Path("/outcome")
 class OutcomeResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    fun feedback() = "--> sample feedback <--"
+    @Produces(MediaType.APPLICATION_JSON)
+    fun read(): Response? {
+        return Response.ok().build()
 }
