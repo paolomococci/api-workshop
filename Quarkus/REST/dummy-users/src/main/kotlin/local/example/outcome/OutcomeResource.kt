3aeeb8e0dbccf2297c1405634caeef6684783a74
paolomococci <paolomococci@gmail.com>
2022-10-24 09:25:20 +0200

    modified:   Quarkus/REST/dummy-users/src/main/kotlin/local/example/outcome/OutcomeResource.kt

--- Quarkus/REST/dummy-users/src/main/kotlin/local/example/outcome/OutcomeResource.kt
+++ Quarkus/REST/dummy-users/src/main/kotlin/local/example/outcome/OutcomeResource.kt
@@ -4,8 +4,18 @@ import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
 
+import local.example.outcome.model.User
+
+import java.util.*
+
 @Path("/outcome")
 class OutcomeResource {
+
+    private val users = Collections.newSetFromMap(
+        Collections.synchronizedMap(
+            LinkedHashMap<User, Boolean>()
+        )
+    )
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
