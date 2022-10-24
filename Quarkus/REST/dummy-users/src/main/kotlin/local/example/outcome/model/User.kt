06eeb8c2c3dd7b886cf753b1b5c1a56f37ecb7aa
paolomococci <paolomococci@gmail.com>
2022-10-24 16:09:01 +0200

    edited User.kt

--- Quarkus/REST/dummy-users/src/main/kotlin/local/example/outcome/model/User.kt
+++ Quarkus/REST/dummy-users/src/main/kotlin/local/example/outcome/model/User.kt
@@ -1,12 +1,10 @@
 package local.example.outcome.model
 
-class User(
-    //?,
-    //var picture: UserPicture?
-) {
+class User() {
     val login: UserLogin = UserLogin()
     var name: UserName? = null
     var email: String = ""
     var phone: String = ""
     var cell: String = ""
+    var picture: UserPicture? = null
 }
