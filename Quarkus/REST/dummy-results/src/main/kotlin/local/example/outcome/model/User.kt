1d1967dcc02c1c67875c192b4539e76d611defec
paolomococci <paolomococci@gmail.com>
2022-10-26 10:01:59 +0200

    edited

--- Quarkus/REST/dummy-results/src/main/kotlin/local/example/outcome/model/User.kt
+++ Quarkus/REST/dummy-results/src/main/kotlin/local/example/outcome/model/User.kt
@@ -1,4 +1,10 @@
 package local.example.outcome.model
 
 class User {
+    val login: Login = Login()
+    var name: Name? = null
+    var email: String = ""
+    var phone: String = ""
+    var cell: String = ""
+    var picture: Picture? = null
 }
