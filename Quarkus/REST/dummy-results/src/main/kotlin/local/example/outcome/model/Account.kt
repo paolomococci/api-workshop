97f2f53fd03bcbbafc6a2a17f0ea0bbc74c4fcca
paolomococci <paolomococci@gmail.com>
2022-10-26 16:50:32 +0200

    edited

--- Quarkus/REST/dummy-results/src/main/kotlin/local/example/outcome/model/Account.kt
+++ Quarkus/REST/dummy-results/src/main/kotlin/local/example/outcome/model/Account.kt
@@ -1,4 +1,10 @@
 package local.example.outcome.model
 
+import java.util.*
+
 class Account {
+    val users: Set<User>? = Collections.newSetFromMap(
+        Collections.synchronizedMap(
+            LinkedHashMap<User, Boolean>()
+        ))
 }
