128fd1f7ed21d27c2df511d72a4b89ef153aae2e
paolomococci <paolomococci@gmail.com>
2022-10-26 18:37:26 +0200

    edited

--- Quarkus/REST/dummy-accounts/src/main/kotlin/local/example/outcome/model/User.kt
+++ Quarkus/REST/dummy-accounts/src/main/kotlin/local/example/outcome/model/User.kt
@@ -1,4 +1,16 @@
 package local.example.outcome.model
 
 class User {
+    var gender: String = ""
+    var name: Name? = null
+    var location: Location? = null
+    var email: String = ""
+    val login: Login = Login()
+    var dob: Dob? = null
+    var registered: Dob? = null
+    var phone: String = ""
+    var cell: String = ""
+    var id: Id? = null
+    var picture: Picture? = null
+    var nat: String = ""
 }
