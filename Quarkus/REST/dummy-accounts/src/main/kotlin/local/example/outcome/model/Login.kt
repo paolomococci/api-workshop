20a54cd23cbc201276d2a917f9919b82aba0f2a1
paolomococci <paolomococci@gmail.com>
2022-10-26 18:36:53 +0200

    edited

--- Quarkus/REST/dummy-accounts/src/main/kotlin/local/example/outcome/model/Login.kt
+++ Quarkus/REST/dummy-accounts/src/main/kotlin/local/example/outcome/model/Login.kt
@@ -1,4 +1,13 @@
 package local.example.outcome.model
 
+import java.util.*
+
 class Login {
+    val uuid: String = UUID.randomUUID().toString()
+    var username: String = ""
+    var password: String = ""
+    var salt: String = ""
+    var md5: String = ""
+    var sha1: String = ""
+    var sha256: String = ""
 }
