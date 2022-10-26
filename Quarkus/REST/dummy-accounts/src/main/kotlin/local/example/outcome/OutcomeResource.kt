826955d9e041b793f93174befd6fd71346fc4ce5
paolomococci <paolomococci@gmail.com>
2022-10-26 18:51:09 +0200

    edited

--- Quarkus/REST/dummy-accounts/src/main/kotlin/local/example/outcome/OutcomeResource.kt
+++ Quarkus/REST/dummy-accounts/src/main/kotlin/local/example/outcome/OutcomeResource.kt
@@ -1,14 +1,29 @@
 package local.example.outcome
 
+import local.example.outcome.model.User
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
 
 @Path("/outcome")
 class OutcomeResource {
 
+    private val result = local.example.outcome.model.Result()
+
+    init {
+        for (i in 1..10) {
+            this.result.results!!.add(User())
+        }
+    }
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    fun hello() = "Hello from RESTEasy Reactive"
+    @Produces(MediaType.APPLICATION_JSON)
+    fun readAll(): Response? {
+        return if (
+            result.results!!.isEmpty()
+        ) Response.ok().build()
+        else Response.ok(result).build()
+    }
 }
