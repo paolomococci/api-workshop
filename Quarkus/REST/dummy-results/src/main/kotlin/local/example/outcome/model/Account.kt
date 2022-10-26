9a84ca3ec46df6753e422ffd4129ded3dee13178
paolomococci <paolomococci@gmail.com>
2022-10-26 17:42:59 +0200

    edited

--- Quarkus/REST/dummy-results/src/main/kotlin/local/example/outcome/model/Account.kt
+++ Quarkus/REST/dummy-results/src/main/kotlin/local/example/outcome/model/Account.kt
@@ -3,7 +3,7 @@ package local.example.outcome.model
 import java.util.*
 
 class Account {
-    val users: Set<User>? = Collections.newSetFromMap(
+    val users: MutableSet<User>? = Collections.newSetFromMap(
         Collections.synchronizedMap(
             LinkedHashMap<User, Boolean>()
         ))
