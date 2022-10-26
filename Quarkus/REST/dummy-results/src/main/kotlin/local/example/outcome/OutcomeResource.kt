7081f9ce349216fc764224f6ca9fe85ff80c33fc
paolomococci <paolomococci@gmail.com>
2022-10-26 16:50:39 +0200

    edited

--- Quarkus/REST/dummy-results/src/main/kotlin/local/example/outcome/OutcomeResource.kt
+++ Quarkus/REST/dummy-results/src/main/kotlin/local/example/outcome/OutcomeResource.kt
@@ -1,14 +1,23 @@
 package local.example.outcome
 
+import local.example.outcome.model.Account
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
 
 @Path("/outcome")
 class OutcomeResource {
 
+    private val account: Account = Account()
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    fun feedback() = "--> feedback <--"
+    @Produces(MediaType.APPLICATION_JSON)
+    fun readAll(): Response? {
+        return if (
+            account.users!!.isEmpty()
+        ) Response.ok().build()
+        else Response.ok(account).build()
+    }
 }
