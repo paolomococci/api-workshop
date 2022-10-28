9126311ba8d3823e7960c8b9411ccdc552497490
paolomococci <paolomococci@gmail.com>
2022-10-28 09:39:38 +0200

    added

---
+++ Spring-Boot/REST/graph-flow/src/test/kotlin/local/example/graph/GraphApplicationTests.kt
@@ -0,0 +1,13 @@
+package local.example.graph
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraphApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
