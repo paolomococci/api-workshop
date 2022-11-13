0548081ebd7456cf850b5dffc09b19291de563a0
paolomococci <paolomococci@gmail.com>
2022-11-13 23:56:55 +0100

    edited

--- Spring-Boot/GraphQL/graphql/src/main/java/local/example/graphql/controller/ArticleController.java
+++ Spring-Boot/GraphQL/graphql/src/main/java/local/example/graphql/controller/ArticleController.java
@@ -2,11 +2,20 @@ package local.example.graphql.controller;
 
 import local.example.graphql.model.Article;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
 
 @Controller
 public class ArticleController {
 
     @Autowired
     private Article article;
+
+    @SchemaMapping
+    public List<Article> articles(Article article) {
+        // TODO
+        return null;
+    }
 }
