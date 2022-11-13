5ad9b5844037f7fc398e028e7a49a327dfdd3c61
paolomococci <paolomococci@gmail.com>
2022-11-13 23:52:49 +0100

    edited

--- Spring-Boot/GraphQL/graphql/src/main/java/local/example/graphql/controller/AuthorController.java
+++ Spring-Boot/GraphQL/graphql/src/main/java/local/example/graphql/controller/AuthorController.java
@@ -1,7 +1,12 @@
 package local.example.graphql.controller;
 
+import local.example.graphql.model.Author;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class AuthorController {
+
+    @Autowired
+    private Author author;
 }
