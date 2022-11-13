a24027f36fc9265aa66887d3891c4e478c5caf50
paolomococci <paolomococci@gmail.com>
2022-11-13 23:41:22 +0100

    edited

--- Spring-Boot/GraphQL/graphql/src/main/java/local/example/graphql/model/Author.java
+++ Spring-Boot/GraphQL/graphql/src/main/java/local/example/graphql/model/Author.java
@@ -1,4 +1,14 @@
 package local.example.graphql.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+@Data
+@Entity
 public class Author {
+    private String id;
+    private String name;
+    private String surname;
+    private String nickname;
 }
