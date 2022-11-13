9758d630b0f4bf4eabca93cbc5cd93c68fbf8011
paolomococci <paolomococci@gmail.com>
2022-11-13 06:53:18 +0100

    new file:   Spring-Boot/GraphQL/graphql/src/test/java/local/example/graphql/GraphqlApplicationTests.java

---
+++ Spring-Boot/GraphQL/graphql/src/test/java/local/example/graphql/GraphqlApplicationTests.java
@@ -0,0 +1,16 @@
+package local.example.graphql;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlApplicationTests {
+
+	@Test
+	void sampleTest() {
+		assertTrue(true);
+	}
+
+}
