03342a5497786be2466e8d2f50ed56fff4b62132
paolomococci <paolomococci@gmail.com>
2022-11-13 23:45:05 +0100

    edited

--- Spring-Boot/GraphQL/graphql/src/main/java/local/example/graphql/model/Article.java
+++ Spring-Boot/GraphQL/graphql/src/main/java/local/example/graphql/model/Article.java
@@ -7,4 +7,9 @@ import javax.persistence.Entity;
 @Data
 @Entity
 public class Article {
+    private String id;
+    private String title;
+    private String text;
+    private String category;
+    private String authorId;
 }
