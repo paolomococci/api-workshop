0e6fd4365febdea526500de44a8d7e9301cd9991
paolomococci <paolomococci@gmail.com>
2022-11-14 20:30:26 +0100

    edited

--- Spring-Boot/GraphQL/take-away/src/main/java/local/example/graphql/repo/ArticleRepo.java
+++ Spring-Boot/GraphQL/take-away/src/main/java/local/example/graphql/repo/ArticleRepo.java
@@ -3,6 +3,7 @@ package local.example.graphql.repo;
 import local.example.graphql.model.Article;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ArticleRepo {
     private final List<Article> articles;
@@ -15,7 +16,9 @@ public class ArticleRepo {
             int count,
             int offset
     ) {
-        return null;
+        return articles.stream()
+                .skip(offset).limit(count)
+                .collect(Collectors.toList());
     }
 
     public List<Article> getArticlesOfAuthor(String authorId) {
