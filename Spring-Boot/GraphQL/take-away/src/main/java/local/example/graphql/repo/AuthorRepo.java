d4889efd7fa97e35d1cae7bd887f261b270e261a
paolomococci <paolomococci@gmail.com>
2022-11-14 20:10:15 +0100

    edited

--- Spring-Boot/GraphQL/take-away/src/main/java/local/example/graphql/repo/AuthorRepo.java
+++ Spring-Boot/GraphQL/take-away/src/main/java/local/example/graphql/repo/AuthorRepo.java
@@ -1,4 +1,13 @@
 package local.example.graphql.repo;
 
+import local.example.graphql.model.Author;
+
+import java.util.List;
+
 public class AuthorRepo {
+    private final List<Author> authors;
+
+    public AuthorRepo(List<Author> authors) {
+        this.authors = authors;
+    }
 }
