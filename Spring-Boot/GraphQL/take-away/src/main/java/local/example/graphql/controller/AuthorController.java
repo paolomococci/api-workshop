e6372eb6ea3f6f4466023cf26f8181448ec086e1
paolomococci <paolomococci@gmail.com>
2022-11-14 21:12:53 +0100

    edited

--- Spring-Boot/GraphQL/take-away/src/main/java/local/example/graphql/controller/AuthorController.java
+++ Spring-Boot/GraphQL/take-away/src/main/java/local/example/graphql/controller/AuthorController.java
@@ -3,6 +3,7 @@ package local.example.graphql.controller;
 import local.example.graphql.model.Article;
 import local.example.graphql.model.Author;
 import local.example.graphql.repo.ArticleRepo;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
@@ -23,7 +24,11 @@ public class AuthorController {
     }
 
     @MutationMapping
-    public Author create() {
+    public Author create(
+            @Argument String name,
+            @Argument String surname,
+            @Argument String nickname
+    ) {
         return null;
     }
 }
