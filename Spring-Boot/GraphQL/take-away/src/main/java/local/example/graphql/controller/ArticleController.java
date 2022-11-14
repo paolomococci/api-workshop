8e89dbd9d0b8d09f6cf3cd8d035af05fa3453492
paolomococci <paolomococci@gmail.com>
2022-11-14 20:39:57 +0100

    edited

--- Spring-Boot/GraphQL/take-away/src/main/java/local/example/graphql/controller/ArticleController.java
+++ Spring-Boot/GraphQL/take-away/src/main/java/local/example/graphql/controller/ArticleController.java
@@ -1,7 +1,13 @@
 package local.example.graphql.controller;
 
+import local.example.graphql.repo.ArticleRepo;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ArticleController {
+    private final ArticleRepo articleRepo;
+
+    public ArticleController(ArticleRepo articleRepo) {
+        this.articleRepo = articleRepo;
+    }
 }
