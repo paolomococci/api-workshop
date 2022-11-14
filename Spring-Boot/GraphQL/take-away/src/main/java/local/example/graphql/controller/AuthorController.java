f8bbf3ebe73746d8294a741975c09a5090b31f95
paolomococci <paolomococci@gmail.com>
2022-11-14 20:39:22 +0100

    edited

--- Spring-Boot/GraphQL/take-away/src/main/java/local/example/graphql/controller/AuthorController.java
+++ Spring-Boot/GraphQL/take-away/src/main/java/local/example/graphql/controller/AuthorController.java
@@ -1,7 +1,13 @@
 package local.example.graphql.controller;
 
+import local.example.graphql.repo.AuthorRepo;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class AuthorController {
+    private final AuthorRepo authorRepo;
+
+    public AuthorController(AuthorRepo authorRepo) {
+        this.authorRepo = authorRepo;
+    }
 }
