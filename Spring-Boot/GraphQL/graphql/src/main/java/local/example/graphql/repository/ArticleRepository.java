9ae906531517fe2f6ff2a327c9ee653cf969223a
paolomococci <paolomococci@gmail.com>
2022-11-14 08:25:46 +0100

    edited

--- Spring-Boot/GraphQL/graphql/src/main/java/local/example/graphql/repository/ArticleRepository.java
+++ Spring-Boot/GraphQL/graphql/src/main/java/local/example/graphql/repository/ArticleRepository.java
@@ -1,4 +1,10 @@
 package local.example.graphql.repository;
 
-public interface ArticleRepository {
+import local.example.graphql.model.Article;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository
+        extends CrudRepository<Article, String> {
 }
