23d014c2d20f7603514ade77437ecfcb28d2fd27
paolomococci <paolomococci@gmail.com>
2022-11-14 08:39:36 +0100

    edited

--- Spring-Boot/GraphQL/graphql/src/main/java/local/example/graphql/controller/ArticleController.java
+++ Spring-Boot/GraphQL/graphql/src/main/java/local/example/graphql/controller/ArticleController.java
@@ -1,7 +1,11 @@
 package local.example.graphql.controller;
 
 import local.example.graphql.model.Article;
+import local.example.graphql.repository.ArticleRepository;
+import local.example.graphql.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
@@ -11,11 +15,16 @@ import java.util.List;
 public class ArticleController {
 
     @Autowired
-    private Article article;
+    private ArticleRepository articleRepository;
 
-    @SchemaMapping
-    public List<Article> articles(Article article) {
-        // TODO
-        return null;
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    @QueryMapping
+    public List<Article> recentArticles(
+            @Argument int count,
+            @Argument int offset
+    ) {
+        return articleRepository.findRecentArticles(count, offset);
     }
 }
