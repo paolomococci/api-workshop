85b8602429c28442abeaf7af546ce61e6c06ebdc
paolomococci <paolomococci@gmail.com>
2022-11-14 08:39:51 +0100

    edited

--- Spring-Boot/GraphQL/graphql/src/main/java/local/example/graphql/controller/AuthorController.java
+++ Spring-Boot/GraphQL/graphql/src/main/java/local/example/graphql/controller/AuthorController.java
@@ -1,6 +1,9 @@
 package local.example.graphql.controller;
 
+import local.example.graphql.model.Article;
 import local.example.graphql.model.Author;
+import local.example.graphql.repository.ArticleRepository;
+import local.example.graphql.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
@@ -11,11 +14,10 @@ import java.util.List;
 public class AuthorController {
 
     @Autowired
-    private Author author;
+    private ArticleRepository articleRepository;
 
     @SchemaMapping
-    public List<Author> authors(Author author) {
-        // TODO
-        return null;
+    public List<Article> authors(Author author) {
+        return articleRepository.findArticleByAuthorId(author.getId());
     }
 }
