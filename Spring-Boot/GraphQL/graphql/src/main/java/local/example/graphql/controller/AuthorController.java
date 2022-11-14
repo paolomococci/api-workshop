50c6d4475fcb8c72b76331d6768941534814c4d8
paolomococci <paolomococci@gmail.com>
2022-11-14 09:20:46 +0100

    edited

--- Spring-Boot/GraphQL/graphql/src/main/java/local/example/graphql/controller/AuthorController.java
+++ Spring-Boot/GraphQL/graphql/src/main/java/local/example/graphql/controller/AuthorController.java
@@ -5,10 +5,13 @@ import local.example.graphql.model.Author;
 import local.example.graphql.repository.ArticleRepository;
 import local.example.graphql.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 public class AuthorController {
@@ -29,5 +32,18 @@ public class AuthorController {
         if (authorRepository.findById(author.getId()).isPresent())
             return  authorRepository.findById(author.getId()).get();
         return new Author();
+    }
+
+    @MutationMapping
+    public Author createAuthor(
+            @Argument String name,
+            @Argument String surname,
+            @Argument String nickname
+    ) {
+        Author author = new Author();
+        author.setId(UUID.randomUUID().toString());
+        // TODO
+        authorRepository.save(author);
+        return author;
     }
 }
