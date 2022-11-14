372f414617bbe25e2db9002485e4ab5681dd8eb2
paolomococci <paolomococci@gmail.com>
2022-11-14 16:27:14 +0100

    edited

--- Spring-Boot/GraphQL/graphql/src/test/java/local/example/graphql/repository/ArticleRepositoryTests.java
+++ Spring-Boot/GraphQL/graphql/src/test/java/local/example/graphql/repository/ArticleRepositoryTests.java
@@ -1,5 +1,6 @@
 package local.example.graphql.repository;
 
+import local.example.graphql.model.Article;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,5 +16,13 @@ public class ArticleRepositoryTests {
     @Test
     void contextTest() {
         assertThat(articleRepository).isNotNull();
+    }
+
+    @Test
+    void createTest() {
+        Article article = new Article();
+        articleRepository.save(article);
+        assertThat(article.getId()).isGreaterThanOrEqualTo(0);
+        assertThat(articleRepository.count()).isGreaterThanOrEqualTo(1L);
     }
 }
