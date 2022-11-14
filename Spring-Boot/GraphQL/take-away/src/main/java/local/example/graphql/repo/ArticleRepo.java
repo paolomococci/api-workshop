d14b9a13d0c49ed628f4476f273c7e89735f73ad
paolomococci <paolomococci@gmail.com>
2022-11-14 20:17:03 +0100

    edited

--- Spring-Boot/GraphQL/take-away/src/main/java/local/example/graphql/repo/ArticleRepo.java
+++ Spring-Boot/GraphQL/take-away/src/main/java/local/example/graphql/repo/ArticleRepo.java
@@ -1,4 +1,13 @@
 package local.example.graphql.repo;
 
+import local.example.graphql.model.Article;
+
+import java.util.List;
+
 public class ArticleRepo {
+    private final List<Article> articles;
+
+    public ArticleRepo(List<Article> articles) {
+        this.articles = articles;
+    }
 }
