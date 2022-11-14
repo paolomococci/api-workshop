37806c40552914b16e17fe3f1cc69f5dd83e66cd
paolomococci <paolomococci@gmail.com>
2022-11-14 08:39:41 +0100

    edited

--- Spring-Boot/GraphQL/graphql/src/main/java/local/example/graphql/repository/ArticleRepository.java
+++ Spring-Boot/GraphQL/graphql/src/main/java/local/example/graphql/repository/ArticleRepository.java
@@ -4,7 +4,12 @@ import local.example.graphql.model.Article;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ArticleRepository
         extends CrudRepository<Article, String> {
+    List<Article> findArticleByAuthorId(String id);
+
+    List<Article> findRecentArticles(int count, int offset);
 }
