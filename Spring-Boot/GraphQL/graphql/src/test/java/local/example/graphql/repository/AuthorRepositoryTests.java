c4e329ee7c92642ecc234fb5806b80aa7e10f569
paolomococci <paolomococci@gmail.com>
2022-11-14 15:30:44 +0100

    edited

--- Spring-Boot/GraphQL/graphql/src/test/java/local/example/graphql/repository/AuthorRepositoryTests.java
+++ Spring-Boot/GraphQL/graphql/src/test/java/local/example/graphql/repository/AuthorRepositoryTests.java
@@ -1,15 +1,19 @@
 package local.example.graphql.repository;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class AuthorRepositoryTests {
 
+    @Autowired
+    private AuthorRepository authorRepository;
+
     @Test
-    void sampleTest() {
-        assertTrue(true);
+    void contextTest() {
+        assertThat(authorRepository).isNotNull();
     }
 }
