f720fcbcfc323e160eae95934b2b714d2ff9cd31
paolomococci <paolomococci@gmail.com>
2022-11-14 20:56:10 +0100

    edited

--- Spring-Boot/GraphQL/take-away/src/main/java/local/example/graphql/controller/ArticleController.java
+++ Spring-Boot/GraphQL/take-away/src/main/java/local/example/graphql/controller/ArticleController.java
@@ -1,10 +1,12 @@
 package local.example.graphql.controller;
 
 import local.example.graphql.model.Article;
+import local.example.graphql.model.Author;
 import local.example.graphql.repo.ArticleRepo;
 import local.example.graphql.repo.AuthorRepo;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
@@ -31,5 +33,10 @@ public class ArticleController {
                 count,
                 offset
         );
+    }
+
+    @SchemaMapping
+    public Author author(Article article) {
+        return this.authorRepo.getAuthor(article.getAuthorId());
     }
 }
