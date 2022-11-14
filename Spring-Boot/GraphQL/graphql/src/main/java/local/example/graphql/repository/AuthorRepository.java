efba29e92e320cb4092f9c1e4f208b8ce3133a4b
paolomococci <paolomococci@gmail.com>
2022-11-14 15:30:35 +0100

    edited

--- Spring-Boot/GraphQL/graphql/src/main/java/local/example/graphql/repository/AuthorRepository.java
+++ Spring-Boot/GraphQL/graphql/src/main/java/local/example/graphql/repository/AuthorRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuthorRepository
-        extends CrudRepository<Author, String> {
+        extends CrudRepository<Author, Integer> {
 }
