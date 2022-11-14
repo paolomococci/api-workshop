a22dfc9788ff020ab5c0539f90defc2b1c115d41
paolomococci <paolomococci@gmail.com>
2022-11-14 20:07:43 +0100

    edited

--- Spring-Boot/GraphQL/take-away/src/main/java/local/example/graphql/model/Article.java
+++ Spring-Boot/GraphQL/take-away/src/main/java/local/example/graphql/model/Article.java
@@ -4,4 +4,9 @@ import lombok.Data;
 
 @Data
 public class Article {
+    private String id;
+    private String title;
+    private String text;
+    private String category;
+    private String authorId;
 }
