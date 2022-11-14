cdd32ceafb1fa13968394dd0291bf3f8a1044e9a
paolomococci <paolomococci@gmail.com>
2022-11-14 14:32:24 +0100

    edited

--- Spring-Boot/GraphQL/graphql/src/test/java/local/example/graphql/repository/ArticleRepositoryTests.java
+++ Spring-Boot/GraphQL/graphql/src/test/java/local/example/graphql/repository/ArticleRepositoryTests.java
@@ -1,4 +1,15 @@
 package local.example.graphql.repository;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
 public class ArticleRepositoryTests {
+
+    @Test
+    void sampleTest() {
+        assertTrue(true);
+    }
 }
