064a4814ff13b333766f7e7f876e3d96fc3a926f
paolomococci <paolomococci@gmail.com>
2022-11-14 21:46:27 +0100

    edited

--- Spring-Boot/GraphQL/take-away/src/main/java/local/example/graphql/config/GraphQLConfig.java
+++ Spring-Boot/GraphQL/take-away/src/main/java/local/example/graphql/config/GraphQLConfig.java
@@ -1,6 +1,8 @@
 package local.example.graphql.config;
 
+import local.example.graphql.model.Article;
 import local.example.graphql.model.Author;
+import local.example.graphql.repo.ArticleRepo;
 import local.example.graphql.repo.AuthorRepo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,5 +26,20 @@ public class GraphQLConfig {
             authors.add(author);
         }
         return new AuthorRepo(authors);
+    }
+
+    @Bean
+    public ArticleRepo articleRepo() {
+        List<Article> articles = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Article article = new Article();
+            article.setId(UUID.randomUUID().toString());
+            article.setTitle("title_" + i);
+            article.setText("Some text of article with id: " + article.getId() + ".");
+            article.setCategory("category_" + i);
+            article.setAuthorId("");
+            articles.add(article);
+        }
+        return new ArticleRepo(articles);
     }
 }
