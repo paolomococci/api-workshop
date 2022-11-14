85895acbc3c30a1125911a83a57616fa00c8da61
paolomococci <paolomococci@gmail.com>
2022-11-14 15:30:18 +0100

    edited

--- Spring-Boot/GraphQL/graphql/src/main/java/local/example/graphql/model/Author.java
+++ Spring-Boot/GraphQL/graphql/src/main/java/local/example/graphql/model/Author.java
@@ -3,13 +3,18 @@ package local.example.graphql.model;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Data
 @Entity
 @Table(name = "authors")
 public class Author {
-    private String id;
+
+    @Id
+    @GeneratedValue
+    private int id;
     private String name;
     private String surname;
     private String nickname;
