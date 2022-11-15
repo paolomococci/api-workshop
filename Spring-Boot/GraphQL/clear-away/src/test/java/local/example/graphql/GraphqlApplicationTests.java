cc61ec2347d33c66b31db529f80ec59a6a370980
paolomococci <paolomococci@gmail.com>
2022-11-15 07:35:24 +0100

    edited

---
+++ Spring-Boot/GraphQL/clear-away/src/test/java/local/example/graphql/GraphqlApplicationTests.java
@@ -0,0 +1,15 @@
+package local.example.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class GraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertThat(1).isEqualTo(1);
+	}
+}
