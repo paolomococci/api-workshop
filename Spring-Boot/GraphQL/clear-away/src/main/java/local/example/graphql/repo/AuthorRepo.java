08f6f9c3a8393bfd373f38b47d14b5f6e1c543c0
paolomococci <paolomococci@gmail.com>
2022-11-15 07:34:27 +0100

    edited

---
+++ Spring-Boot/GraphQL/clear-away/src/main/java/local/example/graphql/repo/AuthorRepo.java
@@ -0,0 +1,23 @@
+package local.example.graphql.repo;
+
+import local.example.graphql.model.Author;
+
+import java.util.List;
+
+public class AuthorRepo {
+    private final List<Author> authors;
+
+    public AuthorRepo(List<Author> authors) {
+        this.authors = authors;
+    }
+
+    public Author getAuthor(String id) {
+        return authors.stream().filter(
+                author -> id.equals(author.getId())
+        ).findFirst().orElseThrow(RuntimeException::new);
+    }
+
+    public void save(Author author) {
+        this.authors.add(author);
+    }
+}
